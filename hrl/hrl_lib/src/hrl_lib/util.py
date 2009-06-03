60d3c42b9a8b5178b979fbffa4ff9e49d1472e40
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-06-03 22:41:45 +0000

    update pickle helper functions in hrl_lib
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@8 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_lib/src/hrl_lib/util.py
+++ hrl/hrl_lib/src/hrl_lib/util.py
@@ -1,5 +1,36 @@
 import numpy as np
+import pickle as pk
+import time
 
 def list_mat_to_mat(list_mat, axis=0):
     return np.concatenate(tuple(list_mat), axis=axis)
+
+
+## returns current time as a string: year|month|date_hours|min|sec.
+## @return current time as a string: year|month|date_hours|min|sec.
+def formatted_time():
+    date_name = time.strftime('%Y%h%d_%H%M%S', time.localtime())
+#    curtime = time.localtime()
+#    date_name = time.strftime('%Y%m%d_%I%M%S', curtime)
+    return date_name
+
+## read a pickle and return the object.
+# @param filename - name of the pkl
+# @param return - object that had been pickled.
+def load_pickle(filename):
+    p = open(filename, 'r')
+    picklelicious = pk.load(p)
+    p.close()
+    return picklelicious
+
+## Pickle an object.
+# @param object - object to be pickled
+# @param filename - name of the pkl file
+def save_pickle(object, filename):
+    pickle_file = open(filename, 'w')
+    pk.dump(object, pickle_file)
+    pickle_file.close()
+
+
+
 
