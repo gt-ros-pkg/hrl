98950deabde07b00fd64c4f5a4474167f6c13969
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-06-10 17:43:25 +0000

    added ROSAMTI back
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@43 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/force_torque/src/force_torque/ROSAMTIForce.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2009, Georgia Tech Research Corporation
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Georgia Tech Research Corporation nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL GEORGIA TECH BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#  \author Advait Jain (Healthcare Robotics Lab, Georgia Tech.)
+#  \author Cressel Anderson (Healthcare Robotics Lab, Georgia Tech.)
+#  \author Hai Nguyen (Healthcare Robotics Lab, Georgia Tech.)
+
+import roslib; roslib.update_path('force_torque')
+import rospy
+from hrl_lib.msg import FloatArray
+#import hrl_lib.rutils as ru
+
+import AMTIForce2 as af
+
+#from threading import RLock
+#import threading
+import time
+
+#import rutils as ru
+
+class AMTIForceServer(threading.Thread):
+    def __init__(self, device_path, id):
+        threading.Thread.__init__(self)
+        try:
+            rospy.init_node('AMTIForceServer')
+            print 'AMTIForceServer: ros is up!'
+        except rospy.ROSException:
+            pass
+        print 'AMTIForceServer: connecting to', device_path
+        self.force_plate = af.AMTI_force(device_path)
+
+        name = 'force_torque_' + str(id)
+        print 'AMTIForceServer: publishing', name, 'with type FloatArray'
+        self.channel = rospy.Publisher(name, FloatArray, tcp_nodelay=True)
+
+    def broadcast(self):
+        print 'AMTIForceServer: started!'
+        while not rospy.is_shutdown():
+            time.sleep(1/5000.0)
+            self.channel.publish(FloatArray(None, self.force_plate.read().T.tolist()[0]))
+
+#DEPRECATED, use FTClient from ROSFTSensor with id = 0
+#def AMTIForceClient():
+#    return ru.FloatArrayListener('AMTIForceClient', 'force_plate', 100.0)
+
+#import roslib; roslib.update_path('force_torque')
+#import force_torque.ROSAMTIForce as ft
+if __name__ == '__main__':
+    server = ft.AMTIForceServer('/dev/robot/force_plate0', 0)
+    server.broadcast()
+
+
