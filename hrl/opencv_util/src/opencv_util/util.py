9571cf1764a7a794cfc0e0329e6b7e80c90d0169
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-06-23 06:00:42 +0000

    adding new funcs
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@55 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/opencv_util/src/opencv_util/util.py
+++ hrl/opencv_util/src/opencv_util/util.py
@@ -9,6 +9,9 @@ import opencv.adaptors as ad
 import Image as pil
 import numpy as np
 
+##
+# Convert from ROS to OpenCV image datatype
+# @param image to convert
 def ros2cv(image):
     #ros to pil then pil to opencv
     if image.encoding != 'bgr' or image.encoding != 'rgb':
@@ -21,6 +24,74 @@ def ros2cv(image):
     channels  = image.uint8_data.layout.dim[2].size
     np_image  = np.reshape(np.fromstring(image.uint8_data.data, dtype='uint8', count=height*width*channels), [height, width, channels])
     return ad.NumPy2Ipl(np_image)
+
+##
+# Simple drawing of text on an image with a drop shadow
+def text(image, x, y, a_string):
+    font = cvInitFont(CV_FONT_HERSHEY_SIMPLEX, .3, .3)
+    cvPutText(image, a_string, cvPoint(x, y),     font, cvScalar(0,0,0))
+    cvPutText(image, a_string, cvPoint(x+1, y+1), font, cvScalar(255,255,255))
+
+##
+# Tries to clean up segmentation 
+# @param cv_image
+# @param n_times to run erode & dilate
+# @return image in original type with hopefully cleaner segmentations
+def clean_segmentation(cv_image, n_times=1):
+    dst  = cv.cvCloneImage(cv_image)
+    dst2 = cv.cvCloneImage(cv_image)
+    cv.cvErode(cv_image, dst, None, n_times)
+    cv_image = cv.cvDilate(dst, dst2, None, n_times)
+    return dst2
+
+##
+# Mask a color image with a given black and white mask
+# @param img
+# @param img_mask one channeled image
+# @return color image with masked part filled with black
+def mask(img, img_mask):
+    dim      = img.width, img.height
+    depth    = img.depth
+    channels = img.nChannels
+
+    r_chan = cvCreateImage(cvSize(*dim), depth, 1)
+    g_chan = cvCreateImage(cvSize(*dim), depth, 1)
+    b_chan = cvCreateImage(cvSize(*dim), depth, 1)
+    combined = cvCreateImage(cvSize(*dim), depth, 3)
+    cvSplit(img, r_chan, g_chan, b_chan, None)
+
+    cvAnd(r_chan, img_mask, r_chan)
+    cvAnd(g_chan, img_mask, g_chan)
+    cvAnd(b_chan, img_mask, b_chan)
+    cvMerge(r_chan, g_chan, b_chan, None, combined)
+    return combined
+
+##
+# Split a color image into its component parts
+def split(image):
+    img1 = cvCloneImage(image)
+    img2 = cvCloneImage(image)
+    img3 = cvCloneImage(image)
+    cvSplit(image, img1, img2, img3, None)
+    return (img1, img2, img3)
+###
+## Fill holes in a binary image using scipy
+##
+#def fill_holes(cv_img):
+#    img_np     = ut.cv2np(cv_img)
+#    img_binary = img_np[:,:,0]
+#    results    = ni.binary_fill_holes(img_binary)
+#    img_cv     = ut.np2cv(np_mask2np_image(results))
+#    return img_cv
+
+##
+# Easy scaling of an image by factor s
+# @param image
+# @param s
+def scale(image, s):
+    scaled = cv.cvCreateImage(cv.cvSize(int(image.width * s), int(image.height * s)), image.depth, image.nChannels)
+    cv.cvResize(image, scaled, cv.CV_INTER_AREA)
+    return scaled
 
 if __name__ == '__main__':
     import opencv.highgui as hg
