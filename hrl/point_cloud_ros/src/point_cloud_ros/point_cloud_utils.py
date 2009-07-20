b7b7ef28cd4629475d049c3ab51c5aad1b61d8db
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-07-20 22:40:01 +0000

    some changes in point_cloud_utils. Will break Hai's code, sorry Hai.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@87 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/point_cloud_ros/src/point_cloud_ros/point_cloud_utils.py
+++ hrl/point_cloud_ros/src/point_cloud_ros/point_cloud_utils.py
@@ -3,18 +3,28 @@ import robot_msgs.msg as rm
 import numpy as np
 #import psyco
 
-def ros_pts_to_np(ros_pts):
+## PointCloud  -> 3xN np matrix
+# @param ros_pointcloud - robot_msgs/PointCloud
+# @return 3xN np matrix
+def ros_pointcloud_to_np(ros_pointcloud):
     ''' ros PointCloud.pts -> 3xN numpy matrix
     '''
+    return ros_pts_to_np(ros_pointcloud.pts)
+
+## list of Point32 points  -> 3xN np matrix
+# @param ros_points - Point32[ ] (for e.g. from robot_msgs/PointCloud or Polygon3D)
+# @return 3xN np matrix
+def ros_pts_to_np(ros_pts):
     pts_list = []
-    for p in ros_pts.pts:
+    for p in ros_pts:
         pts_list.append([p.x,p.y,p.z])
 
     return np.matrix(pts_list).T
 
+## 3xN np matrix -> ros PointCloud
+# @param pts - 3xN np matrix
+# @return PointCloud as defined in robot_msgs/msg/PointCloud.msg
 def np_points_to_ros(pts):
-    ''' 3xN np matrix -> ros PointCloud
-    '''
     p_list = []
     chlist = []
     for p in pts.T:
@@ -24,3 +34,5 @@ def np_points_to_ros(pts):
     ch = rm.ChannelFloat32('t',chlist)
     pc = rm.PointCloud(None,p_list,[ch])
     return pc
+
+
