60f62aed588e92eb364241d6eeb814cb1af10d6d
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-10-12 01:47:00 +0000

    added new funcs
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@136 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_opencv/src/hrl_opencv/cv.py
+++ hrl/hrl_opencv/src/hrl_opencv/cv.py
@@ -1,6 +1,6 @@
 import roslib
 roslib.update_path('hrl_opencv')
-from sensor_msgs.msg import Image as RosImage
+#from sensor_msgs.msg import Image as RosImage
 from std_msgs.msg import UInt8MultiArray
 from std_msgs.msg import MultiArrayLayout
 from std_msgs.msg import MultiArrayDimension
@@ -11,21 +11,63 @@ import opencv.adaptors as ad
 import Image as pil
 import numpy as np
 
+#################################################################################################################
+# ROS Utility Functions
+#################################################################################################################
+
 ##
 # Convert from ROS to OpenCV image datatype
 # @param image to convert
 def ros2cv(image):
     #ros to pil then pil to opencv
-    if image.encoding != 'bgr' and image.encoding != 'rgb':
+    #if image.encoding != 'bgr' and image.encoding != 'rgb' and image.encoding != 'bgr8':
+    channels = 1
+    if image.encoding != 'bgr8':
         raise RuntimeError('Unsupported format "%s"' % image.encoding)
-    if image.depth != 'uint8':
-        raise RuntimeError('Unsupported depth "%s"' % image.depth)
+    else:
+        channels = 3
+    if image.is_bigendian != 0:
+        raise RuntimeError('Unsupported endianess')
 
-    height    = image.uint8_data.layout.dim[0].size
-    width     = image.uint8_data.layout.dim[1].size
-    channels  = image.uint8_data.layout.dim[2].size
-    np_image  = np.reshape(np.fromstring(image.uint8_data.data, dtype='uint8', count=height*width*channels), [height, width, channels])
-    return ad.NumPy2Ipl(np_image)
+    #print image.encoding
+    #print image.step
+    #print image.is_bigendian
+    #if image.depth != 'uint8':
+    #    raise RuntimeError('Unsupported depth "%s"' % image.depth)
+
+    #height    = image.data.layout.dim[0].size
+    #width     = image.data.layout.dim[1].size
+    #channels  = image.data.layout.dim[2].size
+    #print 'expected', image.width * channels
+    #print 'step', image.step
+    #print 'image.width', image.width, image.height
+    assert(image.width * channels == image.step)
+    height = image.height
+    width  = image.width
+    np_image = np.reshape(np.fromstring(image.data, dtype='uint8', count=height*width*channels), [height, width, 3])
+    #np
+    #np_image[:,:,2].A1
+    #np_image[:,:,1].A1
+    #np_image[:,:,0].A1
+    np_image2 = np.empty(np_image.shape, dtype='uint8')
+    np_image2[:, :, 0] = np_image[:, :, 2]
+    np_image2[:, :, 1] = np_image[:, :, 1]
+    np_image2[:, :, 2] = np_image[:, :, 0]
+
+    #concatenated = np.concatenate([np_image[:,:,2].flatten(), np_image[:,:,1].flatten(), np_image[:,:,0].flatten()], axis=0)
+    #print 2050*2448*3
+    #print concatenated.shape
+    #np_image2 = np.reshape(np.concatenate([np_image[:,:,0].flatten(), np_image[:,:,1].flatten(), np_image[:,:,2].flatten()], axis=0), [3, width, height])
+    #np_image2 = np.swapaxes(np_image2, 0, 2)
+    #print np_image[:,:,2].shape
+    #print 'datatype:', np_image2.dtype
+    #print np_image2.shape
+    return ad.NumPy2Ipl(np_image2)
+    #return None
+
+#################################################################################################################
+# Highgui 
+#################################################################################################################
 
 ##
 # Simple drawing of text on an image with a drop shadow
@@ -34,6 +76,36 @@ def text(image, x, y, a_string):
     cv.cvPutText(image, a_string, cv.cvPoint(x, y),     font, cv.cvScalar(0,0,0))
     cv.cvPutText(image, a_string, cv.cvPoint(x+1, y+1), font, cv.cvScalar(255,255,255))
 
+def clone_image(image):
+    return cv.cvCloneImage(image)
+
+def save_image(name, image):
+    hg.cvSaveImage(name, image)
+
+def show_image(name, image):
+    hg.cvShowImage(name, image)
+
+def named_window(name):
+    hg.cvNamedWindow(name, hg.CV_WINDOW_AUTOSIZE)
+
+def wait_key(msecs=33):
+    return hg.cvWaitKey(msecs)
+
+#################################################################################################################
+# CXCore 
+#################################################################################################################
+
+#################################################################################################################
+# CVAux 
+#################################################################################################################
+
+#################################################################################################################
+# Machine Learning 
+#################################################################################################################
+
+#################################################################################################################
+# CVReference 
+#################################################################################################################
 ##
 # Morphological closing
 def morpho_close(cv_image, n_times=1):
@@ -51,6 +123,20 @@ def morpho_open(cv_image, n_times=1):
     cv.cvErode(cv_image, dst, None, n_times)
     cv.cvDilate(dst, dst2, None, n_times)
     return dst2
+
+##
+# Morphological opening
+def dilate(cv_image, n_times=1):
+    dst  = cv.cvCloneImage(cv_image)
+    cv.cvDilate(cv_img, dst, None, n_times)
+    return dst
+
+##
+# Morphological opening
+def erode(cv_image, n_times=1):
+    dst  = cv.cvCloneImage(cv_image)
+    cv.cvErode(cv_img, dst, None, n_times)
+    return dst
 
 
 ##
@@ -84,16 +170,6 @@ def split(image):
     cv.cvSplit(image, img1, img2, img3, None)
     return (img1, img2, img3)
 
-###
-## Fill holes in a binary image using scipy
-##
-#def fill_holes(cv_img):
-#    img_np     = ut.cv2np(cv_img)
-#    img_binary = img_np[:,:,0]
-#    results    = ni.binary_fill_holes(img_binary)
-#    img_cv     = ut.np2cv(np_mask2np_image(results))
-#    return img_cv
-
 ##
 # Easy scaling of an image by factor s
 # @param image
@@ -125,3 +201,13 @@ if __name__ == '__main__':
 #    layout     = MultiArrayLayout(dim = [d1,d2,d3])
 #    multiarray = UInt8MultiArray(layout=layout, data=data)
 #    return RosImage(label='image', encoding='bgr', depth='uint8', uint8_data=multiarray)
+
+###
+## Fill holes in a binary image using scipy
+##
+#def fill_holes(cv_img):
+#    img_np     = ut.cv2np(cv_img)
+#    img_binary = img_np[:,:,0]
+#    results    = ni.binary_fill_holes(img_binary)
+#    img_cv     = ut.np2cv(np_mask2np_image(results))
+#    return img_cv
