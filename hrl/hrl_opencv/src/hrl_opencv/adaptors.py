1c24f5b781fc0ec314ceac99f8f1295fd30f191b
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2009-11-04 17:00:08 +0000

    added rudimentary set of opencv adaptors
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@161 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_opencv/src/hrl_opencv/adaptors.py
@@ -0,0 +1,56 @@
+##
+# Taken from http://opencv.willowgarage.com/wiki/PythonInterface
+#
+import roslib
+roslib.load_manifest('hrl_opencv')
+import cv
+import Image #PIL
+import numpy as np
+
+def pil2cv(pi):
+    cv_im = cv.CreateImageHeader(pi.size, cv.IPL_DEPTH_8U, 1)
+    cv.SetData(cv_im, pi.tostring())
+    return cv_im
+
+def cv2pil(cv_im):
+    return Image.fromstring(cv.GetSize(cv_im), "L", cv_im.tostring())
+
+def cv2array(im):
+  depth2dtype = {
+        cv.IPL_DEPTH_8U: 'uint8',
+        cv.IPL_DEPTH_8S: 'int8',
+        cv.IPL_DEPTH_16U: 'uint16',
+        cv.IPL_DEPTH_16S: 'int16',
+        cv.IPL_DEPTH_32S: 'int32',
+        cv.IPL_DEPTH_32F: 'float32',
+        cv.IPL_DEPTH_64F: 'float64',
+    }
+  
+  arrdtype=im.depth
+  a = np.fromstring(
+         im.tostring(),
+         dtype=depth2dtype[im.depth],
+         count=im.width*im.height*im.nChannels)
+  a.shape = (im.height,im.width,im.nChannels)
+  return a
+    
+def array2cv(a):
+  dtype2depth = {
+        'uint8':   cv.IPL_DEPTH_8U,
+        'int8':    cv.IPL_DEPTH_8S,
+        'uint16':  cv.IPL_DEPTH_16U,
+        'int16':   cv.IPL_DEPTH_16S,
+        'int32':   cv.IPL_DEPTH_32S,
+        'float32': cv.IPL_DEPTH_32F,
+        'float64': cv.IPL_DEPTH_64F,
+    }
+  try:
+    nChannels = a.shape[2]
+  except:
+    nChannels = 1
+  cv_im = cv.CreateImageHeader((a.shape[1],a.shape[0]), 
+          dtype2depth[str(a.dtype)],
+          nChannels)
+  cv.SetData(cv_im, a.tostring(), 
+             a.dtype.itemsize*nChannels*a.shape[1])
+  return cv_im
