d39853ba8ea51a5a6117c7081d5011187f8f6cf3
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-01-26 15:26:12 +0000

    utilities for publishing video stream across low bandwidth network
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@257 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_camera/src/hrl_camera/theora_listener.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/CvBridge.h>
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  sensor_msgs::CvBridge bridge;
+  try
+  {
+    cvShowImage("view", bridge.imgMsgToCv(msg, "rgb8"));
+  }
+  catch (sensor_msgs::CvBridgeException& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'rgb8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "theora_listener");
+  ros::NodeHandle nh;
+  cvNamedWindow("view");
+  cvStartWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("hrl_compressed_img/image", 1, imageCallback);
+  ros::spin();
+  cvDestroyWindow("view");
+}
+
---
+++ hrl/hrl_camera/src/hrl_camera/theora_pub.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/CvBridge.h>
+  
+
+class Theora_obj
+{
+  public:
+    image_transport::Publisher pub;
+    void callback(const sensor_msgs::ImageConstPtr& msg);
+    void set_pub(image_transport::ImageTransport& it);
+      
+};
+
+void Theora_obj::set_pub(image_transport::ImageTransport& it)
+{
+    pub = it.advertise("hrl_compressed_img/image", 1);
+}
+
+void Theora_obj::callback(const sensor_msgs::ImageConstPtr& msg)
+{
+    pub.publish(msg);
+}
+
+
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "theora_publish");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  Theora_obj t_obj;
+  t_obj.set_pub(it);
+  ros::Subscriber cam_image = nh.subscribe("cvcamera_dummy_deepthought", 1, &Theora_obj::callback, &t_obj);
+
+  ros::spin();
+}
+
+
