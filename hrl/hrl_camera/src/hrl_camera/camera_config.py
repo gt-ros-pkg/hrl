979fd6fb19fcc9e8ef4df8ccfaee380befc4f432
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-02-11 19:04:30 +0000

    added parameters for lab overhead camera
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@264 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_camera/src/hrl_camera/camera_config.py
+++ hrl/hrl_camera/src/hrl_camera/camera_config.py
@@ -26,6 +26,26 @@ camera_parameters = {
             'uid': None 
             },
 
+        'dummy_deepthought':
+        {
+            'calibration_image_width' : 640.0,
+            'calibration_image_height' : 480.0, 
+            'focal_length_x_in_pixels' : 362.381,
+            'focal_length_y_in_pixels' : 362.260,
+            'optical_center_x_in_pixels' : 275.630,
+            'optical_center_y_in_pixels' : 267.914,
+            'lens_distortion_radial_1' :    -0.270544,
+            'lens_distortion_radial_2' :    0.0530850,
+            'lens_distortion_tangential_1' : 0,
+            'lens_distortion_tangential_2' : 0,
+            'opencv_bayer_pattern' :  cv.CV_BayerBG2BGR,
+            'upside_down': False,
+            'color': True,
+            'class': 'firefly',
+            'uid': 7281161
+        },
+
+
 #########################################################################
 # Start of camera on ELE
 #########################################################################
@@ -173,4 +193,27 @@ camera_parameters = {
             'class': 'dragonfly2',
             'uid': 9030543
         }
+
+        'lab_overhead' :
+        {
+            'calibration_image_width' : 1024.0,
+            'calibration_image_height' : 768.0, 
+            'focal_length_x_in_pixels' : 462.794,
+            'focal_length_y_in_pixels' : 462.041,
+            'optical_center_x_in_pixels' : 488.590,
+            'optical_center_y_in_pixels' : 428.419,
+            'lens_distortion_radial_1' : -0.240018,
+            'lens_distortion_radial_2' : 0.0372740,
+            'lens_distortion_tangential_1' : 0,
+            'lens_distortion_tangential_2' : 0,
+            'opencv_bayer_pattern' : None,
+            #whether this camera was mounted upside down
+            'upside_down': True,
+            'color': True,
+            'type': 'Point Grey DragonFly2',
+            #the class to load in a normal python import statement
+            'class': 'dragonfly2',
+            #UUID obtained by calling 'python camera_uuid.py'
+            'uid': None 
+            },
     }
