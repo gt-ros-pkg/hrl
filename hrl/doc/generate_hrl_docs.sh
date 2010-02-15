f8be2d389781da91f6514a49a2ebdb790160249e
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-02-15 19:57:55 +0000

    updated manifests and dox files
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@269 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/doc/generate_hrl_docs.sh
+++ hrl/doc/generate_hrl_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #rosrun rosdoc rosdoc hrl_lib force_torque point_cloud_ros rfid zenither robotis
-rosrun rosdoc rosdoc code_publications  force_torque  hrl_hokuyo  hrl_opencv		 LICENSE.txt	  rfid	   sample_pkg	tele_mobile  zenither doc		   hrl_camera	 hrl_lib     hrl_tilting_hokuyo  point_cloud_ros  robotis  segway_omni	tmp
+rosrun rosdoc rosdoc code_publications  force_torque  hrl_hokuyo  hrl_opencv rfid  tele_mobile  zenither hrl_camera	 hrl_lib     hrl_tilting_hokuyo  point_cloud_ros  robotis  segway_omni	
 
