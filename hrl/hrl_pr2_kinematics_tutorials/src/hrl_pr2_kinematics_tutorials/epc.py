6d21d36d9c06924f2897d31bace5eb64a957c3e5
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-05-22 07:26:06 +0000

    M    hrl_pr2_kinematics_tutorials/hrl_pr2.py A    hrl_pr2_kinematics_tutorials/epc.py things might still move around a lot. added FK into hrl_pr2.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@312 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_pr2_kinematics_tutorials/src/hrl_pr2_kinematics_tutorials/epc.py
@@ -0,0 +1,81 @@
+
+import roslib; roslib.load_manifest('hrl_pr2_kinematics_tutorials')
+import rospy
+
+## Class defining the core EPC function and a few simple examples.
+# More complex behaviors that use EPC should have their own ROS
+# packages.
+class EPC():
+
+    def __init__(self, robot):
+        self.robot = robot
+
+    ##
+    # @param equi_pt_generator: function that returns stop, ea  where ea: equilibrium angles and  stop: string which is '' for epc motion to continue
+    # @param rapid_call_func: called in the time between calls to the equi_pt_generator can be used for logging, safety etc.  returns string which is '' for epc motion to continue
+    # @param time_step: time between successive calls to equi_pt_generator
+    # @param arg_list - list of arguments to be passed to the equi_pt_generator
+    # @return stop (the string which has the reason why the epc
+    # motion stopped.), ea (last commanded equilibrium angles)
+    def epc_motion(self, equi_pt_generator, time_step, arm, arg_list,
+                   rapid_call_func=None):
+
+        stop, ea = equi_pt_generator(*arg_list)
+        t_end = time.time()
+        while stop == '':
+            t_end += time_step
+            self.robot.set_joint_angles(arm, q)
+            # self.robot.step() this should be within the rapid_call_func for the meka arms.
+            t1 = time.time()
+            while t1<t_end:
+                if rapid_call_func != None:
+                    stop = rapid_call_func(arm)
+                    if stop != '':
+                        break
+                # self.robot.step() this should be within the rapid_call_func for the meka arms.
+                t1 = time.time()
+
+            stop, ea = equi_pt_generator(*arg_list)
+
+            if stop == 'reset timing':
+                stop = ''
+                t_end = time.time()
+
+        return stop, ea
+
+    ## Pull back along a straight line (-ve x direction)
+    # @param arm - 'right_arm' or 'left_arm'
+    # @param ea - starting equilibrium angle.
+    # @param rot_mat - rotation matrix defining end effector pose
+    # @param distance - how far back to pull.
+    def pull_back(self, arm, ea, rot_mat, distance):
+        self.cep = self.robot.FK(arm, ea)
+        self.dist_left = distance
+
+        def eq_gen_pull_back(robot, arm, rot_mat):
+            if dist_left <= 0.:
+                return 'done', None
+            step_size = 0.01
+            self.cep[0,0] -= step_size
+            self.dist_left -= step_size
+            ea = robot.IK(arm, self.cep, rot_mat)
+            if ea == None:
+                return 'IK fail', ea
+            return ea, ''
+        
+        arg_list = [self.robot, arm, rot_mat]
+        stop, ea = self.epc_motion(eq_gen_pull_back, 0.1, arg_list)
+
+
+
+
+if __name__ == '__main__':
+    print 'Hello World'
+
+    import hrl_pr2
+    pr2 = hrl_pr2.HRL_PR2()
+    epc = EPC(pr2)
+
+
+
+
--- hrl/hrl_pr2_kinematics_tutorials/src/hrl_pr2_kinematics_tutorials/hrl_pr2.py
+++ hrl/hrl_pr2_kinematics_tutorials/src/hrl_pr2_kinematics_tutorials/hrl_pr2.py
@@ -1,8 +1,13 @@
 
+import numpy as np, math
+
 import roslib; roslib.load_manifest('hrl_pr2_kinematics_tutorials')
 import rospy
 
 import actionlib
+
+from kinematics_msgs.srv import GetKinematicSolverInfo
+from kinematics_msgs.srv import GetPositionFK, GetPositionFKRequest, GetPositionFKResponse
 from pr2_controllers_msgs.msg import JointTrajectoryAction, JointTrajectoryGoal
 from pr2_controllers_msgs.msg import Pr2GripperCommandGoal, Pr2GripperCommandAction
 from trajectory_msgs.msg import JointTrajectoryPoint
@@ -10,6 +15,19 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 class HRL_PR2():
 
     def __init__(self):
+
+        self.joint_names_list = ['r_shoulder_pan_joint',
+                           'r_shoulder_lift_joint', 'r_upper_arm_roll_joint',
+                           'r_elbow_flex_joint', 'r_forearm_roll_joint',
+                           'r_wrist_flex_joint', 'r_wrist_roll_joint']
+
+        rospy.wait_for_service('pr2_right_arm_kinematics/get_fk_solver_info');
+        rospy.wait_for_service('pr2_right_arm_kinematics/get_fk');
+        self.fk_info = rospy.ServiceProxy('pr2_right_arm_kinematics/get_fk_solver_info',
+                                          GetKinematicSolverInfo)
+        self.fk_srv = rospy.ServiceProxy('pr2_right_arm_kinematics/get_fk',
+                                         GetPositionFK)
+
         self.joint_action_client = actionlib.SimpleActionClient('r_arm_controller/joint_trajectory_action', JointTrajectoryAction)
         self.gripper_action_client = actionlib.SimpleActionClient('r_gripper_controller/gripper_action', Pr2GripperCommandAction)
         self.joint_action_client.wait_for_server()
@@ -18,12 +36,10 @@ class HRL_PR2():
     ## go to a joint configuration.
     # @param q - list of 7 joint angles in RADIANS.
     # @param duration - how long (SECONDS) before reaching the joint angles.
-    def go_jointangles(self, q, duration):
+    def set_jointangles(self, arm, q, duration=0.1):
+        rospy.logwarn('Currently ignoring the arm parameter.')
         jtg = JointTrajectoryGoal()
-        jtg.trajectory.joint_names = ['r_shoulder_pan_joint',
-                           'r_shoulder_lift_joint', 'r_upper_arm_roll_joint',
-                           'r_elbow_flex_joint', 'r_forearm_roll_joint',
-                           'r_wrist_flex_joint', 'r_wrist_roll_joint']
+        jtg.trajectory.joint_names = self.joint_names_list
         jtp = JointTrajectoryPoint()
         jtp.positions = q
         jtp.velocities = [0 for i in range(len(q))]
@@ -31,6 +47,28 @@ class HRL_PR2():
         jtp.time_from_start = rospy.Duration(duration)
         jtg.trajectory.points.append(jtp)
         self.joint_action_client.send_goal(jtg)
+
+    def FK(self, arm, q):
+        rospy.logwarn('Currently ignoring the arm parameter.')
+        fk_req = GetPositionFKRequest()
+        fk_req.header.frame_id = 'torso_lift_link'
+        fk_req.fk_link_names.append('r_wrist_roll_link')
+        fk_req.robot_state.joint_state.name = self.joint_names_list
+        fk_req.robot_state.joint_state.position = q
+
+        fk_resp = GetPositionFKResponse()
+        fk_resp = self.fk_srv.call(fk_req)
+        if fk_resp.error_code.val == fk_resp.error_code.SUCCESS:
+            x = fk_resp.pose_stamped[0].pose.position.x
+            y = fk_resp.pose_stamped[0].pose.position.y
+            z = fk_resp.pose_stamped[0].pose.position.z
+            ret = np.matrix([x,y,z]).T
+        else:
+            rospy.logerr('Forward kinematics failed')
+            ret = None
+
+        return ret
+
 
 
 if __name__ == '__main__':
@@ -40,7 +78,9 @@ if __name__ == '__main__':
 
     hrl_pr2 = HRL_PR2()
     q = [0, 0, 0, 0, 0, 0, 0]
-    hrl_pr2.go_jointangles(q, duration = 1.)
+    hrl_pr2.set_jointangles('right_arm', q)
+    ee_pos = hrl_pr2.FK('right_arm', q)
+    print 'FK result:', ee_pos.A1
 
     rospy.spin()
 
