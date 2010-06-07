f42058573cfcce95c6f5b7660b651162082cce66
beambot <beambot@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-06-07 23:06:00 +0000

    A    trial_pub.py A    trial_sub.py
    Trying out something...
    
    
    
    
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@335 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/force_torque/src/force_torque/trial_pub.py
@@ -0,0 +1,16 @@
+import roslib; roslib.load_manifest('hrl_lib')
+import rospy
+import std_srvs.srv as srv
+from hrl_lib.msg import FloatArray
+
+import time
+
+x = 3.0
+rospy.init_node('trial_publisher')
+pub = rospy.Publisher( 'trial_pub', FloatArray)
+while not rospy.is_shutdown():
+    x += 0.1
+    fa = FloatArray()
+    fa.data = [ x, x+1.0, x+2.0 ]
+    pub.publish( fa )
+    time.sleep( 0.3 )
---
+++ hrl/force_torque/src/force_torque/trial_sub.py
@@ -0,0 +1,17 @@
+import roslib; roslib.load_manifest('hrl_lib')
+import rospy
+import std_srvs.srv as srv
+from hrl_lib.msg import FloatArray
+
+import time
+
+def cb( msg ):
+    print 'Received msg: ', msg, '\n\tType: ', msg.__class__
+    return
+
+x = 3.0
+rospy.init_node('trial_subscriber')
+sub = rospy.Subscriber('trial_pub', FloatArray, cb)
+
+while not rospy.is_shutdown():
+    rospy.spin()
