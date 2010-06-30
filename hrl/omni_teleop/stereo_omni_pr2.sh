5922d4ba9f2b33d8aaddf063c972286b44eb58f0
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-06-30 05:52:20 +0000

    added visual force feedback
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@412 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/omni_teleop/stereo_omni_pr2.sh
+++ hrl/omni_teleop/stereo_omni_pr2.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 rxplot -b 20 /accelerometer/l_gripper_motor/samples[0]/x:y:z &
 rxplot -b 20 /accelerometer/r_gripper_motor/samples[0]/x:y:z &
-roslaunch omni_teleop stereo_omni_pr2.launch
+roslaunch omni_teleop c_jt_c1.launch &
+rxplot -b 20 /l_cart/state/F/force/x:y:z &
+rxplot -b 20 /r_cart/state/F/force/x:y:z &
+roslaunch omni_teleop stereo_omni_pr2.launch 
