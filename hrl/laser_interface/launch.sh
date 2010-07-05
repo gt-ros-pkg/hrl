dd7e3fa6b242fbe4f60cd58f92348461fa1b6deb
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-07-05 20:36:36 +0000

    M    laser_interface/launch.sh now providing correct stereo topic in launch files
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@423 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/laser_interface/launch.sh
+++ hrl/laser_interface/launch.sh
@@ -1,5 +1,5 @@
 rosparam load params.yaml
-python nodes/laser_pointer_detector_node.py -r -o 2> /dev/null
+python nodes/laser_pointer_detector_node.py -c wide_stereo_raw -k wide_stereo -r -o 2> /dev/null
 
 
 
