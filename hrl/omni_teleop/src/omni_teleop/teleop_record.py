2f112dd66d1e0d6c832dcfd736e0609a132c7d63
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-07-20 20:19:43 +0000

    AM   omni_teleop/teleop_record.py Script that tries to get the robot's pose.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@495 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/omni_teleop/src/omni_teleop/teleop_record.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('omni_teleop')
+
+import math
+import numpy as np
+import hrl_lib.tf_utils as tfu
+import pdb
+
+import rospy
+import tf
+import tf.transformations as tr
+
+
+class TeleopRecord:
+
+    def __init__(self):
+        self.tflistener = tf.TransformListener()
+        tfu.wait('map', 'base_footprint', self.tflistener, 4.)
+
+    def print_pose(self):
+        try:
+            tfu.wait('map', 'base_footprint', self.tflistener, 1.)
+            m_T_b = tfu.transform('map', 'base_footprint', self.tflistener)
+            t, q = tfu.matrix_as_tf(m_T_b)
+
+            print m_T_b
+            print 't', t, 'q', q
+
+        except Exception, e:
+            print e
+
+if __name__ == '__main__':
+    rospy.init_node('teleop_record')
+    t = TeleopRecord()
+    t.print_pose()
