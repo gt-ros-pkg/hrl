164167f2cb77c2133b959bc0b35e70cbb959778b
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-07-24 00:34:01 +0000

    M    hai_sandbox/test03.py gets images from left, right wide stereo too
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@532 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hai_sandbox/src/hai_sandbox/test03.py
+++ hrl/hai_sandbox/src/hai_sandbox/test03.py
@@ -35,11 +35,48 @@ def find_contact_times(left_mat, right_mat, times):
 
     return loc_r, times_contact
 
+def group_by_first_el(a_list):
+    d = {}
+    for el in a_list:
+        if not d.has_key(el[0]):
+            d[el[0]] = []
+        d[el[0]].append(el)
+    return d
+
+def get_closest_msgs(fname, topics, times):
+    times_set = set(times)
+    for top, msg, t in ru.bag_iter(fname, topics):
+        msg_time = msg.header.stamp.to_time()
+        if len(times_set.intersection([msg_time])) > 0:
+            yield msg
+
+def find_contact_images(bag_name, contact_times, all_times, topic_name):
+    print 'finding closest images for ', topic_name
+    times_tree = sp.KDTree(np.matrix(all_times).T)
+    closest_times = [all_times[times_tree.query([a_time])[1]] for a_time in contact_times]
+    pdb.set_trace()
+    
+    print 'getting & saving images, expecting', len(set(closest_times)), 'images'
+    bridge = CvBridge()
+    cleaned_topic_name = topic_name.replace('/', '')
+    i = 0
+    for ros_msg in get_closest_msgs(bag_name, [topic_name], closest_times):
+        i = i + 1
+        msg_time = ros_msg.header.stamp.to_time() - all_times[0]
+        cv_image = bridge.imgmsg_to_cv(ros_msg, 'bgr8')
+        img_name = "%s_%.3f_touched.png" % (cleaned_topic_name, msg_time)
+        print 'writing', img_name
+        cv.SaveImage(img_name, cv_image)
+    print 'got', i, 'images'
+
 
 fname = sys.argv[1]
+fname_wide = sys.argv[2]
 press_lt = '/pressure/l_gripper_motor'
 press_rt = '/pressure/r_gripper_motor'
-forearm_cam = '/l_forearm_cam/image_rect_color'
+forearm_cam_l = '/l_forearm_cam/image_rect_color'
+ws_l = '/wide_stereo/left/image_rect_color'
+ws_r = '/wide_stereo/right/image_rect_color'
 
 print 'reading pressure messages'
 #Get the pressure messages
@@ -47,9 +84,13 @@ msgs_dict = ru.bag_sel(fname, [press_lt, press_rt])
 
 #Get the image times
 print 'getting image times'
-forearm_cam_times = np.array([msg.header.stamp.to_time() for top, msg, t in ru.bag_iter(fname, [forearm_cam])])
+all_cam_times = group_by_first_el([[top, msg.header.stamp.to_time()] for top, msg, t in ru.bag_iter(fname_wide, [ws_l, ws_r])])
+all_cam_times[forearm_cam_l] = [[top, msg.header.stamp.to_time()] for top, msg, t in ru.bag_iter(fname, [forearm_cam_l])]
 
-print 'processing'
+[msg.header.stamp.to_time() for top, msg, t in ru.bag_iter(fname, ['/wide_stereo/left/image_raw'])][0:4]
+
+
+print 'processing pressure'
 press_lmsgs = [msg for top, msg, t in msgs_dict[press_lt]]
 press_rmsgs = [msg for top, msg, t in msgs_dict[press_rt]]
 
@@ -61,23 +102,12 @@ print 'contact loc', contact_loc
 
 #figure out which images are closest in time
 #note: each row is an instance in KDTrees, query return ([distance], [indices])
-print 'finding closest images'
-forearm_times_tree = sp.KDTree(np.matrix(forearm_cam_times).T)
-closest_times_in_forearm_frames = [forearm_cam_times[forearm_times_tree.query([a_time])[1]] for a_time in times_contact_pressure]
-
-def get_closest_msgs(fname, topics, times):
-    times_set = set(times)
-    for top, msg, t in ru.bag_iter(fname, topics):
-        msg_time = msg.header.stamp.to_time()
-        if len(times_set.intersection([msg_time])) > 0:
-            yield msg
-
-print 'getting & saving images'
-bridge = CvBridge()
-for ros_msg in get_closest_msgs(fname, [forearm_cam], closest_times_in_forearm_frames):
-    msg_time = ros_msg.header.stamp.to_time() - forearm_cam_times[0]
-    cv_image = bridge.imgmsg_to_cv(ros_msg, 'bgr8')
-    cv.SaveImage("%.3f_touched.png" % msg_time, cv_image)
+import pdb
+pdb.set_trace()
+#Maybe just get the range of messages around contact time? +/- a couple of messages? make that a bag?
+find_contact_images(fname,      times_contact_pressure.copy(), [t for top, t in all_cam_times[forearm_cam_l]], forearm_cam_l)
+find_contact_images(fname_wide, times_contact_pressure.copy(), [t for top, t in all_cam_times[ws_l]], ws_l)
+find_contact_images(fname_wide, times_contact_pressure.copy(), [t for top, t in all_cam_times[ws_r]], ws_r)
 
 print 'plotting'
 #Plot readings
