e5dc99ed43d9dae30bb365b70106bbe52651feb8
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-08-05 20:10:30 +0000

    M    pr2_omni_telop/capture_experiment.py now records the needed transforms.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@551 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/pr2_omni_teleop/src/pr2_omni_telop/capture_experiment.py
+++ hrl/pr2_omni_teleop/src/pr2_omni_telop/capture_experiment.py
@@ -7,6 +7,11 @@ import hrl_lib.rutils as ru
 import hrl_lib.util as ut
 import math
 
+#TF
+import tf
+import hrl_lib.tf_utils as tfu
+import tf.transformations as tr
+
 
 if __name__ == '__main__':
     import sys
@@ -14,16 +19,37 @@ if __name__ == '__main__':
     base_name = sys.argv[1]
     test = 'laser'
     ls = ru.LaserScanner('point_cloud_srv')
-    prosilica = rc.Prosilica('prosilica', 'polled')
+    prosilica = rc.Prosilica('prosilica', 'streaming')
+    tf_listener = tf.TransformListener()
+
     rospy.loginfo( 'Getting laser scan.')
     points = ls.scan(math.radians(180.), math.radians(-180.), 20.)
-    rospy.loginfo( 'Getting image')
+    rospy.loginfo('Size of point cloud: %d' % len(points.points))
+    rospy.loginfo( 'Grabbing image.')
     image = prosilica.get_frame()
+    rospy.loginfo( 'Grabbing transforms.')
 
-    rospy.loginfo('saving')
+    #transform from tilt_laser => base_footprint (pointcloud is already in base_footprint)
+    #transform from base_footprint => (pose of head) prosilica
+    pro_T_bf = tfu.transform('/high_def_optical_frame', '/base_footprint', tf_listener)
+
+    #transform from base_footprint => map
+    map_T_bf = tfu.transform('/map', '/base_footprint', tf_listener)
+
+    #get camera's P matrix
+    rospy.loginfo('Waiting for camera_info.')
+    calibration = rc.ROSCameraCalibration('/prosilica/camera_info')
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown() and calibration.has_msg == False:
+        r.sleep()
+
+    rospy.loginfo('Saving.')
     pkl_name = '%s.pkl' % base_name
     img_name = '%s.png' % base_name
-    ut.save_pickle(points,  pkl_name)
+    ut.save_pickle({'points': points, 
+                    'pro_T_bf': pro_T_bf, 
+                    'map_T_bf': map_T_bf, 
+                    'camera_info': calibration},  pkl_name)
     cv.SaveImage(img_name, image)
     rospy.loginfo( 'Saved to %s and %s.' % (pkl_name, img_name))
 
