0c951f82e1498ec8040d1dc9a698c73543264cb4
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-08-05 19:57:59 +0000

    added class to listen to camerainfo
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@550 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_camera/src/hrl_camera/ros_camera.py
+++ hrl/hrl_camera/src/hrl_camera/ros_camera.py
@@ -17,8 +17,13 @@ import cv
 import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 from cv_bridge.cv_bridge import CvBridge, CvBridgeError
 import hrl_lib.rutils as ru
+import polled_camera.srv as ps
+import numpy as np
+import hrl_lib.tf_utils as tfu
+import tf.transformations as tr
 
 class ROSImageClient:
     def __init__(self, topic_name):
@@ -39,17 +44,58 @@ class ROSImageClient:
 
 class Prosilica(ROSImageClient):
     def __init__(self, camera_name, mode):
+        if mode == 'polled':
+            srv_name = '/%s/request_image' % camera_name
+            self.trigger_proxy = rospy.ServiceProxy(srv_name, ps.GetPolledImage)
+
+        self.mode = mode
         self.camera_name = camera_name
-        srv_name = '/%s/request_image' % camera_name
         topic_name = '/%s/image_rect_color' % camera_name
         ROSImageClient.__init__(self, topic_name)
-        self.trigger_proxy = rospy.ServiceProxy(srv_name, ps.GetPolledImage)
 
     def get_frame(self):
-        rq = ps.GetPolledImageRequest()
-        rq.response_namespace = '/%s' % self.camera_name
-        resp = self.trigger_proxy(rq)
+        if self.mode == 'polled':
+            #will get a rospy.service.ServiceException if mode is wrong
+            rq = ps.GetPolledImageRequest()
+            rq.response_namespace = '/%s' % self.camera_name
+            resp = self.trigger_proxy(rq)
         return self.listener.read(allow_duplication=False, willing_to_wait=True, warn=False)
+
+##
+# from camera.py in laser_interface.
+class ROSCameraCalibration:
+    def __init__(self, channel):
+        rospy.Subscriber(channel, CameraInfo, self.camera_info)
+        self.has_msg = False
+
+    def camera_info(self, msg):
+        self.distortion = np.matrix(msg.D)
+        self.K = np.reshape(np.matrix(msg.K), (3,3))
+        self.R = np.reshape(np.matrix(msg.R), (3,3))
+        self.P = np.reshape(np.matrix(msg.P), (3,4))
+        self.w = msg.width
+        self.h = msg.height
+        self.frame = msg.header.frame_id
+        self.has_msg = True
+
+    ##
+    # project 3D point into this camera 
+    #   
+    # @param p 3x1 matrix in given coord frame
+    # @param tf_listener None if transformation not needed
+    # @param from_frame None is default camera frame
+    # @return 2x1 matrix
+    def project(self, p, tf_listener=None, from_frame=None):
+        if not(from_frame == None or from_frame == self.frame):
+            p_cam = tfu.transform(self.frame, from_frame, tf_listener) \
+                           * tfu.tf_as_matrix((p.A1.tolist(), tr.quaternion_from_euler(0,0,0)))
+            trans, q = tfu.matrix_as_tf(p_cam)
+            p = np.matrix(trans).T
+
+        p = np.row_stack((p, np.matrix([1.])))
+        pp = self.P * p
+        pp = pp / pp[2,0]
+        return pp[0:2,0]
 
 
 class ROSCamera(ROSImageClient):
