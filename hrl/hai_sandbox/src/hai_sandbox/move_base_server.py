db79f39c83f7876910332decf2f64ba6f328bfd4
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-09-02 16:32:20 +0000

    AM   hai_sandbox/move_base_server.py simple odometry based base movement behavior used in pr2 strongbot
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@598 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hai_sandbox/src/hai_sandbox/move_base_server.py
@@ -0,0 +1,113 @@
+#! /usr/bin/python
+import roslib; roslib.load_manifest('hai_sandbox')
+import rospy
+import tf.transformations as tr
+import hrl_lib.tf_utils as tfu
+import hrl_lib.util as ut
+import tf
+
+import geometry_msgs.msg as gm
+import trigger_msgs.msg as trm
+import sys
+import math
+import numpy as np
+
+class MoveBase:
+
+    def __init__(self):
+        rospy.init_node('drive')
+        self.tw_pub = rospy.Publisher('base_controller/command', gm.Twist)
+        rospy.Subscriber('simple_move_base', gm.Pose2D, self.pose2d_cb)
+        self.tl = tf.TransformListener()
+
+    def pose2d_cb(self, msg):
+        print 'MESSAGE RECEIVED'
+        if msg.x != 0:
+            rospy.loginfo('go x called')
+            self.go_x(msg.x, msg.y)
+
+        elif msg.theta != 0:
+            rospy.loginfo('go theta called')
+            self.go_ang(msg.theta, msg.y)
+
+    def go_ang(self, ang, speed):
+        dt = math.radians(ang)
+
+        if dt > 0:
+            sign = -1
+        elif dt < 0:
+            sign = 1
+        else:
+            sign = 0
+
+        self.tl.waitForTransform('base_footprint', 'odom_combined', rospy.Time(), rospy.Duration(10))
+        p0_base = tfu.transform('base_footprint', 'odom_combined', self.tl)# \
+        start_ang = tr.euler_from_matrix(p0_base[0:3, 0:3], 'sxyz')[2]
+        r = rospy.Rate(100)
+        dist_so_far = 0.
+
+        last_ang = start_ang
+        while not rospy.is_shutdown():
+            pcurrent_base = tfu.transform('base_footprint', 'odom_combined', self.tl) #\
+            current_ang = tr.euler_from_matrix(pcurrent_base[0:3, 0:3], 'sxyz')[2]
+            #relative_trans = np.linalg.inv(p0_base) * pcurrent_base
+            #relative_ang = math.degrees(tr.euler_from_matrix(relative_trans[0:3, 0:3], 'sxyz')[2])
+            dist_so_far = dist_so_far + (ut.standard_rad(current_ang - last_ang))
+            #print 'dist_so_far %.3f dt %.3f diff %.3f' % (dist_so_far, dt, ut.standard_rad(current_ang - last_ang))
+            if dt > 0 and dist_so_far > dt:
+                rospy.loginfo('stopped! %f %f' % (dist_so_far, dt))
+                break
+            elif dt < 0 and dist_so_far < dt:
+                rospy.loginfo('stopped! %f %f' % (dist_so_far, dt))
+                break  
+            elif dt == 0:  
+                rospy.loginfo('stopped! %f %f' % (dist_so_far, dt))
+                break
+
+            tw = gm.Twist()
+            tw.linear.x = 0
+            tw.linear.y = 0
+            tw.linear.z = 0
+            tw.angular.x = 0
+            tw.angular.y = 0#0#math.radians(10)
+            tw.angular.z = math.radians(speed * sign)
+
+            self.tw_pub.publish(tw)
+            r.sleep()
+            last_ang = current_ang
+
+    def go_x(self, x, speed):
+        vel = speed * np.sign(x)
+        self.tl.waitForTransform('base_footprint', 'odom_combined', rospy.Time(), rospy.Duration(10))
+        p0_base = tfu.transform('base_footprint', 'odom_combined', self.tl)
+        r = rospy.Rate(100)
+
+        while not rospy.is_shutdown():
+            pcurrent_base = tfu.transform('base_footprint', 'odom_combined', self.tl)
+            relative_trans = np.linalg.inv(p0_base) * pcurrent_base
+            dist_moved = np.linalg.norm(relative_trans[0:3,3])
+            print "%s" % str(dist_moved)
+            
+            if dist_moved > np.abs(x):
+                rospy.loginfo('stopped! error %f' % (np.abs(dist_moved-np.abs(x))))
+                break
+
+            tw = gm.Twist()
+            tw.linear.x = vel
+            tw.linear.y = 0
+            tw.linear.z = 0
+            tw.angular.x = 0
+            tw.angular.y = 0
+            tw.angular.z = 0
+
+            self.tw_pub.publish(tw)
+            r.sleep()
+
+if __name__ == '__main__':
+    m = MoveBase()
+    rospy.loginfo('simple move base server up!')
+    rospy.spin()
+    #m.go_ang(-390, 100)
+    #m.go_x(.2, .05)
+
+
