c8c83c69531a50dc9b5158e4d8b580d2709f4a72
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-09-15 19:29:30 +0000

    M    hrl_lib/rutils.py A    hrl_lib/prob.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@618 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_lib/src/hrl_lib/rutils.py
+++ hrl/hrl_lib/src/hrl_lib/rutils.py
@@ -32,9 +32,8 @@ import rospy
 import rosrecord
 import std_srvs.srv as srv
 from hrl_lib.msg import FloatArray
-import tf
-import tf.msg
-import pr2_laser_snapshotter.srv as snp
+#import tf
+#import tf.msg
 
 import time
 import numpy as np
@@ -58,17 +57,17 @@ def ros_to_dict(msg):
         d[f] = val
     return d
 
+###
+## Converts a list of pr2_msgs/PressureState into a matrix
 ##
-# Converts a list of pr2_msgs/PressureState into a matrix
-#
-# @return left mat, right mat, array
-def pressure_state_to_mat(contact_msgs):
-    times = np.array([c.header.stamp.to_time() for c in contact_msgs])
-    left, right = zip(*[[list(c.l_finger_tip), list(c.r_finger_tip)] for c in contact_msgs])
-    
-    left = np.matrix(left).T
-    right = np.matrix(right).T
-    return left, right, times
+## @return left mat, right mat, array
+#def pressure_state_to_mat(contact_msgs):
+#    times = np.array([c.header.stamp.to_time() for c in contact_msgs])
+#    left, right = zip(*[[list(c.l_finger_tip), list(c.r_finger_tip)] for c in contact_msgs])
+#    
+#    left = np.matrix(left).T
+#    right = np.matrix(right).T
+#    return left, right, times
 
 def np_to_pointcloud(points_mat, frame):
     pc = sm.PointCloud()
@@ -88,18 +87,18 @@ def pointcloud_to_np(pc):
         plist.append([p.x, p.y, p.z])
     return np.matrix(plist).T
 
-class LaserScanner:
-    def __init__(self, service):
-        srv_name = '/%s/single_sweep_cloud' % service
-        self.sp = rospy.ServiceProxy(srv_name, snp.BuildCloudAngle)
-
-    def scan_np(self, start, end, duration):
-        resp = self.sp(start, end, duration)
-        return pointcloud_to_np(resp.cloud)
-
-    def scan(self, start, end, duration):
-        resp = self.sp(start, end, duration)
-        return resp.cloud
+#class LaserScanner:
+#    def __init__(self, service):
+#        srv_name = '/%s/single_sweep_cloud' % service
+#        self.sp = rospy.ServiceProxy(srv_name, snp.BuildCloudAngle)
+#
+#    def scan_np(self, start, end, duration):
+#        resp = self.sp(start, end, duration)
+#        return pointcloud_to_np(resp.cloud)
+#
+#    def scan(self, start, end, duration):
+#        resp = self.sp(start, end, duration)
+#        return resp.cloud
 
 ##
 # Iterator function for simplified filtered bag reading. Works with large bags/messages.
@@ -396,16 +395,16 @@ class GenericListener:
                     self.last_msg_returned = reading['msg_id']
                     return reading['message']
 
-class TransformBroadcaster:
-
-    def __init__(self):
-        self.pub_tf = rospy.Publisher("/tf", tf.msg.tfMessage)
-
-    ## send transform as a tfmessage.
-    # @param tf_stamped - object of class TransformStamped (rosmsg show TransformStamped)
-    def send_transform(self,tf_stamped):
-        tfm = tf.msg.tfMessage([tf_stamped])
-        self.pub_tf.publish(tfm)
+#class TransformBroadcaster:
+#
+#    def __init__(self):
+#        self.pub_tf = rospy.Publisher("/tf", tf.msg.tfMessage)
+#
+#    ## send transform as a tfmessage.
+#    # @param tf_stamped - object of class TransformStamped (rosmsg show TransformStamped)
+#    def send_transform(self,tf_stamped):
+#        tfm = tf.msg.tfMessage([tf_stamped])
+#        self.pub_tf.publish(tfm)
 
 
 
