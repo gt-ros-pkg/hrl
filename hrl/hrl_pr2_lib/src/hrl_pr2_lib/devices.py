699454ce81ad65e78de5b59fba445693630a4753
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-09-15 19:32:52 +0000

    A    hrl/hrl_pr2_lib
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@620 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_pr2_lib/src/hrl_pr2_lib/devices.py
@@ -0,0 +1,42 @@
+
+import pr2_laser_snapshotter.srv as snp
+#import tf
+#import tf.msg
+import rospy
+import numpy as np
+
+
+##
+# Converts a list of pr2_msgs/PressureState into a matrix
+#
+# @return left mat, right mat, array
+def pressure_state_to_mat(contact_msgs):
+    times = np.array([c.header.stamp.to_time() for c in contact_msgs])
+    left, right = zip(*[[list(c.l_finger_tip), list(c.r_finger_tip)] for c in contact_msgs])
+    
+    left = np.matrix(left).T
+    right = np.matrix(right).T
+    return left, right, times
+
+class LaserScanner:
+    def __init__(self, service):
+        srv_name = '/%s/single_sweep_cloud' % service
+        self.sp = rospy.ServiceProxy(srv_name, snp.BuildCloudAngle)
+
+    def scan_np(self, start, end, duration):
+        resp = self.sp(start, end, duration)
+        return pointcloud_to_np(resp.cloud)
+
+    def scan(self, start, end, duration):
+        resp = self.sp(start, end, duration)
+        return resp.cloud
+
+#class TransformBroadcaster:
+#    def __init__(self):
+#        self.pub_tf = rospy.Publisher("/tf", tf.msg.tfMessage)
+#
+#    ## send transform as a tfmessage.
+#    # @param tf_stamped - object of class TransformStamped (rosmsg show TransformStamped)
+#    def send_transform(self,tf_stamped):
+#        tfm = tf.msg.tfMessage([tf_stamped])
+#        self.pub_tf.publish(tfm)
