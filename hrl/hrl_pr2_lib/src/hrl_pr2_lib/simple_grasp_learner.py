88f6555f5b32a799a1c90575b1fd1dcfe2f1bea8
kphawkins <kphawkins@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-10-24 01:22:25 +0000

    Updated grasp learner to save grasp configurations.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@653 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_pr2_lib/src/hrl_pr2_lib/simple_grasp_learner.py
+++ hrl/hrl_pr2_lib/src/hrl_pr2_lib/simple_grasp_learner.py
@@ -10,66 +10,89 @@ import actionlib
 
 from hrl_lib.util import save_pickle, load_pickle
 from hrl_lib.transforms import rotX, rotY, rotZ
-# from hrl_lib.rutils import 
-from hrl_pr2_lib.simple_arm_trajectories import SimpleArmTrajectory, LinearArmTrajectory
+from hrl_pr2_lib.pr2_arms import PR2Arms
 
 node_name = "simple_grasp_learner" 
 
 def log(str):
     rospy.loginfo(node_name + ": " + str)
 
-SETUP_POS_PICKLE = "pickles//init_pos_r.pickle"
+SETUP_POS_ANGS = []
 ARM = 0 # right arm
+NUM_X = 4
+NUM_Y = 4
+NUM_N = 6
+RECT = ((0.35, -0.35), (0.8, 0.4))
+GRASP_DIST = 0.30 
+
+def get_xy_list():
+    grasp_xy_list = []
+    for x in np.linspace(RECT[0][0], RECT[1][0], NUM_X):
+        for y in np.linspace(RECT[0][1], RECT[1][1], NUM_Y):
+            grasp_xy_list += [(x,y)]
 
 def save_setup_pos():
-    print "save_setup_pos"
-    slat = LinearArmTrajectory()
-    slat.move_arm(ARM, np.matrix((0.3, 0., -0.1)).T, rotY(1.), 4.)
-    slat.wait_for_arm_completion(0)
-    q = slat.get_joint_angles(0)
-    save_pickle(q, SETUP_POS_PICKLE)
+    arms = PR2Arms()
 
-def grasp_learn(arm=ARM, hover_z=0.4, num_x=10, num_y=10, num_n=6,
-                        rect=((0.4,-0.4),(0.8,0.4)), table_z=-0.3):
-    slat = LinearArmTrajectory()
-
-    grasp_xy_list = []
-    for x in np.linspace(rect[0][0], rect[1][0], num_x):
-        for y in np.linspace(rect[0][1], rect[1][1], num_y):
-            grasp_xy_list += [(x,y)]
+    grasp_xy_list = get_xy_list()
+    grasp_configs = []
 
     for xy in grasp_xy_list:
         # Move to setup position in the middle of the grasp space
-        q = load_pickle(SETUP_POS_PICKLE)
-        slat.set_joint_angles(arm, q, 3.)
-        rospy.sleep(4.5)
+        arms.set_joint_angles(ARM, SETUP_POS_ANGS, 4.)
+        arms.wait_for_arm_completion(ARM)
+        rospy.sleep(2.)
 
-        # Do grasping num_n times
-        for i in range(num_n):
+        if arms.can_move_arm(ARM, [xy[0], xy[1], HOVER_Z], rotY(np.pi / 2.))
+            print "Moving to pos (%1.2f, %1.2f)" % xy
+            arms.move_arm(ARM, [xy[0], xy[1], HOVER_Z], rotY(np.pi / 2.), 4.)
+            arms.wait_for_arm_completion(ARM)
+            rospy.sleep(2.)
+            angs = arms.get_joint_angles(ARM)
+            grasp_configs += [(xy, angs)]
+        else:
+            print "Can't move to to pos (%1.2f, %1.2f)" % xy
+            grasp_configs += [(xy, None)]
 
-            # Move arm to current grasp position
-            self.slat.move_arm(arm, [xy[0], xy[1], hover_z], 2.)
-            self.slat.wait_for_arm_completion(arm)
-            rospy.sleep(1.5)
-        
-            # TODO Begin monitoring sensors
-            
-            grasp_dist = hover_z - table_z
-            # move arm down
-            dur = self.slat.smooth_linear_move_arm(arm, grasp_dist, (0.,0.,-1.),  max_jerk=0.5)
-            self.slat.wait_for_arm_completion(arm)
-            rospy.sleep(1.5)
+    save_pickle(grasp_configs, GRASP_CONFIGS_FILE)
+    print "Configurations:", grasp_configs
 
-            # TODO End monitoring sensors
-
-            # move arm up
-            dur = self.slat.smooth_linear_move_arm(arm, grasp_dist, (0.,0.,1.),  max_jerk=0.5)
-            self.slat.wait_for_arm_completion(arm)
-            rospy.sleep(1.5)
-
-        # TODO Find statistics for sensors
-
-    # TODO save statistics for sensors
+# def grasp_learn():
+#     slat = LinearArmTrajectory()
+# 
+# 
+#     for xy in grasp_xy_list:
+#         # Move to setup position in the middle of the grasp space
+#         q = load_pickle(SETUP_POS_PICKLE)
+#         slat.set_joint_angles(ARM, q, 3.)
+#         rospy.sleep(4.5)
+# 
+#         # Do grasping num_n times
+#         for i in range(num_n):
+# 
+#             # Move arm to current grasp position
+#             self.slat.move_arm(ARM, [xy[0], xy[1], hover_z], 2.)
+#             self.slat.wait_for_arm_completion(ARM)
+#             rospy.sleep(1.5)
+#         
+#             # TODO Begin monitoring sensors
+#             
+#             grasp_dist = hover_z - table_z
+#             # move arm down
+#             dur = self.slat.smooth_linear_move_arm(arm, grasp_dist, (0.,0.,-1.),  max_jerk=0.5)
+#             self.slat.wait_for_arm_completion(arm)
+#             rospy.sleep(1.5)
+# 
+#             # TODO End monitoring sensors
+# 
+#             # move arm up
+#             dur = self.slat.smooth_linear_move_arm(arm, grasp_dist, (0.,0.,1.),  max_jerk=0.5)
+#             self.slat.wait_for_arm_completion(arm)
+#             rospy.sleep(1.5)
+# 
+#         # TODO Find statistics for sensors
+# 
+#     # TODO save statistics for sensors
             
 
 def main():
