8a42d668d78a2d9b67a85f426f90565b7708875a
kphawkins <kphawkins@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-11-08 20:21:37 +0000

    Grasp working with perception.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@669 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_pr2_lib/src/hrl_pr2_lib/simple_grasp_learner.py
+++ hrl/hrl_pr2_lib/src/hrl_pr2_lib/simple_grasp_learner.py
@@ -12,13 +12,17 @@ from geometry_msgs.msg import  Point, Pose, Quaternion, PoseStamped
 
 from hrl_lib.util import save_pickle, load_pickle
 from hrl_lib.transforms import rotX, rotY, rotZ
-from tf.transformations import quaternion_about_axis, quaternion_matrix, quaternion_multiply
+from tf.transformations import *
 from hrl_pr2_lib.pr2_arms import PR2Arms
 from hrl_pr2_lib.perception_monitor import ArmPerceptionMonitor
 
-from pr2_gripper_reactive_approach.controller_manager import ControllerManager
+# from pr2_gripper_reactive_approach.controller_manager import ControllerManager
+from hrl_pr2_lib.hrl_controller_manager import HRLControllerManager as ControllerManager
 from pr2_controllers_msgs.msg import JointTrajectoryGoal
 import object_manipulator.convert_functions as cf
+from object_manipulator.cluster_bounding_box_finder import ClusterBoundingBoxFinder
+from tabletop_object_detector.srv import TabletopDetection
+from tabletop_object_detector.msg import TabletopDetectionResult
 
 node_name = "simple_grasp_learner" 
 
@@ -32,19 +36,23 @@ if ARM == 0:
     armc = 'r'
 else:
     armc = 'l'
-NUM_X = 4#7
-NUM_Y = 4#20
-NUM_N = 10
-RECT = ((0.55, 0.10), (0.7, -0.10)) #((0.4, -0.77), (0.75, 0.23))
+NUM_X = 5#7 #4#7
+NUM_Y = 5#9 #4#20
+NUM_N = 3 #4
+RECT = ((0.45, 0.10), (0.65, -0.10))#((0.4, 0.20), (0.7, -0.20))#((0.4, 0.35), (0.8, -0.35)) #((0.55, 0.10), (0.7, -0.10)) #((0.4, -0.77), (0.75, 0.23))
 HOVER_Z = -0.10
 MAX_JERK = 0.2
 GRASP_DIST = 0.30 
 GRASP_TIME = 2.0
-STD_DEV = 1.9
+STD_DEV = 3.5 #1.9
 NOISE_DEV = 0.2
 GRIPPER_POINT = np.array([0.23, 0.0, 0.0])
+JOINTS_BIAS = [0., -.25, -1., 0., 0., 0.5, 0.]
+BIAS_RADIUS = 0.012
+DETECT_ERROR = -0.02
 GRASP_CONFIGS_FILE = "pickles//grasp_configs.pickle"
 GRASP_DATA_FILE = "pickles//grasp_data.pickle"
+GRASP_MODELS_FILE = "pickles//grasp_models.pickle"
 ZEROS_FILE = "pickles//current_zeros.pickle"
 MONITOR_WINDOW = 200
 PERCEPT_MON_LIST = ["accelerometer"]
@@ -93,12 +101,14 @@ def move_to_grasp_pos(cm, grasp_pose, block=True):
     cm.move_cartesian_ik(grasp_pose, collision_aware = False, blocking = block,
                       step_size = .005, pos_thres = .02, rot_thres = .1,
                       settling_time = rospy.Duration(1.0))
+                      # joints_bias = JOINTS_BIAS, bias_radius = BIAS_RADIUS)
 
 def downward_grasp(cm, goal_pose, block=True):
     goal_pose.header.stamp = rospy.Time.now()
-    cm.move_cartesian_ik(goal_pose, collision_aware = False, blocking = block,
+    return cm.move_cartesian_ik(goal_pose, collision_aware = False, blocking = block,
                       step_size = .005, pos_thres = .02, rot_thres = .1,
-                      settling_time = rospy.Duration(GRASP_TIME))
+                      settling_time = rospy.Duration(GRASP_TIME),
+                      joints_bias = JOINTS_BIAS, bias_radius = BIAS_RADIUS)
 
 def save_grasp_configurations():
     arms = PR2Arms()
@@ -108,13 +118,13 @@ def save_grasp_configurations():
     setup = False
 
     for xy in grasp_xy_list:
-        if not setup:
-            # Move to setup position in the middle of the grasp space
-            print "Setting up"
-            arms.set_joint_angles(ARM, SETUP_POS_ANGS, 3.)
-            arms.wait_for_arm_completion(ARM)
-            rospy.sleep(0.5)
-            setup = True
+#       if not setup:
+#           # Move to setup position in the middle of the grasp space
+#           print "Setting up"
+#           arms.set_joint_angles(ARM, SETUP_POS_ANGS, 3.)
+#           arms.wait_for_arm_completion(ARM)
+#           rospy.sleep(0.5)
+#           setup = True
 
         if arms.can_move_arm(ARM, [xy[0], xy[1], HOVER_Z], rotY(np.pi / 2.)):
             print "Moving to pos (%1.2f, %1.2f)" % xy
@@ -149,7 +159,7 @@ def trim_bad_grasps(filename):
     print_configs(new_configs)
     save_pickle(new_configs, filename)
 
-def collect_grasp_data(grasp_configs, generate_models=False):
+def collect_grasp_data(grasp_configs, generate_models=False, use_config=True):
     cm = ControllerManager(armc)
     apm = ArmPerceptionMonitor(ARM)
 
@@ -166,9 +176,13 @@ def collect_grasp_data(grasp_configs, generate_models=False):
             # Do grasping num_n times
             for i in range(NUM_N):
                 # Move to grasp position
-                print "Moving to grasp position"
-                cm.command_joint(config[1])
-                cm.wait_joint_trajectory_done()
+                print "Moving to grasp position (%1.2f, %1.2f)" % config[0]
+                if use_config:
+                    cm.command_joint(config[1])
+                    cm.wait_joint_trajectory_done()
+                else:
+                    grasp_pose = create_goal_pose(config[0][0], config[0][1], HOVER_Z)
+                    move_to_grasp_pos(cm, grasp_pose, block=False)
                 rospy.sleep(0.5)
                 if zeros is None:
                     # wait for a bit to get the zeros here
@@ -180,16 +194,20 @@ def collect_grasp_data(grasp_configs, generate_models=False):
                 apm.start_training()
                 # move arm down
                 print "Moving arm down"
-                downward_grasp(cm, goal_pose, block=True)
+                result = downward_grasp(cm, goal_pose, block=True)
+                print result
                 print "Finished moving arm"
                 apm.stop_training()
                 rospy.sleep(0.5)
 
-            if generate_models:
+            if result != "no solution" and generate_models:
                 models = apm.generate_models()
             else:
                 models = None
-            grasp_data += [(config[0], config[1], apm.datasets, models, zeros)]
+            if result != "no solution":
+                grasp_data += [(config[0], config[1], apm.datasets, models, zeros)]
+            else:
+                print "Grasp Failed, not adding data"
             apm.clear_vars()
         # else:
             # grasp_data += [(config[0], None, None, None, None)]
@@ -198,11 +216,14 @@ def collect_grasp_data(grasp_configs, generate_models=False):
 
 def load_data_and_generate(grasp_data):
     ret = []
+    n = 0
     for grasp in grasp_data:
         apm = ArmPerceptionMonitor(ARM, percept_mon_list=PERCEPT_MON_LIST)
         apm.datasets = grasp[2]
         apm.generate_models()
         ret += [(grasp[0], grasp[1], grasp[2], apm.models, grasp[4])]
+        n += 1
+        print "Generated %d models" % (n)
     return ret
 
 def test_monitor(grasp_data):
@@ -225,6 +246,12 @@ def save_current_zeros(filename=ZEROS_FILE):
 
 def load_current_zeros(filename=ZEROS_FILE):
     return load_pickle(filename)
+
+def open_gripper(cm=None):
+    if cm is None:
+        cm = ControllerManager(armc)
+    cm.command_gripper(0.08, -1.0, False)
+    cm.gripper_action_client.wait_for_result(rospy.Duration(4.0))
 
 def perform_grasp(grasp_data, x, y, z=None, gripper_rot = np.pi / 2., is_place=False, 
                   cm=None, apm=None):
@@ -261,7 +288,7 @@ def perform_grasp(grasp_data, x, y, z=None, gripper_rot = np.pi / 2., is_place=F
     cm.wait_joint_trajectory_done()
     rospy.sleep(0.5)
 
-    zeros = apm.get_zeros(0.5)
+    zeros = apm.get_zeros(1.0)
     print "Current zeros:", zeros
 
     goal_pose = create_goal_pose(x, y, HOVER_Z - GRASP_DIST, gripper_rot)
@@ -295,6 +322,7 @@ def perform_grasp(grasp_data, x, y, z=None, gripper_rot = np.pi / 2., is_place=F
                     cm.joint_action_client.cancel_all_goals()
                     rospy.sleep(0.01)
                 break
+            rospy.sleep(0.01)
 
     print "Finished moving arm"
 
@@ -350,9 +378,10 @@ def perform_grasp(grasp_data, x, y, z=None, gripper_rot = np.pi / 2., is_place=F
 
 def display_grasp_data(grasp_data, percept="accelerometer", indicies=range(3), 
                        std_dev=STD_DEV, noise_dev_add=NOISE_DEV, monitor_data=[], 
-                       plot_data=False, rows=2):
+                       plot_data=False, rows=2, colors=None):
     import matplotlib.pyplot as plt
-    colors = ['r', 'b', 'g', 'c']
+    if colors is None:
+        colors = ['r', 'b', 'g', 'c']
     j = 0
     cols = len(indicies)
     for i in range(rows):
@@ -411,29 +440,123 @@ def display_grasp_data(grasp_data, percept="accelerometer", indicies=range(3),
 def die():
     sys.exit()
 
+def test_num_samples(percept="accelerometer"):
+    # This function proves that adding more than 3-4 grasps per location is unnecessary
+    # Grasping trajectories are very repeatable given a specific grasping location.
+    # The issue comes from more from shifting of the actual trajectory from expected
+    # given a grasping location just a few cm off.  The models can handle the trajectory
+    # so long as it is not much more than 5cm away from the training model.
+
+    grasp_data = load_pickle(GRASP_DATA_FILE)[0:1]
+    data_list = grasp_data[0][2][percept]
+    for i in [3, 6, 9]:
+        apm = ArmPerceptionMonitor(ARM, percept_mon_list=PERCEPT_MON_LIST)
+        grasp_data[0][2][percept] = data_list[0:i+1]
+        apm.datasets = grasp_data[0][2]
+        apm.generate_models()
+        grasp_data[0][3][percept] = apm.models[percept]
+        if i != 3:
+            colors = ['m', 'y', 'k', 'r']
+        else:
+            colors = None
+        display_grasp_data(grasp_data, percept, rows=1)
+
+def detect_tabletop_objects():
+    cbbf = ClusterBoundingBoxFinder()
+    object_detector = rospy.ServiceProxy("/object_detection", TabletopDetection)
+    detects = object_detector(True, False).detection
+    object_detector.close()
+    if detects.result != 4:
+        print "Detection failed (err %d)" % (detects.result)
+    table_z = detects.table.pose.pose.position.z
+    objects = []
+    for cluster in detects.clusters:
+        (object_points, 
+         object_bounding_box_dims, 
+         object_bounding_box, 
+         object_to_base_link_frame, 
+         object_to_cluster_frame) = cbbf.find_object_frame_and_bounding_box(cluster)
+        # print "bounding box:", object_bounding_box
+        (object_pos, object_quat) = cf.mat_to_pos_and_quat(object_to_cluster_frame)
+        angs = euler_from_quaternion(object_quat)
+        print "angs:", angs
+        # position is half of height
+        object_pos[2] = table_z + object_bounding_box[1][2] / 2. + DETECT_ERROR
+        print "pos:", object_pos
+        print "table_z:", table_z
+        objects += [[object_pos, angs[2]]]
+    return objects
+
+def grasp_closest_object(grasp_data, x, y, cm=None, apm=None):
+    objects = detect_tabletop_objects()
+    if len(objects) > 0:
+        def dist(o):
+            return (o[0][0] - x) ** 2 + (o[0][1] - y) ** 2
+        obj = min(objects, key=dist)
+        return perform_grasp(grasp_data, obj[0][0], obj[0][1], #z=obj[0][2], 
+                             gripper_rot=obj[1], cm=cm, apm=apm)
+    else:
+        print "No objects near point"
+        return None
+
+def trim_test_data():
+    grasp_data = load_pickle(GRASP_DATA_FILE)
+    new_data = []
+    for grasp in grasp_data:
+        new_data += [(grasp
+
 def main():
     rospy.init_node(node_name)
-    rospy.on_shutdown(die)
+    # rospy.on_shutdown(die)
 
+    cm = ControllerManager(armc)
+    apm = ArmPerceptionMonitor(ARM, percept_mon_list=PERCEPT_MON_LIST)
+    # print detect_tabletop_objects()
+    # return 0
+
+    # get_setup_pos_angs()
+    # print_configs(load_pickle(GRASP_CONFIGS_FILE))
+
+    # save_grasp_configurations()
+    # return 0
+    # import random
+    # data = load_pickle(GRASP_CONFIGS_FILE)
+    # random.shuffle(data)
+    # collect_grasp_data(data)
+    # return 0
+    # data = load_pickle(GRASP_CONFIGS_FILE)
+    # new_data = []
+    # xys = get_xy_list()
+    # for grasp in data:
+    #     for xy in xys:
+    #         if np.allclose(xy, grasp[0]):
+    #             new_data += [grasp]
+
+    # print "length:", len(new_data)
+    # collect_grasp_data(new_data)
+    # collect_grasp_data(load_pickle(GRASP_CONFIGS_FILE))
+    # return 0
     # trim_bad_grasps("gcb2.pickle")
     # return 0
     # save_current_zeros()
     # return 0
+    # test_num_samples()
+    # return 0
 
-    # get_setup_pos_angs()
-    # save_grasp_configurations()
-    # print_configs(load_pickle(GRASP_CONFIGS_FILE))
-    # collect_grasp_data(load_pickle(GRASP_CONFIGS_FILE)[0:1])
-    grasp_data = load_pickle(GRASP_DATA_FILE)
-    print "Generating data"
-    # grasp_data = load_data_and_generate(load_pickle(GRASP_DATA_FILE)[0:1])
+    # print "Generating data"
+    # grasp_data = load_data_and_generate(load_pickle(GRASP_DATA_FILE))
     # save_pickle(grasp_data, GRASP_DATA_FILE)
+    # print "Done generating data"
+    # return 0
+
+    grasp_data = load_pickle(GRASP_DATA_FILE)
     
+    monitor_data = []
+
+    monitor_data += [grasp_closest_object(grasp_data, 0.48, 0.07, cm=cm, apm=apm)]
     # grasp_data = load_pickle(GRASP_DATA_FILE)
-    print "Done generating data"
     # print grasp_data[0][0]
     # monitor_data = perform_template_grasp(grasp_data[0])
-    monitor_data = []
     # display_grasp_data(grasp_data, "accelerometer", monitor_data=monitor_data, rows=1)
     # display_grasp_data(grasp_data, "r_finger_periph_pressure", rows=1)
     # monitor_data = perform_grasp(grasp_data, .58, .14, gripper_rot = 0.)
@@ -442,16 +565,16 @@ def main():
     # monitor_data += [perform_grasp(grasp_data, .56, .10, gripper_rot = 0.)]
     # monitor_data += [perform_grasp(grasp_data, .57, .10, gripper_rot = 0.)]
     # monitor_data += [perform_grasp(grasp_data, .58, .10, gripper_rot = 0.)]
-    cm = ControllerManager(armc)
-    apm = ArmPerceptionMonitor(ARM, percept_mon_list=PERCEPT_MON_LIST)
+
     # for i in range(2):
     #     monitor_data += [perform_grasp(grasp_data, .55, .10, gripper_rot = 0., cm=cm, apm=apm)]
     #     monitor_data += [perform_grasp(grasp_data, .55, .15, gripper_rot = 0., is_place=True, cm=cm, apm=apm)]
     #     monitor_data += [perform_grasp(grasp_data, .55, .15, gripper_rot = 0., cm=cm, apm=apm)]
     #     monitor_data += [perform_grasp(grasp_data, .55, .10, gripper_rot = 0., is_place=True, cm=cm, apm=apm)]
-    monitor_data += [perform_grasp(grasp_data, .55, .10, z=HOVER_Z-0.14, gripper_rot = 0., is_place=False, cm=cm, apm=apm)]
+    # monitor_data += [perform_grasp(grasp_data, .55, .10, z=HOVER_Z-0.14, gripper_rot = 0., is_place=False, cm=cm, apm=apm)]
     display_grasp_data(grasp_data, "accelerometer", monitor_data=monitor_data, rows=1)
     raw_input()
+    open_gripper(cm=cm)
     # test_monitor(load_pickle(GRASP_DATA_FILE))
     return 0
 
