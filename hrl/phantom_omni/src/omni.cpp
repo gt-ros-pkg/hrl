20ad72f0cb7e64dab8d2a2d87ab5395498d12b1d
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2010-12-13 14:03:04 +0000

    trying to add damping, this revision includes a velocity estimate for end efffector
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@774 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/phantom_omni/src/omni.cpp
+++ hrl/phantom_omni/src/omni.cpp
@@ -19,11 +19,16 @@
 #include "phantom_omni/PhantomButtonEvent.h"
 #include <pthread.h>
 
+float prev_time;
+
 struct OmniState
 {
     hduVector3Dd position; //3x1 vector of position
+    hduVector3Dd velocity; //3x1 vector of velocity
+    hduVector3Dd pos_hist1; //3x1 history of position 
+    hduVector3Dd pos_hist2; //3x1 history of position
     hduVector3Dd rot;
-	hduVector3Dd joints;
+    hduVector3Dd joints;
     hduVector3Dd force; //3 element double vector force[0], force[1], force[2]
     float thetas[7];
     int buttons[2];
@@ -84,6 +89,20 @@ class PhantomROS {
         }
 
         state = s;
+	state.buttons[0] = 0;
+	state.buttons[1] = 0;
+	state.buttons_prev[0] = 0;
+	state.buttons_prev[1] = 0;
+	state.velocity[0] = 0;
+	state.velocity[1] = 0;
+	state.velocity[2] = 0;
+	state.pos_hist1[0] = 0;
+	state.pos_hist1[1] = 0;
+	state.pos_hist1[2] = 0;
+	state.pos_hist2[0] = 0;
+	state.pos_hist2[1] = 0;
+	state.pos_hist2[2] = 0;
+
     }
 
     /*******************************************************************************
@@ -169,6 +188,7 @@ HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
 {
     OmniState *omni_state = static_cast<OmniState *>(pUserData);
 
+    
 	hdBeginFrame(hdGetCurrentDevice());
 	//hdGetDoublev(HD_CURRENT_TRANSFORM,     omni_state->omni_mx);  Wrong
     //hdGetDoublev(HD_CURRENT_VELOCITY, omni_state>vel);            Too lagged
@@ -178,6 +198,11 @@ HDCallbackCode HDCALLBACK omni_state_callback(void *pUserData)
 	hdGetDoublev(HD_CURRENT_POSITION,      omni_state->position);
 	hdGetDoublev(HD_CURRENT_JOINT_ANGLES,  omni_state->joints);
 	hdSetDoublev(HD_CURRENT_FORCE,         omni_state->force);  
+
+	omni_state->velocity = 1/0.02 * (omni_state->position*3 - 4*omni_state->pos_hist1 + omni_state->pos_hist2);
+	omni_state->pos_hist2 = omni_state->pos_hist1;
+	omni_state->pos_hist1 = omni_state->position;
+	printf("position x, y, z: %f %f %f \n", omni_state->position[0], omni_state->position[1], omni_state->position[2]);
 
     //Get buttons
     int nButtons = 0;
@@ -265,7 +290,6 @@ int main(int argc, char** argv)
    // Init Phantom
    ////////////////////////////////////////////////////////////////
    HDErrorInfo error;
-
    HHD hHD;
    hHD = hdInitDevice(HD_DEFAULT_DEVICE);
    if (HD_DEVICE_ERROR(error = hdGetError())) 
@@ -277,6 +301,15 @@ int main(int argc, char** argv)
 
    ROS_INFO("Found %s.\n\n", hdGetString(HD_DEVICE_MODEL_TYPE));
    hdEnable(HD_FORCE_OUTPUT);
+   ////////////////////////////////////////////////////////////////
+   // Init ROS 
+   ////////////////////////////////////////////////////////////////
+   ros::init(argc, argv, "omni_haptic_node");
+   OmniState state;
+   PhantomROS omni_ros;
+   omni_ros.init(&state);
+   hdScheduleAsynchronous(omni_state_callback, &state, HD_MAX_SCHEDULER_PRIORITY);
+
    hdStartScheduler(); 
    if (HD_DEVICE_ERROR(error = hdGetError()))
    {
@@ -285,18 +318,15 @@ int main(int argc, char** argv)
    }
    HHD_Auto_Calibration();
 
-   ////////////////////////////////////////////////////////////////
-   // Init ROS 
-   ////////////////////////////////////////////////////////////////
-   ros::init(argc, argv, "omni_haptic_node");
-   OmniState state;
-   PhantomROS omni_ros;
-   state.buttons[0] = 0;
-   state.buttons[1] = 0;
-   state.buttons_prev[0] = 0;
-   state.buttons_prev[1] = 0;
-   omni_ros.init(&state);
-   hdScheduleAsynchronous(omni_state_callback, &state, HD_MAX_SCHEDULER_PRIORITY);
+   // ////////////////////////////////////////////////////////////////
+   // // Init ROS 
+   // ////////////////////////////////////////////////////////////////
+   // ros::init(argc, argv, "omni_haptic_node");
+   // OmniState state;
+   // PhantomROS omni_ros;
+
+   // omni_ros.init(&state);
+   // hdScheduleAsynchronous(omni_state_callback, &state, HD_MAX_SCHEDULER_PRIORITY);
 
    ////////////////////////////////////////////////////////////////
    // Loop and publish 
@@ -313,177 +343,3 @@ int main(int argc, char** argv)
 }
 
 
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///**
-// * Test this.
-// */
-//void construct_transform(tf::Transform *transform, double theta, double alpha, double a, double d)
-//{
-//    double c_alpha = cos(alpha);
-//    double s_alpha = sin(alpha);
-//    double c_theta = cos(theta);
-//    double s_theta = sin(theta);
-//
-//    transform->setOrigin(tf::Vector3(a, -s_alpha*d, c_alpha*d));
-//    transform->getBasis().setValue(c_theta, -s_theta, 0,
-//                                   c_alpha*s_theta, c_alpha*c_theta, -s_alpha,
-//                                   s_alpha*s_theta, s_alpha*c_theta, c_alpha);
-//}
-//
-//tf::Transform angles_to_transform(float *thetas)
-//{
-//    tf::Transform B_0_6 = btTransform::getIdentity();
-//    double pi = M_PI;
-//    double a[]     = {0.,  0.,    131., 0.,     0.,     0.,      0.};
-//    double alpha[] = {0., -pi/2., 0.,  -pi/2., -pi/2., -pi/2.,   0};
-//    double d[]     = {0., -137., -17.8, 17.8,  -131.,   0.,      38.7};
-//    double t_off[] = {0.,  0.,    0.,   0.,     pi,     (3/2.)*pi, pi};
-//
-//    for (int i = 0; i < 6; i++)
-//    {
-//        tf::Transform t;
-//        construct_transform(&t, t_off[i+1]+thetas[i+1], alpha[i], a[i],  d[i+1]);
-//        B_0_6 = B_0_6 * t;
-//    }
-//    return B_0_6;
-//}
-
-
-        //tf::Transform link;
-        //link.setOrigin(tf::Vector3(0., 0, 0.15));
-        //link.setRotation(tf::Quaternion(0, 0, 0));
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), "world", link_names[0].c_str()));
-
-        //link.setOrigin(tf::Vector3(0., 0, 0));
-        //link.setRotation(tf::Quaternion(-M_PI/2, 0, M_PI/2));
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), "world", "sensable"));
-
-        //link.setOrigin(tf::Vector3(0., 0, 0.));
-        //link.setRotation(tf::Quaternion(-state->thetas[1], 0, 0));
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[0].c_str(), link_names[1].c_str()));
-        //                                                                                                  
-        //link.setOrigin(tf::Vector3(0., 0, 0.));                                                           
-        //link.setRotation(tf::Quaternion(0, state->thetas[2], 0));                                         
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[1].c_str(), link_names[2].c_str()));
-        //                                                                                                  
-        //link.setOrigin(tf::Vector3(-.131, 0, 0.));                                                        
-        //link.setRotation(tf::Quaternion(0, state->thetas[3], 0));                            
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[2].c_str(), link_names[3].c_str()));
-        //                                                                                                  
-        //link.setOrigin(tf::Vector3(0., 0, -.137));                                                        
-        //link.setRotation(tf::Quaternion(state->thetas[4]+M_PI, 0, 0));                       
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[3].c_str(), link_names[4].c_str()));
-        //                                                                                                  
-        //link.setOrigin(tf::Vector3(0., 0., 0.));                                                          
-        //link.setRotation(tf::Quaternion(0., -state->thetas[5]+M_PI,0));                      
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[4].c_str(), link_names[5].c_str()));
-
-        //link.setOrigin(tf::Vector3(0., 0., 0.));
-        //link.setRotation(tf::Quaternion(0.,0, state->thetas[6]+M_PI));
-        //br.sendTransform(tf::StampedTransform(link, ros::Time::now(), link_names[5].c_str(), link_names[6].c_str()));
-
-
-
-
-
-
-        //std::string l1_str("_link1")
-        //std::string l1_str("_link2")
-        //std::string l1_str("_link3")
-        //std::string l1_str("_link4")
-        //std::string l1_str("_link5")
-        //std::string l1_str("_link6")
-
-
-        //tf::Transform transform = angles_to_transform(state->thetas);
-        //tf::Transform Ry90;
-        ////Ry90.setOrigin(
-        //Ry90.setOrigin(tf::Vector3(0., 0, 0.));
-        //Ry90.getBasis().setEulerYPR(0, M_PI/2.0, 0);
-        //transform.setBasis(Ry90.getBasis() * transform.getBasis());
-        ////br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "/world", frame_id.c_str()));
-        //btScalar y, p, r;
-        //transform.getBasis().getEulerYPR(y, p, r);
-        //transform.getBasis().setEulerYPR(-r, p, r);
-        ////printf("%f\n", transform.getRotation()[2]);
-        ////pose_stamped.pose.setEuler(M_PI, 0, 0);
-        //pose_stamped.pose.orientation.x = transform.getRotation()[0];
-        //pose_stamped.pose.orientation.y = transform.getRotation()[1];
-        //pose_stamped.pose.orientation.z = transform.getRotation()[2];
-        //pose_stamped.pose.orientation.w = transform.getRotation()[3];
-        //pose_publisher.publish(pose_stamped);
-
-        //pose_stamped.pose.position.x = state->position[0] / 1000.;
-        //pose_stamped.pose.position.y = state->position[1] / 1000.;
-        //pose_stamped.pose.position.z = state->position[2] / 1000.;
-        //printf("=================== Transform ===================\n");
-        //for (int row = 0; row < 4; row++)
-        //{
-        //    for (int col = 0; col < 4; col++)
-        //    {
-        //        printf("%.3f, ", state->omni_mx[col][row]);
-        //    }
-        //    printf("\n");
-        //}
-    
-        //tf::Transform transform;
-        //transform.setOrigin(tf::Vector3(state->omni_mx[3][0], state->omni_mx[3][1], state->omni_mx[3][2]));
-        //transform.getBasis().setValue(  state->omni_mx[0][0], state->omni_mx[1][0], state->omni_mx[2][0],
-        //                                state->omni_mx[0][1], state->omni_mx[1][1], state->omni_mx[2][1], 
-        //                                state->omni_mx[0][2], state->omni_mx[1][2], state->omni_mx[2][2]);
-
-
-
-
-//glmat[col*4 + row] = state.omni_mx[col][row];
-//state.omni_mx[0][0] state.omni_mx[1][0] state.omni_mx[2][0] state.omni_mx[3][0]
-//state.omni_mx[0][1] state.omni_mx[1][1] state.omni_mx[2][1] state.omni_mx[3][1]
-//state.omni_mx[0][2] state.omni_mx[1][2] state.omni_mx[2][2] state.omni_mx[3][2]
-//state.omni_mx[0][3] state.omni_mx[1][3] state.omni_mx[2][3] state.omni_mx[3][3]
-
-//printf("x %.3f y %.3f z %.3f\n",
-//        transform.getOrigin()[0],
-//        transform.getOrigin()[1],
-//        transform.getOrigin()[2]);
-//printf("%.3f %.3f %.3f\n",
-//        transform.getBasis().getRow(0)[0],
-//        transform.getBasis().getRow(0)[1],
-//        transform.getBasis().getRow(0)[2]
-//        );
-//pose_stamped.pose.orientation.x = ;
-//pose_stamped.pose.orientation.y = ;
-//pose_stamped.pose.orientation.z = ;
-//pose_stamped.pose.orientation.w = ;
-//delete glmat;
