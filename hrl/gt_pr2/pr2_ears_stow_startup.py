0a856ca8c5764f97794762c05cd20a44114b46bb
BEAMBOT <BEAMBOT@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-01-18 20:25:29 +0000

    updated.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@813 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/gt_pr2/pr2_ears_stow_startup.py
+++ hrl/gt_pr2/pr2_ears_stow_startup.py
@@ -11,6 +11,7 @@ import math
 if __name__ == '__main__':
     rospy.logout( 'pr2_ears_stow_startup: Stowing RFID Ears Servos' )
     
+    rospy.logout( 'pr2_ears_stow_startup: Connecting to servos' )
     dyn_right = rs.USB2Dynamixel_Device( '/dev/robot/servo1' )
     pan_right = rs.Robotis_Servo( dyn_right, 29 )
     tilt_right = rs.Robotis_Servo( dyn_right, 30 )
@@ -19,13 +20,14 @@ if __name__ == '__main__':
     pan_left = rs.Robotis_Servo( dyn_left, 27 )
     tilt_left = rs.Robotis_Servo( dyn_left, 28 )
 
+    rospy.logout( 'pr2_ears_stow_startup: Commanding stow positions' )
     pan_right.move_angle( 1.370, math.radians( 10 ), False )
     tilt_right.move_angle( 0.0, math.radians( 10 ), False )
 
     pan_left.move_angle( -1.370, math.radians( 10 ), False )
     tilt_left.move_angle( 0.0, math.radians( 10 ), False )
 
-    while pan_left.is_moving() or pan_right.is_moving():
-        time.sleep( 0.1 )
+    rospy.logout( 'pr2_ears_stow_startup: Done' )
+
     time.sleep(1.0)
 
