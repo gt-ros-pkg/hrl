adfc70bfaad044980d405bae984323d4edb95a0f
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-02-01 19:22:44 +0000

    M    hrl_lib/src/hrl_lib/transforms.py whitespace.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@919 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_lib/src/hrl_lib/transforms.py
+++ hrl/hrl_lib/src/hrl_lib/transforms.py
@@ -93,6 +93,7 @@ def composeHomogeneousTransform(rot, disp):
     t = np.row_stack((np.column_stack((rot, disp)),te))
     return t
 
+
 def angle_within_mod180(angle):
     ''' angle in radians.
         returns angle within -pi and +pi
