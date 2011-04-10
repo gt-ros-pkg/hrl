a6c05ac4d29d35d786d3c934328450576bd012a7
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-04-10 00:08:15 +0000

    A    short.py simple script to power up an individual joint. Now going to try and add in the set_safeops into arm_server.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1177 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/equilibrium_point_control/epc_core/src/cody_arms/short.py
@@ -0,0 +1,78 @@
+
+
+import m3.rt_proxy as m3p
+import m3.component_factory as m3f
+import m3.toolbox as m3t
+
+THETA_GC = 5
+
+proxy = m3p.M3RtProxy()
+proxy.start()
+
+joint_names = ['m3joint_ma1_j0']
+
+c = joint_names[0]
+comp = m3f.create_component(c)
+
+pwr_nm = 'm3pwr_pwr003'
+pwr = m3f.create_component(pwr_nm)
+
+proxy.publish_command(pwr)
+proxy.publish_command(comp)
+proxy.make_operational_all()
+
+#Force safe-op of robot, etc are present
+types=['m3humanoid','m3hand','m3gripper']
+for t in types:
+    cc = proxy.get_available_components(t)
+    for ccc in cc:
+        print 'ccc:', ccc
+        proxy.make_safe_operational(ccc)
+
+
+#Force safe-op of chain so that gravity terms are computed
+chain=None
+if len(joint_names)>0:
+    for j in joint_names:
+        chain_name=m3t.get_joint_chain_name(j)
+        print 'chain_name:', chain_name
+        if chain_name!="":
+            proxy.make_safe_operational(chain_name)
+
+            #Force safe-op of chain so that gravity terms are computed
+            dynamatics_name = m3t.get_chain_dynamatics_component_name(chain_name)
+            print 'dynamatics_name:', dynamatics_name
+            if dynamatics_name != "":
+                proxy.make_safe_operational(dynamatics_name)
+
+
+
+#Force safe-op of robot so that gravity terms are computed
+robot_name = m3t.get_robot_name()
+print 'robot_name:', robot_name
+if robot_name != "":
+    proxy.make_safe_operational(robot_name)
+
+
+
+
+
+comp.set_control_mode(THETA_GC)
+comp.set_stiffness(0.5)
+comp.set_theta_deg(0.)
+#comp.set_thetadot_deg(0.)
+
+proxy.step()
+proxy.step()
+
+raw_input('Hit ENTER to power on')
+pwr.set_motor_power_on()
+
+proxy.step()
+proxy.step()
+
+raw_input('Hit ENTER to stop')
+proxy.stop()
+
+
+
