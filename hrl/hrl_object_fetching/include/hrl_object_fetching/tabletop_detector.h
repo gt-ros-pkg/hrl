784f5bd0c874dd6c005d2a9e02439ff5b12dc3b5
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-04-21 12:34:46 +0000

    Revert of tabletop detection to working revision.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1259 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_object_fetching/include/hrl_object_fetching/tabletop_detector.h
+++ hrl/hrl_object_fetching/include/hrl_object_fetching/tabletop_detector.h
@@ -8,7 +8,6 @@
 #include "pcl_ros/transforms.h"
 #include "pcl/point_types.h"
 #include <boost/make_shared.hpp>
-#include <boost/thread/mutex.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv/cv.h>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -29,26 +28,17 @@ namespace hrl_object_fetching {
     class TabletopDetector {
         public:
             TabletopDetector();
-            // Parameters
-            double minx, maxx, miny, maxy, minz, maxz, resolution, imgx, imgy;
-            double inlier_magnitude, num_edge_dilate;
-            double degree_bins, hough_thresh;
-            double theta_gran, rho_gran;
-            double xgran, ygran;
-
             ros::ServiceServer table_detect_service;
             ros::Subscriber pc_sub;
             ros::NodeHandle nh;
             ros::NodeHandle nh_priv;
             ros::Publisher pc_pub;
             ros::Publisher pose_arr_pub;
-            cv::Mat height_img_sum, height_img_count;
             bool grasp_points_found;
             geometry_msgs::PoseArray grasp_points;
             image_transport::ImageTransport img_trans;
             image_transport::Publisher height_pub;
             tf::TransformListener tf_listener;
-            boost::mutex pc_lock;
             void onInit();
             void pcCallback(PointCloud2::ConstPtr pc);
             bool srvCallback(hrl_object_fetching::DetectTable::Request& req, 
