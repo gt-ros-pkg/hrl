70a2cc24ffb2b22fa625f4d2917c993c7299ecd2
BEAMBOT <BEAMBOT@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-04-21 10:18:33 +0000

    A new package that allows you to record and play back trajectories (forward or backward) from service calls.  Useful for tucking and untucking.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1254 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_trajectory_playback/src/hrl_trajectory_playback/playback.py
@@ -0,0 +1,94 @@
+#! /usr/bin/python
+import roslib
+roslib.load_manifest('hrl_trajectory_playback')
+import rospy
+
+import pr2_controllers_msgs.msg as pm
+from motion_planning_msgs.srv import FilterJointTrajectory
+import hrl_lib.util as hrl_util
+from hrl_pr2_lib.pr2 import PR2, Joint
+from hrl_trajectory_playback.srv import TrajPlaybackSrv
+
+import numpy as np, math
+
+class TrajPlayback():
+    def __init__( self, name, fname ):
+        self.name = name
+
+        rospy.logout( 'TrajPlayback: Initializing (%s)' % self.name )
+        try:
+            rospy.init_node('traj_playback_'+self.name)
+        except:
+            pass
+
+        dat = hrl_util.load_pickle( fname )
+        self.q = np.mat( dat[0].T )
+
+        # subsample
+        self.q = self.q[:,0::30]
+        # smooth
+        self.t = np.linspace( 1.3, 1.3 + (self.q.shape[1]-1)*0.3, self.q.shape[1] ) 
+
+        self.vel = self.q.copy()
+        self.vel[:,1:] -= self.q[:,0:-1]
+        self.vel[:,0] = 0
+        self.vel /= 0.3
+
+        self.pr2 = PR2()
+        # rospy.wait_for_service('trajectory_filter/filter_trajectory')
+        # self.filter_traj = rospy.ServiceProxy('trajectory_filter/filter_trajectory',
+        #                                       FilterJointTrajectory)
+
+        self.__service = rospy.Service( 'traj_playback/' + name,
+                                        TrajPlaybackSrv,
+                                        self.process_service )
+
+        rospy.logout( 'TrajPlayback: Ready for service requests (%s)' % self.name )
+        
+
+    def process_service( self, req ):
+        if req.play_backward:
+            rospy.loginfo( 'TrajPlayback: Playing Reverse (%s)' % self.name )
+        else:
+            rospy.loginfo( 'TrajPlayback: Playing Forward (%s)' % self.name )
+        
+        if req.play_backward:
+            tq = self.q[:,::-1].copy()
+            tvel = -1 * self.vel[:,::-1].copy()
+            tvel[:,-1] = 0
+            tt = self.t.copy()
+            tt[0] = 0
+            tt[1:] += 0.1
+        else:
+            tq = self.q.copy()
+            tvel = self.vel.copy()
+            tt = self.t.copy()
+
+        joint_traj = self.pr2.right._create_trajectory( tq, tt, tvel )
+        dur = rospy.Duration.from_sec( tt[-1] + 5 )
+
+        # result = self.filter_traj( trajectory = joint_traj,
+        #                            allowed_time = dur)
+        # ft = result.trajectory
+        # ft.header.stamp = rospy.get_rostime() + rospy.Duration( 1.0 )
+
+        g = pm.JointTrajectoryGoal()
+        g.trajectory = joint_traj  # why not ft...?
+        self.pr2.right.client.send_goal( g )
+        self.pr2.right.client.wait_for_result()
+
+        return True
+        
+if __name__ == '__main__':
+    import optparse
+    p = optparse.OptionParser()
+    p.add_option('--pkl', action='store', type='string', dest='pkl',
+                 help='Output file [default=\'out.pkl\']', default='out.pkl')
+    p.add_option('--name', action='store', type='string', dest='name',
+                 help='Service "name": /traj_playback/name [default=\'test\']',
+                 default='test')
+    opt, args = p.parse_args()
+    
+    tp = TrajPlayback( opt.name, opt.pkl )
+    rospy.spin()
+    
---
+++ hrl/hrl_trajectory_playback/src/hrl_trajectory_playback/record.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python
+import roslib
+roslib.load_manifest('hrl_trajectory_playback')
+import rospy
+
+import hrl_lib.util as hrl_util
+from sensor_msgs.msg import JointState
+
+from collections import deque # fast appends
+import numpy as np, math
+
+# Hack (this code currently only supports the right arm!)
+r_jt_idx_lis = [17, 18, 16, 20, 19, 21, 22]
+l_jt_idx_lis = [29, 30, 28, 32, 31, 33, 34]
+
+class Listener:
+    def __init__(self):
+        self.q = deque() # np.zeros(( n_samples, 7))
+        self.t = deque() # np.zeros((n_samples, 1))
+        # self.tind = 0
+        # self.ind = 0
+        self.initialized = False
+        self.starttime = rospy.Time(0)
+
+    def callback( self, msg ):
+        currtime = msg.header.stamp.to_sec()
+        
+        if not self.initialized:
+            self.initialized = True
+            self.starttime = currtime
+            self.lasttime = currtime
+            
+        if currtime - self.lasttime > 1.0:
+            self.lasttime = currtime
+            print 'Still Capturing (%d sec)' % (currtime - self.starttime)
+
+        q_ja = [ msg.position[idx] for idx in r_jt_idx_lis ]
+        self.q.append( q_ja )
+        
+        # for i,idx in enumerate( r_jt_idx_lis ):  # only dealing with right arm!
+        #     self.q[self.ind, i] = msg.position[idx]
+        #     print self.q[self.ind, i],
+        # print ""
+        # self.t[self.ind] = msg.header.stamp.to_sec()
+        # self.ind += 1
+        self.t.append([ currtime ])
+        
+
+if __name__ == '__main__':
+    import optparse
+    p = optparse.OptionParser()
+    p.add_option('--pkl', action='store', type='string', dest='pkl',
+                 help='Output file [default=\'out.pkl\']', default='out.pkl')
+    opt, args = p.parse_args()
+    
+    rospy.init_node('quick_data')
+    
+    lis = Listener()
+    sub = rospy.Subscriber( '/joint_states', JointState, lis.callback )
+
+    print 'Recording...  <Ctrl-C> to stop.'
+    rospy.spin()
+    sub.unregister()
+
+    
+    #hrl_util.save_pickle([lis.q[0:lis.ind,:], lis.t[0:lis.ind,:]], 'untuck_traj_6.pickle')
+    print 'Outputting to: %s' % opt.pkl
+    hrl_util.save_pickle([ np.array(lis.q),   # Nx7 numpy array
+                           np.array(lis.t) ], # Nx1 numpy array
+                         opt.pkl )
