b8cefa7adad6e8f4a5b7ae2110551f5deab32a63
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-04-22 09:10:37 +0000

    Blob tabletop detection working much better.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1273 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_object_fetching/src/tabletop_detector.cpp
+++ hrl/hrl_object_fetching/src/tabletop_detector.cpp
@@ -38,8 +38,6 @@ namespace hrl_object_fetching {
         height_pub = img_trans.advertise("/height_image", 1);
         pose_arr_pub = nh.advertise<geometry_msgs::PoseArray>("/table_approach_poses", 1);
         nh_priv = ros::NodeHandle("~");
-        bool run_service;
-        nh_priv.param<bool>("run_service", run_service, false);
         if(run_service)
             table_detect_service = nh.advertiseService("/table_detect", &TabletopDetector::srvCallback, this);
         else
@@ -65,23 +63,44 @@ namespace hrl_object_fetching {
 
     bool compind(uint32_t a, uint32_t b, vector<float> v) { return v[a] < v[b]; }
 
+    void convCvBox2DPoly(CvBox2D& box, CvPoint**& pts, int*& npts) {
+        pts = new CvPoint*[1]; pts[0] = new CvPoint[4];
+        float x = box.center.x, y = box.center.y;
+        float x1 = box.size.height*cos(box.angle-CV_PI/2), y1 = -box.size.height*sin(box.angle-CV_PI/2);
+        float x2 = box.size.width*cos(box.angle), y2 = -box.size.width*sin(box.angle);
+        pts[0][0].x = x+x1+x2; pts[0][0].y = y+y1+y2;
+        pts[0][1].x = x-x1+x2; pts[0][1].y = y-y1+y2;
+        pts[0][2].x = x-x1-x2; pts[0][2].y = y-y1-y2;
+        pts[0][3].x = x+x1-x2; pts[0][3].y = y+y1-y2;
+        pts[0][4].x = x+x1+x2; pts[0][4].y = y+y1+y2;
+        npts = new int[1]; npts[0] = 5;
+    }
+
+    void drawCvBox2D(IplImage& img, CvBox2D& box, cv::Scalar color, int width=1) {
+        CvPoint** rpts; int* npts;
+        convCvBox2DPoly(box, rpts, npts);
+        cvPolyLine(&img, rpts, npts, 1, 1, color, width);
+        delete[] rpts[0]; delete[] rpts; delete[] npts;
+    }
+
+    void fillCvBox2D(IplImage& img, CvBox2D& box, cv::Scalar color) {
+        CvPoint** rpts; int* npts;
+        convCvBox2DPoly(box, rpts, npts);
+        cvFillPoly(&img, rpts, npts, 1, color);
+        delete[] rpts[0]; delete[] rpts; delete[] npts;
+    }
+
     void TabletopDetector::pcCallback(sensor_msgs::PointCloud2::ConstPtr pc_msg) {
         if(!pc_lock.try_lock())
             return;
 
         pcl::PointCloud<pcl::PointXYZRGB> pc_full, pc_full_frame;
         pcl::fromROSMsg(*pc_msg, pc_full);
-        //string head_frame("/head_pan_link");
         string base_frame("/base_link");
         ros::Time now = ros::Time::now();
         tf_listener.waitForTransform(pc_msg->header.frame_id, base_frame, now, ros::Duration(3.0));
         pcl_ros::transformPointCloud(base_frame, pc_full, pc_full_frame, tf_listener);
         // pc_full_frame is in torso lift frame
-
-        tf_listener.waitForTransform(base_frame, head_frame, now, ros::Duration(3.0));
-        tf::StampedTransform base_head_trans;
-        tf_listener.lookupTransform(base_frame, head_frame, now, base_head_trans);
-        float z_diff = base_head_trans.getOrigin().z();
 
         cv::Mat cur_height_img = cv::Mat::zeros(imgx, imgy, CV_8U);
         
@@ -107,25 +126,21 @@ namespace hrl_object_fetching {
         cur_count.convertTo(cur_count_flt, CV_32F);
         height_img_count += cur_count_flt;
         cv::Mat height_img_avg_flt = height_img_sum / height_img_count;
-        /*for(uint32_t x=0;x<imgx;x++)
-            for(uint32_t y=0;y<imgy;y++)
-                printf("%f,",cur_height_img.at<float>(x,y));*/
         cv::Mat height_img_avg(imgx, imgy, CV_8U);
-        //cv::randu(height_img_avg, cv::Scalar(0), cv::Scalar(256));
         height_img_avg_flt.convertTo(height_img_avg, CV_8U);
 
         cv::Mat height_hist(256, 1, CV_32F, cv::Scalar(0));
         for(uint32_t x=0;x<imgx;x++)
             for(uint32_t y=0;y<imgy;y++) {
-                if(height_img.at<uint8_t>(x,y) == 255)
-                    height_img.at<uint8_t>(x,y) = 0;
-                if(height_img.at<uint8_t>(x,y) != 0) {
-                    height_hist.at<float>(height_img.at<uint8_t>(x,y), 0)++;
+                if(height_img_avg.at<uint8_t>(x,y) == 255)
+                    height_img_avg.at<uint8_t>(x,y) = 0;
+                if(height_img_avg.at<uint8_t>(x,y) != 0) {
+                    height_hist.at<float>(height_img_avg.at<uint8_t>(x,y), 0)++;
                 }
             }
         ////////////////////// Finding best table height /////////////////////////
         uint32_t gfiltlen = 25;
-        float stddev = 6;
+        float stddev = 256/(maxz-minz) * 0.005;
         cv::Mat gauss_filt(gfiltlen, 1, CV_32F, cv::Scalar(0));
         for(uint32_t i=0;i<gfiltlen;i++)
             gauss_filt.at<float>(i,0) = 0.39894 / stddev * std::exp(-(i-((float)gfiltlen)/2)*(i-((float)gfiltlen)/2)/(2*stddev*stddev));
@@ -135,14 +150,15 @@ namespace hrl_object_fetching {
             uint32_t sum = 0;
             for(uint32_t j=0;j<gfiltlen;j++) 
                 sum += height_hist.at<float>(i+j,0) * gauss_filt.at<float>(j,0);
-            if(sum > maxval) {
+            if(sum > maxval && i != 0) {
                 maxval = sum;
                 maxidx = i+gfiltlen/2;
             }
         }
         int32_t table_height = ((int32_t)maxidx);
         //printf("%d %d, ", maxval, maxidx);
-        cv::Mat height_img_thresh = height_img.clone();
+        /////////////////////////// Getting table binary /////////////////////
+        cv::Mat height_img_thresh = height_img_avg.clone();
         for(uint32_t x=0;x<imgx;x++)
             for(uint32_t y=0;y<imgy;y++) {
                 if(std::fabs(table_height - ((int32_t)height_img_thresh.at<uint8_t>(x,y))) < stddev*2)
@@ -151,88 +167,51 @@ namespace hrl_object_fetching {
                     height_img_thresh.at<uint8_t>(x,y) = 0;
             }
         //////////////////////////////////////////////////////////////////
-
-        cv::Mat height_morph(imgx, imgy, CV_8U, cv::Scalar(0));
-        cv::Mat tmp_img(imgx, imgy, CV_8U, cv::Scalar(0));
-        IplImage t1 = height_img_thresh; IplImage t2 = height_morph;
         IplConvKernel* element = cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT);
-        cvMorphologyEx(&t1, &t2, NULL, element, CV_MOP_CLOSE);
-        cvMorphologyEx(&t2, &t2, NULL, element, CV_MOP_OPEN, 2);
-        cv::Mat table_edge = height_morph.clone();
+        //cvMorphologyEx(&height_img_thresh, &height_img_thresh, NULL, element, CV_MOP_CLOSE);
+        //cvMorphologyEx(&height_img_thresh, &height_img_thresh, NULL, element, CV_MOP_OPEN, 2);
+
+        cv::Mat height_img_thresh_blob = height_img_thresh.clone();
+        IplImage blob_img = height_img_thresh_blob;
+        CBlobResult blobs = CBlobResult(&blob_img, NULL, 0);
+        blobs.Filter(blobs, B_EXCLUDE, CBlobGetArea(), B_LESS, 10);
+        CBlob biggestblob;
+        blobs.GetNthBlob(CBlobGetArea(), 0, biggestblob);
+        cv::Mat table_blob(imgx, imgy, CV_8U, cv::Scalar(0)); IplImage table_blob_img = table_blob;
+        biggestblob.FillBlob(&table_blob_img, cv::Scalar(150));
+        //drawCvBox2D(blob_img, table_roi, cv::Scalar(50), 1);
+        CvBox2D table_roi = biggestblob.GetEllipse(); table_roi.angle *= CV_PI/180;
+        cv::Mat table_hull(imgx, imgy, CV_8U, cv::Scalar(0));
+        IplImage hull_img = table_hull;
+        fillCvBox2D(hull_img, table_roi, cv::Scalar(255));
+        //printf("Cvbox: %f, %f, %f, %f, %f\n", table_roi.center.x, table_roi.center.y, table_roi.size.width, table_roi.size.height, table_roi.angle);
+
+        //cv::Mat height_morph(imgx, imgy, CV_8U, cv::Scalar(0));
+        //cv::Mat tmp_img(imgx, imgy, CV_8U, cv::Scalar(0));
+        //IplImage t1 = height_img_thresh; IplImage  = height_morph;
+
+        //cv::Sobel(table_blob, table_blob, CV_8U, 0, 1, 1);
+
+        //cv::Mat table_edge = height_morph.clone();
         //cvMorphologyEx(&height_img, &height_morph, NULL, element, CV_MOP_CLOSE);
 
-        double sumx = 0, sumy = 0, suma = 0;
-        for(uint32_t y=0;y<imgy;y++) {
-            bool first_found = false;
-            for(uint32_t x=0;x<imgx;x++) {
-                if(height_morph.at<uint8_t>(x,y) == 255) {
-                    sumx += x;
-                    sumy += y;
-                    suma ++;
-                }
-                if(first_found) {
-                    table_edge.at<uint8_t>(x,y) = 0;
-                    continue;
-                }
-                if(table_edge.at<uint8_t>(x,y) == 255)
-                    first_found = true;
-            }
-        }
-        double centerx = sumx/suma, centery = sumy/suma;
-        double moment11 = 0, moment12 = 0, moment21 = 0, moment22 = 0;
-        for(uint32_t y=0;y<imgx;y++) {
-            for(uint32_t x=0;x<imgx;x++) {
-                if(height_morph.at<uint8_t>(x,y) == 255) {
-                    moment11 += (x - centerx) * (x - centerx); 
-                    moment12 += (y - centery) * (x - centerx); 
-                    moment21 += (x - centerx) * (y - centery); 
-                    moment22 += (y - centery) * (y - centery); 
-                }
-            }
-        }
-        double teig1 = (moment11 + moment22) / 2;
-        double teig2 = sqrt((moment11 + moment22) * (moment11 + moment22) - 
-                            4*(moment11*moment22 - moment12*moment12))/2;
-        double eig1 = teig1 + teig2, eig2 = teig1 - teig2;
-        double sign1 = 1, sign2 = 1;
-        if((moment11 - eig1) * (moment12) < 0)
-            sign1 = -1;
-        if((moment11 - eig2) * (moment12) < 0)
-            sign2 = -1;
-        double evec1 = sign1 * sqrt((moment22 - eig1) / (moment11 - eig1));
-        double evec2 = sign2 * sqrt((moment22 - eig2) / (moment11 - eig2));
-        double m1 = sqrt(1+evec1*evec1), m2 = sqrt(1+evec2*evec2);
-        double mag = 200 * sqrt(eig1) / suma;
-        double comp1x = mag * sqrt(eig2/eig1) * evec1/m1, comp1y = sqrt(eig2/eig1) * mag /m1;
-        double comp2x = mag * evec2/m2, comp2y = mag /m2;
-        CvPoint** rpts = new CvPoint*[1]; rpts[0] = new CvPoint[4];
-        rpts[0][0].y = centerx+comp1y+comp2y; rpts[0][0].x = centery+comp1x+comp2x;
-        rpts[0][1].y = centerx+comp1y-comp2y; rpts[0][1].x = centery+comp1x-comp2x;
-        rpts[0][2].y = centerx-comp1y-comp2y; rpts[0][2].x = centery-comp1x-comp2x;
-        rpts[0][3].y = centerx-comp1y+comp2y; rpts[0][3].x = centery-comp1x+comp2x;
-        rpts[0][4].y = centerx+comp1y+comp2y; rpts[0][4].x = centery+comp1x+comp2x;
-        int npts[1] = {5};
-        cvPolyLine(&t2, rpts, npts, 1, 1, cv::Scalar(180), 2);
-        //delete[] rpts[0]; delete[] rpts;
-        //printf("(%f, %f) [%f, %f] %f, %f, %f, %f;", (evec1/m1)*(evec1/m1) + (1/(m1*m1)), (evec2/m2)*(evec2/m2) + (1/(m2*m2)), sqrt(eig1), sqrt(eig2), comp1x, comp1y, comp2x, comp2y);
-        cv::Mat table_hull(imgx, imgy, CV_8U, cv::Scalar(0));
-        IplImage ipl_hull = table_hull;
-        cvFillPoly(&ipl_hull, rpts, npts, 1, cv::Scalar(255));
+        //cvFillPoly(&ipl_hull, rpts, npts, 1, cv::Scalar(255));
 
-        cv::Mat obj_img(imgx, imgy, CV_8U, cv::Scalar(0));
-        std::vector<int32_t> xfeats, yfeats, zfeats;
-        double sumobjx = 0, sumobjy = 0, sumobja = 0;
-        for(uint32_t y=0;y<imgx;y++) 
-            for(uint32_t x=0;x<imgx;x++) 
-                if(table_hull.at<uint8_t>(x,y) == 255 && height_morph.at<uint8_t>(x,y) == 0
-                        && height_img.at<uint8_t>(x,y) > table_height + stddev*2) {
-                    obj_img.at<uint8_t>(x,y) = height_img.at<uint8_t>(x,y);
-                    sumobjx += x; sumobjy += y; sumobja ++;
-                    //xfeats.push_back(x); yfeats.push_back(y); 
-                    //zfeats.push_back(height_img.at<uint8_t>(x,y));
-                }
-        double objcentx = sumobjx/sumobja, objcenty = sumobjy/sumobja;
+        //cv::Mat obj_img(imgx, imgy, CV_8U, cv::Scalar(0));
+        //std::vector<int32_t> xfeats, yfeats, zfeats;
+        //double sumobjx = 0, sumobjy = 0, sumobja = 0;
+        //for(uint32_t y=0;y<imgx;y++) 
+        //    for(uint32_t x=0;x<imgx;x++) 
+        //        if(table_hull.at<uint8_t>(x,y) == 255 && height_morph.at<uint8_t>(x,y) == 0
+        //                && height_img_avg.at<uint8_t>(x,y) > table_height + stddev*2) {
+        //            obj_img.at<uint8_t>(x,y) = height_img_avg.at<uint8_t>(x,y);
+        //            sumobjx += x; sumobjy += y; sumobja ++;
+        //            //xfeats.push_back(x); yfeats.push_back(y); 
+        //            //zfeats.push_back(height_img.at<uint8_t>(x,y));
+        //        }
+        //double objcentx = sumobjx/sumobja, objcenty = sumobjy/sumobja;
 
+        /*
         CvMemStorage* storage = cvCreateMemStorage(0);
         CvSeq* lines = 0;
         cv::Mat lines_img = height_morph.clone();
@@ -242,7 +221,7 @@ namespace hrl_object_fetching {
         lines = cvHoughLines2(&table_edge_ipl, storage, CV_HOUGH_STANDARD, 1, 3*CV_PI/180, 60, 0, 0);
         vector<float> theta_bins, rho_bins;
         vector<uint32_t> count_bins;
-        for(uint32_t i=0; i < lines->total; i++) {
+        for(uint32_t i=0; i < (uint32_t) lines->total; i++) {
             float* line = (float*)cvGetSeqElem(lines, i);
             float rho = line[0];
             float theta = line[1];
@@ -306,7 +285,6 @@ namespace hrl_object_fetching {
         sort(pose_inds.begin(), pose_inds.end(), sortind);
 
         vector<float> retposesx, retposesy;
-        float xgrand = 0.1 * resolution, ygrand = 0.1 * resolution;
         grasp_points.poses.clear();
         for(uint32_t i=0;i<pose_inds.size();i++) {
             float posex = posesx[pose_inds[i]], posey = posesy[pose_inds[i]];
@@ -327,20 +305,13 @@ namespace hrl_object_fetching {
                 btMatrix3x3 quatmat; btQuaternion quat;
                 quatmat.setEulerZYX(-poses_theta[i] - CV_PI/2, 0 , 0);
                 quatmat.getRotation(quat);
-                btQuaternion head_quat(base_head_trans.getRotation().x(),
-                                       base_head_trans.getRotation().y(),
-                                       base_head_trans.getRotation().z(),
-                                       base_head_trans.getRotation().w());
-                //quat *= head_quat;
-                //quat = quat*head_quat;
-                //quat = head_quat;
                 cpose.orientation.x = quat.x(); cpose.orientation.y = quat.y();
                 cpose.orientation.z = quat.z(); cpose.orientation.w = quat.w();
                 grasp_points.poses.push_back(cpose);
             }
         }
         grasp_points.header.stamp = ros::Time::now();
-        grasp_points.header.frame_id = head_frame;
+        grasp_points.header.frame_id = base_frame;
         pose_arr_pub.publish(grasp_points);
         
 
@@ -354,23 +325,23 @@ namespace hrl_object_fetching {
         }
 
 
-        /*
-        cv::Mat obj_feats(xfeats.size(), 1, CV_32S, cv::Scalar(0));
-        for(uint32_t i=0;i<xfeats.size();i++) {
-            obj_feats.at<uint32_t>(i,0) = xfeats[i]; obj_feats.at<uint32_t>(i,1) = yfeats[i]; obj_feats.at<uint32_t>(i,2) = zfeats[i]; 
-        }
-        cvflann::KMeansIndexParams kmips;
-        kmips.branching = 32;
-        kmips.iterations = 11;
-        kmips.centers_init = cvflann::CENTERS_RANDOM;
-        kmips.cb_index = 0.2;
-        cv::Mat obj_centers;
-        CvMat obj_feats_mat = obj_feats;
-        //cvflann::Matrix<uint32_t> obj_feats_mat;
-        //cvflann::Matrix<cvflann::EUCLIDEAN> obj_centers_mat;
-        int num_clust = cvflann::hierarchicalClustering<CV_32S,CV_32S>(obj_feats_mat, obj_centers, kmips);
-        printf("\nNum clust: %d \n", num_clust);
-        */
+        
+        //cv::Mat obj_feats(xfeats.size(), 1, CV_32S, cv::Scalar(0));
+        //for(uint32_t i=0;i<xfeats.size();i++) {
+        //    obj_feats.at<uint32_t>(i,0) = xfeats[i]; obj_feats.at<uint32_t>(i,1) = yfeats[i]; obj_feats.at<uint32_t>(i,2) = zfeats[i]; 
+        //}
+        //cvflann::KMeansIndexParams kmips;
+        //kmips.branching = 32;
+        //kmips.iterations = 11;
+        //kmips.centers_init = cvflann::CENTERS_RANDOM;
+        //kmips.cb_index = 0.2;
+        //cv::Mat obj_centers;
+        //CvMat obj_feats_mat = obj_feats;
+        ////cvflann::Matrix<uint32_t> obj_feats_mat;
+        ////cvflann::Matrix<cvflann::EUCLIDEAN> obj_centers_mat;
+        //int num_clust = cvflann::hierarchicalClustering<CV_32S,CV_32S>(obj_feats_mat, obj_centers, kmips);
+        //printf("\nNum clust: %d \n", num_clust);
+        
 
 
         cv::Mat table_edge2 = table_edge.clone();
@@ -398,19 +369,22 @@ namespace hrl_object_fetching {
                 ((uint32_t*) &pt.rgb)[0] = blue;
             }
         }
+        */
 
         cv_bridge::CvImage cvb_height_img;
-        //cvb_height_img.image = height_img_avg;
+        cvb_height_img.image = height_img_avg;
         //cvb_height_img.image = height_morph;
         //cvb_height_img.image = obj_img;
-        cvb_height_img.image = lines_img;
+        //cvb_height_img.image = lines_img;
+        //cvb_height_img.image = height_img_thresh_blob;
+        //cvb_height_img.image = table_blob;
         //cvb_height_img.image = table_edge;
         cvb_height_img.header.stamp = ros::Time::now();
-        cvb_height_img.header.frame_id = head_frame;
+        cvb_height_img.header.frame_id = base_frame;
         cvb_height_img.encoding = enc::MONO8;
         height_pub.publish(cvb_height_img.toImageMsg());
         pc_full_frame.header.stamp = ros::Time::now();
-        pc_full_frame.header.frame_id = head_frame;
+        pc_full_frame.header.frame_id = base_frame;
         pc_pub.publish(pc_full_frame);
 
         if(grasp_points.poses.size() > 0)
