17905a92c35f761d7a0b4fe6eec572a70036f3dd
haidai <haidai@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-04-22 03:16:23 +0000

    M    hai_sandbox/rm_low_freq.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1268 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hai_sandbox/src/hai_sandbox/rm_low_freq.py
+++ hrl/hai_sandbox/src/hai_sandbox/rm_low_freq.py
@@ -1,8 +1,19 @@
 import roslib; roslib.load_manifest('hai_sandbox')
 import cv
 import sys
+import os.path as pt
 
-img = cv.LoadImageM(sys.argv[1])
+img_path = sys.argv[1]
+img = cv.LoadImageM()
 dst = cv.CloneMat(img)
-cv.Smooth(img, dst, cv.CV_GAUSSIAN)
-cv.SaveImage('filtered.png', dst)
+dif = cv.CloneMat(img)
+cv.Smooth(img, dst, cv.CV_GAUSSIAN, 91)
+cv.Sub(img, dst, dif)
+cv.SaveImage(img_path, dif)
+
+#orig_path, fname = pt.split(img_path)
+#name = pt.splitext(fname)[0]
+#pt.join(orig_path, name)
+
+
+
