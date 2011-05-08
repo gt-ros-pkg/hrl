32ef10059a196b47541d1bd0ab8dbfc1516cd444
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-05-08 23:46:23 +0000

    Basic surface segmentation.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1339 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_table_detect/src/surface_segmentation.cpp
+++ hrl/hrl_table_detect/src/surface_segmentation.cpp
@@ -1,12 +1,20 @@
 #include <numeric>
 #include <ros/ros.h>
+#include <algorithm>
+
 #include "sensor_msgs/PointCloud2.h"
 #include <pcl/features/normal_3d.h>
 #include <pcl/kdtree/kdtree_flann.h>
-#include "pcl/point_types.h"
+#include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_cloud.h>
 #include <pcl/features/principal_curvatures.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/surface/mls.h>
 
 #include <tf/transform_listener.h>
 #include "pcl_ros/transforms.h"
@@ -24,9 +32,6 @@
 #include <hrl_table_detect/DetectTableInst.h>
 #include <LinearMath/btMatrix3x3.h>
 #include <LinearMath/btQuaternion.h>
-#include <hrl_cvblobslib/Blob.h>
-#include <hrl_cvblobslib/BlobResult.h>
-#include <image_geometry/pinhole_camera_model.h>
 
 using namespace sensor_msgs;
 using namespace std;
@@ -42,7 +47,6 @@ namespace hrl_table_detect {
             ros::NodeHandle nh;
             ros::Publisher pc_pub, pc_pub2, pc_pub3;
             tf::TransformListener tf_listener;
-            image_geometry::PinholeCameraModel cam_model;
 
             SurfaceSegmentation();
             void onInit();
@@ -64,91 +68,107 @@ namespace hrl_table_detect {
 
     void SurfaceSegmentation::pcCallback(sensor_msgs::PointCloud2::ConstPtr pc_msg) {
         double min_z_val = 0.1, max_z_val = 1.5;
-        double norm_ang_thresh = 0.5;
-        double surf_clust_dist = 0.01, surf_clust_min_size = 20;
+        double norm_ang_thresh = 0.7;
+        double surf_clust_dist = 0.02, surf_clust_min_size = 50;
 
-        pcl::PointCloud<PRGB> pc_full, pc_full_frame;
-        pcl::fromROSMsg(*pc_msg, pc_full);
+        pcl::PointCloud<PRGB>::Ptr pc_full_ptr(new pcl::PointCloud<PRGB>());
+        pcl::PointCloud<PRGB>::Ptr pc_full_frame_ptr(new pcl::PointCloud<PRGB>());
+        pcl::fromROSMsg(*pc_msg, *pc_full_ptr);
         string base_frame("/base_link");
         ros::Time now = ros::Time::now();
 
         // Transform PC to base frame
         tf_listener.waitForTransform(pc_msg->header.frame_id, base_frame, now, ros::Duration(3.0));
-        pcl_ros::transformPointCloud(base_frame, pc_full, pc_full_frame, tf_listener);
-        pcl::PointCloud<PRGB>::ConstPtr pc_full_frame_ptr = boost::make_shared<pcl::PointCloud<PRGB> >(pc_full_frame);
+        pcl_ros::transformPointCloud(base_frame, *pc_full_ptr, *pc_full_frame_ptr, tf_listener);
 
         // Filter floor and ceiling
-        pcl::PointCloud<PRGB> pc_filtered;
-        pcl::PointCloud<PRGB>::ConstPtr pc_filtered_ptr = 
-                  boost::make_shared<const pcl::PointCloud<PRGB> >(pc_filtered);
+        pcl::PointCloud<PRGB>::Ptr pc_filtered_ptr(new pcl::PointCloud<PRGB>());
         pcl::PassThrough<PRGB> z_filt;
         z_filt.setFilterFieldName("z");
-        z_filt.setFilterLimits(min_z_val);
-        z_filt.setFilterLimits(max_z_val);
+        z_filt.setFilterLimits(min_z_val, max_z_val);
         z_filt.setInputCloud(pc_full_frame_ptr);
-        z_filt.applyFilter(pc_filtered);
+        z_filt.filter(*pc_filtered_ptr);
+
+        if(pc_filtered_ptr->size() < 20)
+            return;
 
         // Compute normals
-        pcl::PointCloud<PRGBN> cloud_normals;
-        pcl::PointCloud<PRGB>::ConstPtr cloud_normals_ptr = 
-                  boost::make_shared<const pcl::PointCloud<PRGB> >(cloud_normals);
-        pcl::KdTree<PRGB>::Ptr normals_tree = boost::make_shared<pcl::KdTreeFLANN<PRGB> > ();
-        pcl::NormalEstimation<PRGB, PRGBN> norm_est;
+        pcl::PointCloud<pcl::Normal>::Ptr cloud_normals_ptr(new pcl::PointCloud<pcl::Normal>());
+        pcl::KdTree<PRGB>::Ptr normals_tree (new pcl::KdTreeFLANN<PRGB> ());
+        pcl::PointCloud<PRGB> mls_points;
+        pcl::MovingLeastSquares<PRGB, pcl::Normal> mls;
+        normals_tree->setInputCloud(pc_filtered_ptr);
+        mls.setOutputNormals(cloud_normals_ptr);
+        mls.setInputCloud(pc_filtered_ptr);
+        mls.setPolynomialFit(true);
+        mls.setSearchMethod(normals_tree);
+        mls.setSearchRadius(0.02);
+        mls.reconstruct(mls_points);
+        
+        /*pcl::NormalEstimation<PRGB, pcl::Normal> norm_est;
         norm_est.setKSearch(15);
         norm_est.setSearchMethod(normals_tree);
         norm_est.setInputCloud(pc_filtered_ptr);
-        norm_est.compute(cloud_normals);
+        norm_est.compute(*cloud_normals_ptr);
+        */
         
-        pcl::PointIndices flat_inds;
-        pcl::PointIndices::ConstPtr flat_inds_ptr = 
-                  boost::make_shared<const pcl::PointIndices >(flat_inds);
+        pcl::PointIndices::Ptr flat_inds_ptr (new pcl::PointIndices());
         double ang;
         int i = 0;
-        BOOST_FOREACH(const PRGBN& pt, cloud_normals.points) {
+        BOOST_FOREACH(const pcl::Normal& pt, cloud_normals_ptr->points) {
             ang = fabs((acos(pt.normal[2]) - CV_PI/2)/CV_PI*2);
             if(ang > norm_ang_thresh)
-                flat_inds.indices.push_back(i);
+                flat_inds_ptr->indices.push_back(i);
             i++;
         }
 
+        if(flat_inds_ptr->indices.size() < 20)
+            return;
+
         // Cluster into distinct surfaces
-        pcl::EuclideanClusterExtraction<PRGBN> surf_clust;
+        pcl::EuclideanClusterExtraction<PRGB> surf_clust;
+        pcl::KdTree<PRGB>::Ptr clust_tree (new pcl::KdTreeFLANN<PRGB> ());
         surf_clust.setClusterTolerance(surf_clust_dist);
         surf_clust.setMinClusterSize(surf_clust_min_size);
         surf_clust.setIndices(flat_inds_ptr);
-        surf_clust.setInputCloud(cloud_normals_ptr);
+        surf_clust.setInputCloud(pc_filtered_ptr);
+        surf_clust.setSearchMethod(clust_tree);
         std::vector<pcl::PointIndices> surf_clust_list;
         surf_clust.extract(surf_clust_list);
 
         // Fit planes to all of the surfaces
         std::vector<pcl::ModelCoefficients> surf_models;
-        pcl::SACSegmentationFromNormals<PRGBN, PRGBN> sac_seg;
-        sac_seg.setModelType(pcl::SACMODEL_PLANE);
-        sac_seg.setModelType(pcl::SAC_MSAC);
+        pcl::SACSegmentationFromNormals<PRGB, pcl::Normal> sac_seg;
+        sac_seg.setModelType(pcl::SACMODEL_NORMAL_PLANE);
+        sac_seg.setMethodType(pcl::SAC_MSAC);
         sac_seg.setDistanceThreshold(0.03);
         sac_seg.setMaxIterations(10000);
         sac_seg.setNormalDistanceWeight(0.1);
         sac_seg.setOptimizeCoefficients(true);
         sac_seg.setProbability(0.99);
+
         for(uint32_t i =0;i<surf_clust_list.size();i++) {
             sac_seg.setInputNormals(cloud_normals_ptr);
-            sac_seg.setInputCloud(cloud_normals_ptr);
-            pcl::PointIndices::ConstPtr surf_clust_list_ptr =
-                      boost::make_shared<const pcl::PointIndices >(surf_clust_list[i]);
+            sac_seg.setInputCloud(pc_filtered_ptr);
+            pcl::PointIndices::Ptr surf_clust_list_ptr (new pcl::PointIndices());
+            surf_clust_list_ptr->indices = surf_clust_list[i].indices;
+            //std::copy(surf_clust_list[i].indices.begin(), surf_clust_list[i].indices.end(), surf_clust_list_ptr->indices.begin());
+            surf_clust_list_ptr->header = surf_clust_list[i].header;
             sac_seg.setIndices(surf_clust_list_ptr);
             pcl::PointIndices surf_inliers;
             pcl::ModelCoefficients surf_coeffs;
             sac_seg.segment(surf_inliers, surf_coeffs);
             surf_models.push_back(surf_coeffs);
+            uint32_t cur_color = 0xFF000000 | (rand() % 0x01000000);
+            for(uint32_t j=0;j<surf_inliers.indices.size();j++)
+                ((uint32_t*) &pc_filtered_ptr->points[surf_inliers.indices[j]].rgb)[0] = cur_color;
+
+            for(uint32_t j=0;j<surf_coeffs.values.size();j++)
+                cout << i << " " << j << " " << surf_coeffs.values[j] << endl;
         }
-
-        cloud_normals_colors.header.stamp = ros::Time::now();
-        cloud_normals_colors.header.frame_id = base_frame;
-        pc_pub.publish(cloud_normals_colors);
-
-        princip_curves_colors.header.stamp = ros::Time::now();
-        princip_curves_colors.header.frame_id = base_frame;
-        pc_pub2.publish(princip_curves_colors);
+        pc_filtered_ptr->header.stamp = ros::Time::now();
+        pc_filtered_ptr->header.frame_id = base_frame;
+        pc_pub2.publish(pc_filtered_ptr);
 
         // define a plane to project onto
         geometry_msgs::PoseStamped plane_pose;
@@ -167,7 +187,7 @@ namespace hrl_table_detect {
                            plane_pose.pose.position.z);
         i=0;
         pcl::PointCloud<PRGB> flat_pc;
-        BOOST_FOREACH(const PRGB& pt, pc_full.points) {
+        BOOST_FOREACH(const PRGB& pt, pc_full_ptr->points) {
             i++;
             if(pt.x != pt.x || pt.y != pt.y || pt.z != pt.z)
                 continue;
@@ -190,8 +210,9 @@ namespace hrl_table_detect {
             flat_pc.push_back(n_pt);
         }
         flat_pc.header.stamp = ros::Time::now();
-        flat_pc.header.frame_id = pc_full.header.frame_id;
+        flat_pc.header.frame_id = pc_full_ptr->header.frame_id;
         pc_pub3.publish(flat_pc);
+
 
     }
 
