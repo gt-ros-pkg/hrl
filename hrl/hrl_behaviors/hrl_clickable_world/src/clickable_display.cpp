de5e928d25383edc0ff4bfbcad85da194ea7387a
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-05-27 22:57:32 +0000

    Clickable display changes to support refactoring.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1489 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_behaviors/hrl_clickable_world/src/clickable_display.cpp
+++ hrl/hrl_behaviors/hrl_clickable_world/src/clickable_display.cpp
@@ -13,11 +13,6 @@
 #include <sensor_msgs/image_encodings.h>
 #include <std_srvs/Empty.h>
 
-#ifdef HAVE_GTK
-#include <gtk/gtk.h>
-static void destroyNode(GtkWidget *widget, gpointer data) { exit(0); }
-#endif
-
 namespace hrl_clickable_world {
 
     class ClickableDisplay {
@@ -25,41 +20,34 @@ namespace hrl_clickable_world {
             ros::NodeHandle nh, nh_priv;
             image_transport::ImageTransport img_trans;
             image_transport::Subscriber camera_sub;
-            ros::ServiceClient display_buttons_srv;
-            ros::Publisher pt_pub;
+            ros::Publisher l_click_pub, r_click_pub;
             std::string img_frame;
-            double last_time;
+            cv_bridge::CvImagePtr img_ptr;
+            bool have_img;
 
             ClickableDisplay();
             ~ClickableDisplay();
             void onInit();
             void imgCallback(const sensor_msgs::ImageConstPtr& img_msg);
+            void showImg();
             static void mouseClickCallback(int event, int x, int y, int flags, void* data);
-            static void perceiveButtonCallback(int state, void* data);
 
     };
 
-    ClickableDisplay::ClickableDisplay() : nh_priv("clickable_world"), img_trans(nh),
-                                           last_time(0) {
+    ClickableDisplay::ClickableDisplay() : nh_priv("clickable_world"), 
+                                           img_trans(nh),
+                                           have_img(false) {
     }
 
     void ClickableDisplay::onInit() {
         cv::namedWindow("Clickable World", 1);
-        // Can't use: requires QT support
-        //cv::createButton("Perceive Buttons", &ClickableDisplay::perceiveButtonCallback, 
-        //                 this, CV_PUSH_BUTTON, 0);
         cv::setMouseCallback("Clickable World", &ClickableDisplay::mouseClickCallback, this);
-
-#ifdef HAVE_GTK
-        GtkWidge *widget = GTK_WIDGET(cvGetWindowHandle("Clickable World"));
-        g_signal_connect(widget, "destroy", G_CALLBACK(destroyNode), NULL);
-#endif
 
         camera_sub = img_trans.subscribe<ClickableDisplay>
                                               ("image", 1, 
                                                &ClickableDisplay::imgCallback, this);
-        pt_pub = nh_priv.advertise<geometry_msgs::PointStamped>("mouse_click", 1);
-        display_buttons_srv = nh_priv.serviceClient<std_srvs::Empty>("perceive_buttons", true);
+        l_click_pub = nh_priv.advertise<geometry_msgs::PointStamped>("l_mouse_click", 1);
+        r_click_pub = nh_priv.advertise<geometry_msgs::PointStamped>("r_mouse_click", 1);
         
         ROS_INFO("[clickable_display] ClickableDisplay loaded.");
         ros::Duration(1).sleep();
@@ -67,15 +55,25 @@ namespace hrl_clickable_world {
 
     void ClickableDisplay::imgCallback(const sensor_msgs::ImageConstPtr& img_msg) {
         img_frame = img_msg->header.frame_id;
-        cv_bridge::CvImagePtr cv_ptr;
         try {
-            cv_ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::BGR8);
-            cv::imshow("Clickable World", cv_ptr->image);
-            cv::waitKey(3);
+            img_ptr = cv_bridge::toCvCopy(img_msg, sensor_msgs::image_encodings::BGR8);
+            have_img = true;
         }
         catch(cv_bridge::Exception& e) {
             ROS_ERROR("[clickable_display] cv_bridge exception: %s", e.what());
             return;
+        }
+    }
+
+    void ClickableDisplay::showImg() {
+        ros::Rate r(30);
+        while(ros::ok()) {
+            ros::spinOnce();
+            if(have_img) {
+                cv::imshow("Clickable World", img_ptr->image);
+                cv::waitKey(3);
+            }
+            r.sleep();
         }
     }
 
@@ -86,21 +84,15 @@ namespace hrl_clickable_world {
             click_pt.header.frame_id = this_->img_frame;
             click_pt.header.stamp = ros::Time::now();
             click_pt.point.x = x; click_pt.point.y = y;
-            this_->pt_pub.publish(click_pt);
+            this_->l_click_pub.publish(click_pt);
         }
-        if(event == CV_EVENT_RBUTTONDOWN && ros::Time::now().toSec() - this_->last_time > 1) {
-            this_->last_time = ros::Time::now().toSec();
-            std_srvs::EmptyRequest req; std_srvs::EmptyResponse resp;
-            this_->display_buttons_srv.call(req, resp);
+        if(event == CV_EVENT_RBUTTONDOWN) {
+            geometry_msgs::PointStamped click_pt;
+            click_pt.header.frame_id = this_->img_frame;
+            click_pt.header.stamp = ros::Time::now();
+            click_pt.point.x = x; click_pt.point.y = y;
+            this_->r_click_pub.publish(click_pt);
         }
-    }
-
-    void ClickableDisplay::perceiveButtonCallback(int state, void* data) {
-        ClickableDisplay* this_ = reinterpret_cast<ClickableDisplay*>(data);
-        ROS_INFO("%d", state);
-        return;
-        std_srvs::EmptyRequest req; std_srvs::EmptyResponse resp;
-        this_->display_buttons_srv.call(req, resp);
     }
 
     ClickableDisplay::~ClickableDisplay() {
@@ -117,6 +109,7 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "clickable_display");
     ClickableDisplay cd;
     cd.onInit();
+    cd.showImg();
     ros::spin();
     return 0;
 }
