230d1dfd9e0083c7e4adc5456e4ee744b2bacccb
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-06-07 22:09:23 +0000

    Added a special rendition of a FIR filter to the collision monitor.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1576 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_behaviors/pr2_collision_monitor/include/pr2_collision_monitor/fir_diff_filter.h
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <numeric>
+
+struct FIRDiffFilter {
+    std::vector<double> history;
+    int step;
+    double coll_sig, time_const;
+
+    FIRDiffFilter(int history_size=15, double time_constant=0.01) : 
+                                      history(hist_size), step(0), coll_sig(0.0) {
+    }
+    double updateState(double z_obs) {
+        // find the average value over the past few steps
+        double avg = std::accumulate(history.begin(), history.end(), 0.0) / history.size();
+        // exponential zeroing filter
+        // rapidly increases with every observation far from average
+        coll_sig = time_const * (coll_sig + z_obs - avg);
+        // add this observation to the history buffer
+        history[step % history.size()] = z_obs;
+        step++;
+        // return current state
+        return std::fabs(coll_sig);
+    }
+};
