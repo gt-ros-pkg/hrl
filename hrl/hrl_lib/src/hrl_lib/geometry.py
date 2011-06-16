85542f9022c6631fe7af3c2b8b357ecc1a112e5d
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-06-16 22:38:14 +0000

    M    hrl_lib/geometry.py distance along a curve using Shapely
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1677 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_lib/src/hrl_lib/geometry.py
+++ hrl/hrl_lib/src/hrl_lib/geometry.py
@@ -65,6 +65,14 @@ def distance_from_curve(pt, pts_list):
     s_pts_list = sg.LineString([m.A1 for m in pts_list])
     return s_pts_list.distance(spt)
 
+# distance of a point from a curve defined by a series of points.
+# pt - 2x1 or 3x1 np matrix
+# pts_list - list of 2x1 or 3x1 np matrices
+def distance_along_curve(pt, pts_list):
+    spt = sg.Point(pt.A1)
+    s_pts_list = sg.LineString([m.A1 for m in pts_list])
+    return s_pts_list.project(spt)
+
 def test_convex_hull():
     pp.axis('equal')
     pts = np.random.uniform(size=(40,2))
@@ -81,10 +89,11 @@ def test_convex_hull():
     pp.show()
 
 def test_distance_from_curve():
-    pt = np.matrix([1.0, 1.5, 0.3]).T
+    pt = np.matrix([0.8, 0.5, 0.3]).T
     pts_list = [[0, 0.], [1,0.], [1.,1.]]
     pts_l = [np.matrix(p).T for p in pts_list]
     print 'distance_from_curve:', distance_from_curve(pt, pts_l)
+    print 'distance_along_curve:', distance_along_curve(pt, pts_l)
     pp.axis('equal')
     pp.plot([pt[0,0]], [pt[1,0]], 'go', ms=7)
     pp.plot(*zip(*pts_list))
