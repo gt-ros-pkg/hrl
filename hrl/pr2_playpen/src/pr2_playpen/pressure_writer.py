22c259c90b46e5eb67f64c0c6bf9d018661bb339
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-06-29 00:44:08 +0000

    some intermediate changes before final testing tomorrow using pressure_writer from hrl_manipulation.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1750 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/pr2_playpen/src/pr2_playpen/pressure_writer.py
+++ hrl/pr2_playpen/src/pr2_playpen/pressure_writer.py
@@ -13,17 +13,20 @@ class PressureWriter:
         self.l_grip_press = pl.PressureListener(topic='/pressure/l_gripper_motor')
         self.r_grip_data = []
         self.l_grip_data = []
+        self.status = None
 
     def zero(self):
         self.r_grip_press.rezero()
         self.l_grip_press.rezero()
 
-    def record_pressures(self, file_name, arm, time = 10):
+    def record_pressures(self, file_name, arm, time = 10:)
         file_handle = open(file_name, 'w')
         self.zero()
         start = rospy.get_time()
         #might be better to do some condition, like gripper not moving or when arm is moved to side
-        while rospy.get_time()-start < time:
+        #while rospy.get_time()-start < time:
+        while not self.status == 'moving somewhere again':
+            print "saving data now ..."
             self.r_grip_data.append(self.r_grip_press.get_pressure_readings())
             self.l_grip_data.append(self.l_grip_press.get_pressure_readings())
             rospy.sleep(0.05)
@@ -36,10 +39,17 @@ class PressureWriter:
 #        print "here's the right gripper :\n", right_tuple
 #        print "here's the raw values : \n", self.r_grip_press.rmat_raw
 
+    def status_callback(self, data):
+        print "here is the status :", data
+#        self.status = data.feedback.state
+        self.status = data
+        
 if __name__ == '__main__':
     pw = PressureWriter()
+    rospy.Subscriber('OverheadServer/actionlib/feedback/feedback/state', String, pw.status_callback)
 
     while not rospy.is_shutdown():
-        pw.print_pressures()
-        rospy.sleep(0.5)
+        if pw.status == 'closing gripper':
+            pw.record_pressures('test_file', 0)
+        rospy.sleep(0.02)
 
