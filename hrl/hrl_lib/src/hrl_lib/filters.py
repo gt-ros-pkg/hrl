42f053ed1d309293ef070623682b3eb432443181
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-06-30 18:40:22 +0000

    A    hrl_lib/filters.py
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1760 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_lib/src/hrl_lib/filters.py
@@ -0,0 +1,70 @@
+#
+# Copyright (c) 2009, Georgia Tech Research Corporation
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the Georgia Tech Research Corporation nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY GEORGIA TECH RESEARCH CORPORATION ''AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL GEORGIA TECH BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+#  \author Advait Jain (Healthcare Robotics Lab, Georgia Tech.)
+
+
+import numpy as np
+
+# Code copied and modified from: http://www.scipy.org/Cookbook/KalmanFiltering
+class Kalman1D():
+    # Q - process variance
+    # R - measurement variance
+    # P - variance in my estimate
+    def __init__(self, P, Q, R):
+        self.P = float(P)
+        self.Q = float(Q)
+        self.R = float(R)
+        self.xhat = None
+
+    def predict(self, z):
+        if self.xhat == None:
+            self.xhat = z
+        #time update
+        xhatminus = self.xhat
+        Pminus = self.P + self.Q
+        #measurement update
+        K = Pminus / (Pminus + self.R)
+        self.xhat = xhatminus + K * (z-xhatminus)
+        self.P = (1-K) * Pminus
+        return self.xhat
+        
+
+
+# x - 1D np array
+def filter_array(x, P, Q, R):
+    kf = Kalman1D(P, Q, R)
+    x_filt = []
+    for z in x:
+        x_filt.append(kf.predict(z))
+#    print 'final value of P:', kf.P
+    return np.array(x_filt)
+
+
+
+
+
