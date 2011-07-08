fb3583dd89924ca5e01427f3b35d86c7d0ee61f9
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-07-08 05:30:17 +0000

    Added controllers.py to epc, currently only has PID control
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1783 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_arm_control/equilibrium_point_control/src/equilibrium_point_control/controllers.py
@@ -0,0 +1,33 @@
+
+##
+# Classic PID controller with maximum integral thresholding to
+# prevent windup.
+class PIDController(object):
+    def __init__(self, k_p, k_i, k_d, i_max, rate, name=None):
+        self.k_p = k_p
+        self.k_i = k_i
+        self.k_d = k_d
+        self.i_max = i_max
+        self.rate = rate
+        self.err_last = 0
+        self.integral = 0
+        if name is not None:
+            self.err_pub = rospy.Publisher("/pid_controller/" + name + "_error", Float64)
+            self.out_pub = rospy.Publisher("/pid_controller/" + name + "_output", Float64)
+
+    ##
+    # Updates the controller state and returns the current output.
+    def update_state(self, err):
+        self.integral += err / self.rate
+        self.integral = np.clip(self.integral, -self.i_max, self.i_max)
+        y = (self.k_p * err + 
+             self.k_d * (err - self.err_last) * self.rate +
+             self.k_i * self.integral)
+        self.err_last = err
+        self.err_pub.publish(err)
+        self.out_pub.publish(y)
+        return y
+
+    def reset_controller(self):
+        self.err_last = 0
+        self.integral = 0
