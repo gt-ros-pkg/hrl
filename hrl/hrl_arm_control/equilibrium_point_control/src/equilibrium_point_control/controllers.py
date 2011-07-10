Fixed jerk in PID derivative intialization for epc PID controller.
git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1789 5fc10e88-4e10-11de-b850-9977aa68d080

hrl/hrl_arm_control/equilibrium_point_control/src/equilibrium_point_control/controllers.py
@@ -1,3 +1,8 @@
+import numpy as np, math
+
+import roslib; roslib.load_manifest('equilibrium_point_control')
+import rospy
+from std_msgs.msg import Float64
 
 ##
 # Classic PID controller with maximum integral thresholding to
@@ -9,7 +14,7 @@ class PIDController(object):
         self.k_d = k_d
         self.i_max = i_max
         self.rate = rate
-        self.err_last = 0
+        self.err_last = None
         self.integral = 0
         if name is not None:
             self.err_pub = rospy.Publisher("/pid_controller/" + name + "_error", Float64)
@@ -20,6 +25,8 @@ class PIDController(object):
     def update_state(self, err):
         self.integral += err / self.rate
         self.integral = np.clip(self.integral, -self.i_max, self.i_max)
+        if self.err_last is None:
+            self.err_last = err
         y = (self.k_p * err + 
              self.k_d * (err - self.err_last) * self.rate +
              self.k_i * self.integral)
@@ -29,5 +36,5 @@ class PIDController(object):
         return y
 
     def reset_controller(self):
-        self.err_last = 0
+        self.err_last = None
         self.integral = 0