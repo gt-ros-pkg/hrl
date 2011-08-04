ead1ba3722e98a85503779e530fc1cfed64fe7bd
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-08-04 01:52:18 +0000

    PID cartesian EP control seems to work
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1895 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pid_cart_control.py
+++ hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pid_cart_control.py
@@ -2,18 +2,21 @@ import numpy as np, math
 
 import roslib; roslib.load_manifest('hrl_pr2_arms')
 import rospy
+from geometry_msgs.msg import PoseStamped
+import tf.transformations as tf_trans
 
 from equilibrium_point_control.controllers import PIDController
 from equilibrium_point_control.ep_control import EPGenerator, EPC, EPStopConditions
-from hrl_pr2_arms.pr2_arm import PR2ArmJointTrajectory
+from equilibrium_point_control.pose_converter import PoseConverter
+from hrl_pr2_arms.pr2_arm import PR2ArmCartesianBase
 from hrl_pr2_arms.pr2_arm_kinematics import PR2ArmKinematics
 
-class PIDJCFactory:
+class PIDCartFactory:
     def __init__(self, arm, err_goal_thresh, err_coll_thresh,
                  p_list, i_list, d_list, i_max_list, 
-                 duration=10., time_step=0.1):
-        self.jc_arm = PR2ArmJointTrajectory(arm, PR2ArmKinematics(arm))
-        self.jc_arm.reset_ep()
+                 duration=10., time_step=0.1, debug=False):
+        self.cart_arm = PR2ArmCartesianBase(arm, PR2ArmKinematics(arm))
+        self.cart_arm.reset_ep()
         self.err_goal_thresh = err_goal_thresh
         self.err_coll_thresh = err_coll_thresh
         self.p_list = p_list
@@ -22,17 +25,18 @@ class PIDJCFactory:
         self.i_max_list = i_max_list
         self.duration = duration
         self.time_step = time_step
+        self.debug = debug
 
-    def create_pid_jc(self, q_goal):
-        return PIDJointControl(self.jc_arm, q_goal, self.err_goal_thresh, self.err_coll_thresh,
+    def create_controller(self, cart_goal):
+        return PIDCartesianControl(self.cart_arm, cart_goal, self.err_goal_thresh, self.err_coll_thresh,
                                self.p_list, self.i_list, self.d_list, self.i_max_list, 
-                               self.duration, self.time_step)
+                               self.duration, self.time_step, self.debug)
 
 class PIDCartesianControl(EPGenerator):
     def __init__(self, cart_control_arm, cart_goal, err_goal_thresh, err_coll_thresh,
-                 p_list, i_list, d_list, i_max_list, duration=10., time_step=0.1):
-        self.jc_arm = joint_control_arm
-        self.q_goal = self.jc_arm.wrap_angles(q_goal)
+                 p_list, i_list, d_list, i_max_list, duration=10., time_step=0.1, debug=False):
+        self.cart_arm = cart_control_arm
+        self.cart_goal = cart_goal
         self.err_goal_thresh = np.array(err_goal_thresh)
         self.err_coll_thresh = np.array(err_coll_thresh)
         self.controllers = [PIDController(k_p, k_i, k_d, i_max, 1./time_step, 'pid_joint_%d' % i)
@@ -40,44 +44,69 @@ class PIDCartesianControl(EPGenerator):
                             zip(p_list, i_list, d_list, i_max_list, range(len(p_list)))]
         self.time_step = time_step
         self.num_steps = round(duration / time_step)
-        self.trajectory = self.jc_arm.interpolate_ep(self.jc_arm.get_ep(), self.q_goal, self.num_steps)
+        self.trajectory = self.cart_arm.interpolate_ep(self.cart_arm.get_ep(), self.cart_goal, self.num_steps)
         self.step_ind = 0
-        self.q_err = np.zeros(self.jc_arm.kinematics.n_jts)
+        self.cart_err = np.mat(np.zeros(6)).T
+        self.debug = debug
+        self.ep_pub = rospy.Publisher("pid_cartesian_control_ep", PoseStamped)
+        self.act_pub = rospy.Publisher("pid_cartesian_control_actual", PoseStamped)
+        self.goal_pub = rospy.Publisher("pid_cartesian_control_goal", PoseStamped)
 
     def generate_ep(self):
-        q_cur = self.jc_arm.get_joint_angles(True)
+        cart_cur = self.cart_arm.get_end_effector_pose()
         if self.step_ind < self.num_steps:
-            q_cur_goal = self.trajectory[self.step_ind]
+            self.cart_cur_goal = self.trajectory[self.step_ind]
             self.step_ind += 1
         else:
-            q_cur_goal = self.q_goal
-        self.q_err = q_cur_goal - q_cur
-        self.q_err[np.where(np.fabs(self.q_err) < self.err_goal_thresh * 0.6)] = 0.0
-        q_control = np.array([pidc.update_state(q_err_i) for pidc, q_err_i in 
-                              zip(self.controllers, self.q_err)])
-        self.q_control = q_control
-        jep_cur = np.array(self.jc_arm.get_ep())
-        ep = jep_cur + q_control
-        return EPStopConditions.CONTINUE, ep
+            self.cart_cur_goal = self.cart_goal
+        self.cart_err = self.cart_arm.ep_error(cart_cur, self.cart_cur_goal)
+#self.cart_err[np.where(np.fabs(self.cart_err) < self.err_goal_thresh * 0.6)] = 0.0
+        cart_control = np.array([pidc.update_state(cart_err_i) for pidc, cart_err_i in 
+                                zip(self.controllers, self.cart_err)])
+        cep_pos_cur, cep_rot_cur = self.cart_arm.get_ep()
+        cep_pos_new = cep_pos_cur - cart_control[:3,0]
+        cart_rot_control = np.mat(tf_trans.euler_matrix(cart_control[3,0], cart_control[4,0], 
+                                                 cart_control[5,0]))[:3,:3]
+        cep_rot_new = cep_rot_cur * cart_rot_control.T
+        ep_new = (cep_pos_new, cep_rot_new)
+        if self.debug:
+            print "="*50
+            print "cart_cur", cart_cur
+            print "-"*50
+            print "cur goal", self.cart_cur_goal
+            print "-"*50
+            print "err", self.cart_err
+            print "-"*50
+            print "cart_control", cart_control
+        return EPStopConditions.CONTINUE, ep_new
 
     def control_ep(self, ep):
-        wrapped_ep = self.jc_arm.wrap_angles(ep)
-        self.jc_arm.set_ep(wrapped_ep, self.time_step*1.5)
+        cart_cur = self.cart_arm.get_end_effector_pose()
+        self.ep_pub.publish(PoseConverter.to_pose_stamped_msg('torso_lift_link', ep))
+        self.act_pub.publish(PoseConverter.to_pose_stamped_msg('torso_lift_link', cart_cur))
+        self.goal_pub.publish(PoseConverter.to_pose_stamped_msg('torso_lift_link', self.cart_cur_goal))
+        if self.debug:
+            print "-"*50
+            print "ep:", ep
+        self.cart_arm.set_ep(ep, self.time_step*1.5)
 
     def clamp_ep(self, ep):
         # TODO Add joint limits?
         return ep
         
     def terminate_check(self):
-        ep_q_diff = np.array(self.jc_arm.get_ep()) - self.jc_arm.get_joint_angles(True)
-        if np.any(np.fabs(ep_q_diff) > self.err_coll_thresh):
+        cart_cur = self.cart_arm.get_end_effector_pose()
+        ep_actual_diff = self.cart_arm.ep_error(self.cart_arm.get_ep(), cart_cur)
+        if self.debug:
+            print "ep_actual_diff", ep_actual_diff, self.err_coll_thresh
+        if np.any(np.fabs(ep_actual_diff.T.A[0]) > self.err_coll_thresh):
             return EPStopConditions.COLLISION
 
         if self.step_ind < self.num_steps:
             return EPStopConditions.CONTINUE
         else:
             if (self.step_ind == self.num_steps and 
-                np.all(np.fabs(self.q_err) < self.err_goal_thresh)):
+                np.all(np.fabs(self.cart_err.T.A[0]) < self.err_goal_thresh)):
                 return EPStopConditions.SUCCESSFUL
             else:
                 return EPStopConditions.CONTINUE
