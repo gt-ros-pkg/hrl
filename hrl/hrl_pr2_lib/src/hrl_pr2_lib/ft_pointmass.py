a2e667d8f1ff5fd6d0ce01d99642741890061ea5
Phillip.Grice@gmail.com <Phillip.Grice@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-08-10 13:51:51 +0000

    updates using new frame in urdf, aligned with raw netft values
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1934 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_pr2_lib/src/hrl_pr2_lib/ft_pointmass.py
+++ hrl/hrl_pr2_lib/src/hrl_pr2_lib/ft_pointmass.py
@@ -8,14 +8,16 @@ from tf import TransformListener, transformations
 from visualization_msgs.msg import Marker
 
 class Pointmass_Adjust:
-    
+   
+    pub_marker = True #Set to (True) or to not(False) publish rviz marker showing force vector
+
     mass = 1.0213 #kg
     pos_x = 0.0853 #m, in 'l_wrist_roll_link'
     pos_y = 0.0
     pos_z = 0.0
-    x_force_offset = 3.85 #3.88 -- These values determined from experiment, used values are adjusted for better qualitative results using rviz
-    y_force_offset = 13.96 #14.10
-    z_force_offset = -5.72 #-5.70
+    x_force_offset = 5.72 #5.70 -- These values determined from experiment, used values are adjusted for better qualitative results using rviz
+    y_force_offset = -13.96 #14.10
+    z_force_offset = 3.85 #-3.88
     x_torque_offset = -0.21875
     y_torque_offset = 0.3804
     z_torque_offset = 0.3899
@@ -23,55 +25,43 @@ class Pointmass_Adjust:
 
     def __init__(self):
         rospy.init_node("ft_pointmass_adjustment")
-        rospy.Subscriber("netft_data", WrenchStamped, self.transform)
+        rospy.Subscriber("netft_data", WrenchStamped, self.adjust)
         self.ft_out = rospy.Publisher('ft_data_pm_adjusted', WrenchStamped)
-        self.l_wrist_ft_out = rospy.Publisher('ft_data_raw_framed', WrenchStamped)
         self.force_vec_out = rospy.Publisher('ft_force_vector_marker', Marker)
         self.tfl = TransformListener()
-
-    def transform(self, ft_in):
-        ft_in_wrist = WrenchStamped()
-        ft_in_wrist.header.stamp = ft_in.header.stamp
-        ft_in_wrist.header.frame_id = '/l_netft_frame'
-        ft_in_wrist.wrench.force.x = ft_in.wrench.force.z
-        ft_in_wrist.wrench.torque.x = ft_in.wrench.torque.z
-        ft_in_wrist.wrench.force.y = -ft_in.wrench.force.y
-        ft_in_wrist.wrench.torque.y = -ft_in.wrench.torque.y
-        ft_in_wrist.wrench.force.z = -ft_in.wrench.force.x
-        ft_in_wrist.wrench.torque.z = -ft_in.wrench.torque.x
-        self.l_wrist_ft_out.publish(ft_in_wrist)
-        #self.calc_adjustment()
-        self.adjust(ft_in_wrist)
 
     def adjust(self, ft_in):
         ft_out = WrenchStamped()
         ft_out.header.stamp = rospy.Time.now()
-        ft_out.header.frame_id = ft_in.header.frame_id #'l_wrist_roll_link' # OR PROPER FRAME FOR FT SENSOR
-        ft_out.wrench.force.x = ft_in.wrench.force.x - self.adjustment.wrench.force.x - self.x_force_offset
-        ft_out.wrench.force.y = ft_in.wrench.force.y - self.adjustment.wrench.force.y - self.y_force_offset
-        ft_out.wrench.force.z = ft_in.wrench.force.z - self.adjustment.wrench.force.z - self.z_force_offset
+        ft_out.header.frame_id = ft_in.header.frame_id
+        ft_out.wrench.force.x = ft_in.wrench.force.x  - self.x_force_offset + self.adjustment.wrench.force.x
+        ft_out.wrench.force.y = ft_in.wrench.force.y  - self.y_force_offset + self.adjustment.wrench.force.y
+        ft_out.wrench.force.z = ft_in.wrench.force.z  - self.z_force_offset + self.adjustment.wrench.force.z
         ft_out.wrench.torque.x = ft_in.wrench.torque.x - self.x_torque_offset 
-        ft_out.wrench.torque.y = ft_in.wrench.torque.y - self.adjustment.wrench.torque.y - self.y_torque_offset
-        ft_out.wrench.torque.z = ft_in.wrench.torque.z - self.adjustment.wrench.torque.z - self.z_torque_offset
+        ft_out.wrench.torque.y = ft_in.wrench.torque.y  - self.y_torque_offset - self.adjustment.wrench.torque.y
+        ft_out.wrench.torque.z = ft_in.wrench.torque.z  - self.z_torque_offset - self.adjustment.wrench.torque.z
 
         self.ft_out.publish(ft_out)
+        if self.pub_marker:
+            marker = self.form_marker(ft_out)
+            self.force_vec_out.publish(marker) 
 
+    def form_marker(self, ws):
         origin = Point()
         force_point = Point()
-        force_point.x = 0.1*ft_out.wrench.force.x
-        force_point.y = 0.1*ft_out.wrench.force.y
-        force_point.z = 0.1*ft_out.wrench.force.z
+        force_point.x = 0.1*ws.wrench.force.x
+        force_point.y = 0.1*ws.wrench.force.y
+        force_point.z = 0.1*ws.wrench.force.z
         force_vec = Marker()
         force_vec.header.stamp = rospy.Time.now()
         force_vec.header.frame_id = '/l_netft_frame'
         force_vec.ns = "ft_sensor"
-        force_vec.id = 1
         force_vec.action = 0
         force_vec.type = 0
         force_vec.scale.x = 0.1
         force_vec.scale.y = 0.2
         force_vec.scale.z = 1
-        force_vec.color.a = 1.0
+        force_vec.color.a = 0.75
         force_vec.color.r = 0.0
         force_vec.color.g = 1.0
         force_vec.color.b = 0.1
@@ -79,7 +69,7 @@ class Pointmass_Adjust:
         force_vec.lifetime = rospy.Duration(1)
         force_vec.points.append(origin)
         force_vec.points.append(force_point)
-        self.force_vec_out.publish(force_vec) 
+        return force_vec
 
     def calc_adjustment(self):
         try:
@@ -87,8 +77,8 @@ class Pointmass_Adjust:
         except:
             return
         rot = transformations.euler_from_quaternion(quat) 
-        self.adjustment.wrench.torque.y = self.mass*self.pos_x*math.cos(rot[1])
-        self.adjustment.wrench.torque.z = self.mass*self.pos_x*math.sin(rot[0])
+        ###############self.adjustment.wrench.torque.y = self.mass*self.pos_x*math.cos(rot[1])
+        ###########self.adjustment.wrench.torque.z = self.mass*self.pos_x*math.sin(rot[0])
       
         grav = PointStamped() # Generate a 'vector' of the force due to gravity at the ft sensor 
         grav.header.stamp = rospy.Time(0) #Used to tell tf to grab latest available transform in transformVector3
@@ -101,6 +91,10 @@ class Pointmass_Adjust:
         self.adjustment.wrench.force.x = netft_grav.point.x
         self.adjustment.wrench.force.y = netft_grav.point.y
         self.adjustment.wrench.force.z = netft_grav.point.z
+        print "x:%s, y:%s, z:%s" %(netft_grav.point.x, netft_grav.point.y, netft_grav.point.z)
+
+        self.adjustment.header.stamp=rospy.Time.now()
+        #self.form_marker(self.adjustment)
 
 if __name__ == "__main__":
     PMC = Pointmass_Adjust()
