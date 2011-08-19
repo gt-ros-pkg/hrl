69e322e35e927d46722fea474af657c2a1e4ef9c
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-08-19 01:28:21 +0000

    added code at bottom for template for image result analysis instead of 3D
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@1968 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/pr2_playpen/src/pr2_playpen/analyze_playpen_cloud.py
+++ hrl/pr2_playpen/src/pr2_playpen/analyze_playpen_cloud.py
@@ -211,3 +211,52 @@ if __name__ == '__main__':
     ra = ResultsAnalyzer()
     ra.run()
 
+
+
+# import roslib
+# roslib.load_manifest('my_package')
+# import sys
+# import rospy
+# import cv
+# from std_msgs.msg import String
+# from sensor_msgs.msg import Image
+# from cv_bridge import CvBridge, CvBridgeError
+
+# class image_converter:
+
+#   def __init__(self):
+#     self.image_pub = rospy.Publisher("image_topic_2",Image)
+
+#     cv.NamedWindow("Image window", 1)
+#     self.bridge = CvBridge()
+#     self.image_sub = rospy.Subscriber("image_topic",Image,self.callback)
+
+#   def callback(self,data):
+#     try:
+#       cv_image = self.bridge.imgmsg_to_cv(data, "bgr8")
+#     except CvBridgeError, e:
+#       print e
+
+#     (cols,rows) = cv.GetSize(cv_image)
+#     if cols > 60 and rows > 60 :
+#       cv.Circle(cv_image, (50,50), 10, 255)
+
+#     cv.ShowImage("Image window", cv_image)
+#     cv.WaitKey(3)
+
+#     try:
+#       self.image_pub.publish(self.bridge.cv_to_imgmsg(cv_image, "bgr8"))
+#     except CvBridgeError, e:
+#       print e
+
+# def main(args):
+#   ic = image_converter()
+#   rospy.init_node('image_converter', anonymous=True)
+#   try:
+#     rospy.spin()
+#   except KeyboardInterrupt:
+#     print "Shutting down"
+#   cv.DestroyAllWindows()
+
+# if __name__ == '__main__':
+#     main(sys.argv)
