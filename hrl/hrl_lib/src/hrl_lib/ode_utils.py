eb804d30e57cea30a2051e7dc6196c2efaeeca72
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-09-01 21:39:33 +0000

    M    hrl_lib/kdl_utils.py minor.
    A    hrl_lib/ode_utils.py
    copied Marc's function into an ode_utils file.
    
    
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2034 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_lib/src/hrl_lib/ode_utils.py
+++ hrl/hrl_lib/src/hrl_lib/ode_utils.py
@@ -25,37 +25,26 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+#  \author Marc Killpack (Healthcare Robotics Lab, Georgia Tech.)
 #  \author Advait Jain (Healthcare Robotics Lab, Georgia Tech.)
 
+# ODE - Open Dynamics Engine.
 
 import numpy as np, math
 
 import roslib
 roslib.load_manifest('hrl_lib')
 
-import PyKDL as kdl
+
+# take ODE's rotation (as returned by getRotation) and convert to a
+# rotation matrix.
+def ode_roation_to_matrix(self, rot):
+    rot_mat = np.matrix([[rot[0], rot[4], rot[8]],
+                         [rot[1], rot[5], rot[9]],
+                         [rot[2], rot[6], rot[10]]])
+    return rot_mat
 
 
-## kdl vector -> 3x1 np matrix.
-def kdl_vec_to_np(kdl_vec):
-    v = np.matrix([kdl_vec[0],kdl_vec[1],kdl_vec[2]]).T
-    return v
-
-## kdl rotation matrix -> 3x3 numpy matrix.
-def kdl_rot_to_np(kdl_rotation):
-    m = kdl_rotation
-    rot = np.matrix([[m[0,0],m[0,1],m[0,2]],
-                     [m[1,0],m[1,1],m[1,2]],
-                     [m[2,0],m[2,1],m[2,2]]])
-    return rot
-
-## 3x1 np vector -> KDL Vector
-def np_vec_to_kdl(p):
-    return kdl.Vector(p[0,0],p[1,0],p[2,0])
-
-## 3x3 np rotation matrix -> KDL Rotation.
-def np_rot_to_kdl(rot):
-    return kdl.Rotation(rot[0,0],rot[0,1],rot[0,2],rot[1,0],rot[1,1],rot[1,2],rot[2,0],rot[2,1],rot[2,2])
 
 
 
