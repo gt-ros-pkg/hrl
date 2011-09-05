1e3a09374f956058962639d00c5be9993e498ae4
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-09-05 22:20:26 +0000

    Added pub_head to hrl_phri_2011, a simple script for publishing pointclouds from a bag.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2042 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_experiments/hrl_phri_2011/src/pub_head.cpp
@@ -0,0 +1,20 @@
+#include <hrl_phri_2011/pc_utils.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "pc_head_stitcher");
+
+    // Load bag
+    rosbag::Bag bag;
+    bag.open(std::string(argv[1]), rosbag::bagmode::Read);
+    rosbag::View view(bag, rosbag::TopicQuery("/stitched_head"));
+
+    PCRGB::Ptr pc_head(new PCRGB());
+    BOOST_FOREACH(rosbag::MessageInstance const m, view) {
+        sensor_msgs::PointCloud2::Ptr pc2 = m.instantiate<sensor_msgs::PointCloud2>();
+        pcl::fromROSMsg(*pc2, *pc_head);
+        break;
+    }
+    pubLoop(*pc_head, "/stitched_head");
+    return 0;
+}
