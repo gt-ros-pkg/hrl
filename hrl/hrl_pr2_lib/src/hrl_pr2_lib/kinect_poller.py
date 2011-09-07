87ee116eed79699bb457e9e6ed928f3d3560f043
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-09-07 03:10:54 +0000

    Fixed kinect_poller not to rely on hrl_pr2_lib.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2045 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_pr2_lib/src/hrl_pr2_lib/kinect_poller.py
+++ hrl/hrl_pr2_lib/src/hrl_pr2_lib/kinect_poller.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
-import roslib; roslib.load_manifest("hrl_pr2_lib")
+import roslib
+roslib.load_manifest("rospy")
+roslib.load_manifest("sensor_msgs")
 import rospy
 
 from sensor_msgs.msg import PointCloud2
@@ -14,7 +16,12 @@ node_name = 'kinect_poller'
 rospy.init_node(node_name)
 pc_topic = rospy.get_param(node_name+'/topic_name')
 poller = Poller()
-rospy.sleep(8.0)
+rospy.sleep(1.0)
 sub = rospy.Subscriber(pc_topic, PointCloud2, poller.callback)
-print "Subcribed to %s" % pc_topic
+r = rospy.Rate(100)
+while not rospy.is_shutdown():
+    if poller.ind != 0:
+        break
+    r.sleep()
+print "Polling %s" % pc_topic
 rospy.spin()
