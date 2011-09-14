435ce4e688220ecc690384afb0b7bfb2d54f7438
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-09-14 14:28:50 +0000

    Cleaned up pr2_arm_hybrid interface.  Added nicer ways of specifying gains.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2109 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pr2_arm_hybrid.py
+++ hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pr2_arm_hybrid.py
@@ -13,19 +13,68 @@ from hrl_pr2_arms.pr2_arm import PR2ArmCartesianBase
 from hrl_netft.msg import HybridCartesianGains
 
 class PR2ArmHybridForce(PR2ArmCartesianBase):
-    def __init__(self, arm, kinematics):
+    def __init__(self, arm, kinematics, controller_name='/%s_cart'):
         super(PR2ArmHybridForce, self).__init__(arm, kinematics)
-        self.command_gains_pub = rospy.Publisher('/' + arm + '_cart/gains', HybridCartesianGains)
-        self.command_force_pub = rospy.Publisher('/' + arm + '_cart/command_force', Float64MultiArray)
+        if '%s' in controller_name:
+            controller_name = controller_name % arm
+        self.command_gains_pub = rospy.Publisher(controller_name + '/gains', HybridCartesianGains)
+        self.command_force_pub = rospy.Publisher(controller_name + '/command_force', Float64MultiArray)
+        self.tip_frame = rospy.get_param(controller_name + '/tip_name')
+        self.force_selector = 6 * [0]
+        self.trans_p_motion_gains = 3 * [rospy.get_param(controller_name + '/cart_gains/trans/p')]
+        self.trans_d_motion_gains = 3 * [rospy.get_param(controller_name + '/cart_gains/trans/d')]
+        self.rot_p_motion_gains = 3 * [rospy.get_param(controller_name + '/cart_gains/rot/p')]
+        self.rot_d_motion_gains = 3 * [rospy.get_param(controller_name + '/cart_gains/rot/d')]
+        self.trans_p_force_gains = 3 * [rospy.get_param(controller_name + '/force_gains/trans/p')]
+        self.trans_d_force_gains = 3 * [rospy.get_param(controller_name + '/force_gains/trans/d')]
+        self.rot_p_force_gains = 3 * [rospy.get_param(controller_name + '/force_gains/rot/p')]
+        self.rot_d_force_gains = 3 * [rospy.get_param(controller_name + '/force_gains/rot/d')]
         rospy.sleep(1)
 
-    def set_gains(self, p_gains, d_gains, fp_gains, fd_gains, force_selector, frame='%s_gripper_tool_frame'):
-        if '%s' in frame:
-            frame = frame % self.arm
-        all_gains = list(p_gains) + list(d_gains)
-        all_fgains = list(fp_gains) + list(fd_gains)
-        gains_msg = HybridCartesianGains(Header(0, rospy.Time.now(), frame),
-                                         all_gains, all_fgains, force_selector)
+    def set_motion_gains(self, p_trans=None, p_rot=None, d_trans=None, d_rot=None):
+        local_names = ['trans_p_motion_gains', 'trans_d_motion_gains',
+                       'rot_p_motion_gains', 'rot_d_motion_gains']
+        vals = [p_trans, p_rot, d_trans, d_rot]
+        for local_name, val in zip(local_names, vals):
+            self._set_gain(local_name, val)
+
+    def set_force_gains(self, p_trans=None, p_rot=None, d_trans=None, d_rot=None):
+        local_names = ['trans_p_force_gains', 'trans_d_force_gains',
+                       'rot_p_force_gains', 'rot_d_force_gains']
+        vals = [p_trans, p_rot, d_trans, d_rot]
+        for local_name, val in zip(local_names, vals):
+            self._set_gain(local_name, val)
+
+    def _set_gain(self, local_name, val):
+        if val is not None:
+            try:
+                if len(val) == 3:
+                    self.__dict__[local_name] = list(val)
+                elif type(p_trans) in [int, float]:
+                    self.__dict__[local_name] = 3 * [float(val)]
+            except Exception as e:
+                pass
+
+    def set_tip_frame(self, tip_frame):
+        self.tip_frame = tip_frame
+
+    def set_force_directions(self, directions):
+        if len(directions) == 6 and all([direction in [0, 1] for direction in directions]):
+            self.force_selector = list(directions):
+            return
+        self.force_selector = 6 * [0]
+        names = ['x', 'y', 'z', 'rx', 'ry', 'rz']
+        for direction in directions:
+            if direction in names:
+                self.force_selector[names.index(direction)] = 1
+
+    def _update_gains(self):
+        motion_gains = (self.trans_p_motion_gains + self.rot_p_motion_gains + 
+                        self.trans_d_motion_gains + self.rot_d_motion_gains)
+        force_gains = (self.trans_p_force_gains + self.rot_p_force_gains + 
+                       self.trans_d_force_gains + self.rot_d_force_gains)
+        gains_msg = HybridCartesianGains(Header(0, rospy.Time.now(), self.tip_frame),
+                                         motion_gains, force_gains, self.force_selector)
         self.command_gains_pub.publish(gains_msg)
 
     def set_force(self, f):
