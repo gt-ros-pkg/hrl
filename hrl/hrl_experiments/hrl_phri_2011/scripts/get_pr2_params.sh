464ecc9a3b6f32e8d9a1eafc4626ced52d0d897d
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-10-06 18:40:41 +0000

    Added analysis script to extract percentiles from PR2 interaction
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2178 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/hrl_experiments/hrl_phri_2011/scripts/get_pr2_params.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+pkg=`rospack find hrl_phri_2011`
+source $pkg/scripts/variables.sh
+set -x
+rosrun hrl_phri_2011 analyze_pr2_interaction.py $dir/${people[$1]}_${tools[$2]}_treatment_$3_pr2.bag
---
+++ hrl/hrl_experiments/hrl_phri_2011/src/hrl_phri_2011/analyze_pr2_interaction.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+import sys
+import numpy as np
+import scipy.io
+import scipy.stats
+import matplotlib.pyplot as plt
+
+import roslib
+roslib.load_manifest('hrl_phri_2011')
+import rospy
+
+import rosbag
+
+def main():
+    bag = rosbag.Bag(sys.argv[1], 'r')
+    x_forces, cur_x_forces = [], []
+    for topic, msg, t in bag.read_messages(topics=["/l_cart/sensor_ft"]):
+        f = [msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z]
+        f_mag = np.linalg.norm(f)
+        if f_mag > 0.5:
+            cur_x_forces.append(msg.wrench.force.x)
+        else:
+            if len(cur_x_forces) >= 20:
+                x_forces.extend(cur_x_forces)
+            cur_x_forces = []
+    if len(cur_x_forces) >= 20:
+        x_forces.extend(cur_x_forces)
+    bag.close()
+    ptile_inds = [25, 50, 75, 95]
+    ptiles = {}
+    for ptile in ptile_inds:
+        ptiles[ptile] = scipy.stats.scoreatpercentile(x_forces, ptile)
+        print "Percentile %d: %f" % (ptile, ptiles[ptile])
+    plt.plot(x_forces)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
