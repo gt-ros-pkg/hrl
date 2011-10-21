a5fe6a327a764dba4b48ed45b221fdeef8381f6f
Phillip.Grice@gmail.com <Phillip.Grice@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-10-21 18:07:08 +0000

    switched poses to other side of face
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2216 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_experiments/hrl_rfh_fall_2011/src/hrl_rfh_fall_2011/head_tool_pose_server.py
+++ hrl/hrl_experiments/hrl_rfh_fall_2011/src/hrl_rfh_fall_2011/head_tool_pose_server.py
@@ -19,14 +19,14 @@ from hrl_rfh_fall_2011.srv import GetHeadPose
 
 head_poses = {
     #             lat   lon    height    roll   pitch   yaw
-    "near_ear" : [(4 * np.pi/8,    -3 * np.pi/8,     1),      (0,     0,      0)],
-    "upper_cheek" : [(4 * np.pi/8,    -1.5 * np.pi/8,     1),      (0,     0,      0)],
-    "middle_cheek" : [(4.5 * np.pi/8,    -2 * np.pi/8,     1),      (0,     0,      0)],
-    "jaw_bone" : [(5.1 * np.pi/8,    -2 * np.pi/8,     1),      (0,     0,      0)],
-    "back_neck" : [(5.1 * np.pi/8,    -3 * np.pi/8,     1),      (0,     0,      0)],
+    "near_ear" : [(4 * np.pi/8,    3 * np.pi/8,     1),      (0,     0,      0)],
+    "upper_cheek" : [(4 * np.pi/8,    1.5 * np.pi/8,     1),      (0,     0,      0)],
+    "middle_cheek" : [(4.5 * np.pi/8,    2 * np.pi/8,     1),      (0,     0,      0)],
+    "jaw_bone" : [(5.1 * np.pi/8,    2 * np.pi/8,     1),      (0,     0,      0)],
+    "back_neck" : [(5.1 * np.pi/8,    3 * np.pi/8,     1),      (0,     0,      0)],
     "nose" : [(4 * np.pi/8,    0 * np.pi/8,     1),      (0,     0,      0)],
     "chin" : [(5.4 * np.pi/8,    0 * np.pi/8,     1),      (np.pi / 2,    np.pi/8,      0)],
-    "mouth_corner" : [(4.5 * np.pi/8,    -0.9 * np.pi/8,     1),      (0,     0,      0)]
+    "mouth_corner" : [(4.5 * np.pi/8,    0.9 * np.pi/8,     1),      (0,     0,      0)]
 }
 
 def create_arrow_marker(pose, m_id, color=ColorRGBA(1., 0., 0., 1.)):
