bff6ab73ec7c19505bd28464f2bbc4ad2a0b1f1e
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-10-24 21:08:16 +0000

    Real carefree_switch commit
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2230 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pr2_controller_switcher.py
+++ hrl/hrl_arm_control/hrl_pr2_arms/src/hrl_pr2_arms/pr2_controller_switcher.py
@@ -1,8 +1,11 @@
 import roslib; roslib.load_manifest('hrl_pr2_arms')
 import rospy
 import rosparam
+import roslib.substitution_args
 
-from pr2_mechanism_msgs.srv import LoadController, UnloadController, SwitchController
+from pr2_mechanism_msgs.srv import LoadController, UnloadController, SwitchController, ListControllers
+
+POSSIBLE_ARM_CONTROLLERS = ['%s_arm_controller', '%s_cart']
 
 ##
 # Offers controller switching inside python on the fly.
@@ -14,6 +17,8 @@ class ControllerSwitcher:
                                                     UnloadController)
         self.switch_controller_srv = rospy.ServiceProxy('pr2_controller_manager/switch_controller', 
                                                         SwitchController)
+        self.list_controllers_srv = rospy.ServiceProxy('pr2_controller_manager/list_controllers', 
+                                                       ListControllers)
 
     ##
     # Switches controller.
@@ -29,7 +34,7 @@ class ControllerSwitcher:
             self.unload_controller(old_controller)
             return resp.ok
         else:
-            params = rosparam.load_file(param_file)
+            params = rosparam.load_file(roslib.substitution_args.resolve_args(param_file))
             rosparam.upload_params("", params[0][0])
             self.switch_controller_srv([], [old_controller], 1)
             self.unload_controller(old_controller)
@@ -40,3 +45,29 @@ class ControllerSwitcher:
                 self.load_controller(new_controller)
             resp = self.switch_controller_srv([new_controller], [], 1)
             return resp.ok
+
+    ##
+    # Switches controller without having to specify the arm controller to take down.
+    # @param arm (r/l)
+    # @param new_controller Name of new controller to load
+    # @param param_file YAML file containing parameters for the new controller.
+    # @param possible_controllers List of possible controller names like "%s_cart" to lookup and take down
+    # @return Success of switch.
+    def carefree_switch(self, arm, new_controller, param_file=None, possible_controllers=None):
+        if '%s' in new_controller:
+            new_controller = new_controller % arm
+        if param_file is not None:
+            params = rosparam.load_file(roslib.substitution_args.resolve_args(param_file))
+            rosparam.upload_params("", params[0][0])
+        if possible_controllers is None:
+            possible_controllers = POSSIBLE_ARM_CONTROLLERS
+        check_arm_controllers = [controller % arm for controller in possible_controllers]
+        resp = self.list_controllers_srv()
+        start_controllers, stop_controllers = [new_controller], []
+        for i, controller in enumerate(resp.controllers):
+            if controller in check_arm_controllers and resp.state[i] == 'running':
+                stop_controllers.append(controller)
+        resp = self.switch_controller_srv(start_controllers, stop_controllers, 1)
+        return resp.ok
+
+
