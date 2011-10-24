20299a36b418aa5202db6d3b8539e5d73663dc78
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-10-24 21:08:07 +0000

    Added carefree_switch to switch_controller.  This way we don't have to worry about killing the right controller.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2228 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_arm_control/hrl_pr2_arms/scripts/switch_controller.py
+++ hrl/hrl_arm_control/hrl_pr2_arms/scripts/switch_controller.py
@@ -9,9 +9,12 @@ from hrl_pr2_arms.pr2_controller_switcher import ControllerSwitcher
 
 def main():
     rospy.init_node("switch_controller")
-    prefix = "/".join(sys.argv[0].split("/")[:-2]) + "/params/"
     cs = ControllerSwitcher()
     if len(sys.argv) >= 4:
+        if "find" in sys.argv[3]
+            prefix = ""
+        else:
+            prefix = "$(find hrl_pr2_arms)/params/"
         print cs.switch(sys.argv[1], sys.argv[2], prefix + sys.argv[3])
     else:
         print cs.switch(sys.argv[1], sys.argv[2])
