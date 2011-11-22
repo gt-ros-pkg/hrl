11a172473f5a00078d0622e3793bc112559b5f50
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-11-22 18:59:39 +0000

    M    simple_occupancy_grid/src/examples/og_sample.cpp publishing but not showing up in rviz.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2350 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/simple_occupancy_grid/src/examples/og_sample.cpp
+++ hrl/simple_occupancy_grid/src/examples/og_sample.cpp
@@ -1,19 +1,64 @@
 
 #include "simple_occupancy_grid/occupancy_grid.h"
-//#include "point_cloud_ros/OccupancyGrid.h"
 
-#include "ros/ros.h"
-#include "ros/console.h"
+#include "hrl_msgs/FloatArrayBare.h"
 
-//#include "pcl/io/pcd_io.h"
-//#include "sensor_msgs/point_cloud_conversion.h"
+#include <ros/ros.h>
+#include <ros/console.h>
+
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "occupancy_grid_node");
-    ros::NodeHandle n;
+    ros::NodeHandle nh("~");
 
     ROS_INFO("Hello World");
+
+    occupancy_grid::OccupancyGrid og(nh,
+                                     0.5, 0., 1.0,
+                                     0.5, 0.5, 0.5,
+                                     0.02, 0.02, 0.02);
+
+    hrl_msgs::FloatArrayBare fa;
+
+    fa.data.push_back(0.5);
+    fa.data.push_back(0.);
+    fa.data.push_back(1.);
+
+    fa.data.push_back(0.6);
+    fa.data.push_back(0.);
+    fa.data.push_back(1.);
+
+    fa.data.push_back(0.4);
+    fa.data.push_back(0.);
+    fa.data.push_back(1.);
+
+    fa.data.push_back(0.5);
+    fa.data.push_back(0.1);
+    fa.data.push_back(1.);
+
+    fa.data.push_back(0.5);
+    fa.data.push_back(-0.1);
+    fa.data.push_back(1.);
+
+    fa.data.push_back(0.5);
+    fa.data.push_back(0.);
+    fa.data.push_back(1.1);
+
+    fa.data.push_back(0.5);
+    fa.data.push_back(0.);
+    fa.data.push_back(0.9);
+
+    og.addPointsUnstamped(fa);
+
+
+    ros::Rate loop_rate(1);
+
+    while(ros::ok())
+    {
+        og.publishMarkerArray_simple();
+        loop_rate.sleep();
+    }
 
 }
 
