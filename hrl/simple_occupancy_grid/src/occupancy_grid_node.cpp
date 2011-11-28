c335c5ce4f18de4daff70e486b8d5688fef1b79a
advaitjain <advaitjain@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-11-28 16:53:48 +0000

    M    src/occupancy_grid_node.cpp starting with an empty grid and changed the resolution to 1cm. at some point of time I should make this a ROS service, or at least command line params (ideally both)
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2367 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/simple_occupancy_grid/src/occupancy_grid_node.cpp
+++ hrl/simple_occupancy_grid/src/occupancy_grid_node.cpp
@@ -8,14 +8,14 @@ int main (int argc, char *argv[])
     ros::init(argc, argv, "occupancy_grid_node");
 
     occupancy_grid::OccupancyGrid og(0.5, 0., 1.0,
-                                     0.5, 0.5, 0.5,
-                                     0.02, 0.02, 0.02);
+                                     1.0, 1.0, 0.5,
+                                     0.01, 0.01, 0.01);
 
     hrl_msgs::FloatArrayBare fa;
 
-    fa.data.push_back(0.5);
-    fa.data.push_back(0.);
-    fa.data.push_back(1.);
+//    fa.data.push_back(0.5);
+//    fa.data.push_back(0.);
+//    fa.data.push_back(1.);
 
 //    fa.data.push_back(0.6);
 //    fa.data.push_back(0.);
