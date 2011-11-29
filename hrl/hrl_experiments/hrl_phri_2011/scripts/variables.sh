b2001a9e2a5a45bfc4ec49fd8a12d839150eb349
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-11-29 19:29:34 +0000

    Updated variables for phri script so that subject numbers make more sense.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2374 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/hrl_experiments/hrl_phri_2011/scripts/variables.sh
+++ hrl/hrl_experiments/hrl_phri_2011/scripts/variables.sh
@@ -1,5 +1,5 @@
 dir=`rospack find hrl_phri_2011`/data
-people=( "kelsey" "tiffany" "phil" "aaron" "sub1" "sub2" "sub3" "sub4" "sub5" "sub6" "sub7" "sub8" "sub9" "sub10" "sub11" "sub12" "sub13" "sub14" "sub15" "sub16" "sub17" "sub18" "sub19" "sub20" )
+people=( "ERROR" "sub1" "sub2" "sub3" "sub4" "sub5" "sub6" "sub7" "sub8" "sub9" "sub10" "sub11" "sub12" "sub13" "sub14" "sub15" "sub16" "sub17" "sub18" "sub19" "sub20" "kelsey" "tiffany" "phil" "aaron" )
 tools=( "wipe_finger" "shaver" )
-places=( "cheek" "nose" "chin" )
+places=( "cheek" "nose" "chin" "all")
 functions=( "force_magnitude" )
