d9b98363e14186b14ef0899456e5138484d46f6a
staderennais <staderennais@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-11-29 17:59:43 +0000

    minor changes to in order to play bag files within the python script
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2370 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/pr2_playpen/src/pr2_playpen/get_gripper_position.py
+++ hrl/pr2_playpen/src/pr2_playpen/get_gripper_position.py
@@ -5,23 +5,35 @@ import rospy
 import math
 import tf
 import numpy as np
+import os
+import sys
+import cPickle as pkl
 
-if __name__ == '__main__':
-    rospy.init_node('get_gripper_position')
+def is_topic_pub(topic):
+    flag = False
+    for item in rospy.get_published_topics():
+        for thing in item:
+            if topic in thing:
+                flag = True
+            else:
+                pass
+    return flag
 
-    listener = tf.TransformListener()
-
-    rate = rospy.Rate(100.0)
-    
+def get_data(listener, rate):
     right = False
     left = False
 
-    for item in rospy.get_published_topics():
-        for thing in item:
-            if '/r_overhead_grasp/feedback' in thing:
-                right = True
-            elif '/l_overhead_grasp/feedback' in thing:
-                left = True
+    dist_ls = []
+    time_ls = []
+
+    while is_topic_pub('/tf') == False:
+        print "waiting for bag file"
+        rate.sleep()
+
+    if is_topic_pub('/r_overhead_grasp/feedback'):
+        right = True
+    elif is_topic_pub('/l_overhead_grasp/feedback'):
+        left = True
 
     if left == True:
         prefix = 'l_'
@@ -31,18 +43,41 @@ if __name__ == '__main__':
     frame1 = prefix+'gripper_l_finger_tip_link'
     frame2 = prefix+'gripper_r_finger_tip_link'
 
-    #######next step is to put into loop to run through each bag file and then to put in pickle file####
-    #f_hand = open(bag_path,  
-    while not rospy.is_shutdown():
+    #run = True
+    while is_topic_pub('/clock') == True: #run == True:#not rospy.is_shutdown():
         try:
             (trans,rot) = listener.lookupTransform(frame1, frame2, rospy.Time(0))
             dist = math.sqrt((np.matrix(trans)*np.matrix(trans).T)[0,0])
             time = rospy.get_time()
-            print "distance is :", dist, "time is :", time
+            dist_ls.append(dist)
+            time_ls.append(time)
 
         except (tf.LookupException, tf.ConnectivityException):
-            print "didn't get anything yet"
+            #run = False
             continue
         
 
         rate.sleep()
+
+    return dist_ls, time_ls
+
+if __name__ == '__main__':
+    rospy.init_node('get_gripper_position')
+    listener = tf.TransformListener()            
+    rate = rospy.Rate(100.0)
+    
+    path = sys.argv[1]
+    print "path is :", path
+
+    for i in xrange(1):
+        j = 0
+        dist_dict = {}
+        f_hand = open(path+'/object'+str(i).zfill(3)+'_gripper_dist.pkl', 'w')
+        while j < 2: #os.path.isfile(path + '/object'+str(i).zfill(3)+'_try'+str(j).zfill(3)+'.bag') == True: #j < 999:
+            f_path = path + '/object'+str(i).zfill(3)+'_try'+str(j).zfill(3)+'.bag'
+            os.system('rosbag play '+ f_path + ' &')
+            dist_ls, time_ls = get_data(listener, rate)
+            dist_dict['try'+str(j).zfill(3)] = {'dist':dist_ls, 'time':time_ls}                
+            j = j+1
+        pkl.dump(dist_dict, f_hand)
+        f_hand.close()
