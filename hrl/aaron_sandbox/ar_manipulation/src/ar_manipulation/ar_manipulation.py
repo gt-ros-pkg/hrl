c6fe85073665216880f5f39202c49c82520a2664
jesushine <jesushine@5fc10e88-4e10-11de-b850-9977aa68d080>
2011-12-13 17:22:33 +0000

    create ar_manipulation package
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2402 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/aaron_sandbox/ar_manipulation/src/ar_manipulation/ar_manipulation.py
@@ -0,0 +1,69 @@
+import roslib
+roslib.load_manifest("hrl_pr2_arms")
+roslib.load_manifest("hrl_generic_arms")
+roslib.load_manifest("rospy")
+roslib.load_manifest("tf")
+roslib.load_manifest("hrl_lib")
+
+import math, time, copy
+import numpy as np
+import hrl_lib, tf, rospy
+import hrl_pr2_arms.pr2_controller_switcher as pr2cs
+import hrl_pr2_arms.pr2_arm as pr2arm
+import hrl_generic_arms.ep_trajectory_controller as eptc
+
+class ToolGrab():
+	def __init__(self):
+		rospy.init_node("pr2_tutorial")
+		self.cs = pr2cs.ControllerSwitcher()
+        self.tf_listener = tf.TransformListener()
+#	Load JTcontroller
+		self.cs.carefree_switch('r', '%s_cart', 
+			"$(find hrl_pr2_arms)/params/j_transpose_params_low.yaml")
+		self.r_arm_cart = pr2arm.create_pr2_arm('r', pr2arm.PR2ArmJTranspose)
+		self.cs.carefree_switch('r', '%s_cart', 
+			"$(find hrl_pr2_arms)/params/j_transpose_params_low.yaml")
+		self.l_arm_cart = pr2arm.create_pr2_arm('r', pr2arm.PR2ArmJTranspose)
+
+# 	Load Joint space controller
+		self.cs.carefree_switch('r', '%s_arm_controller')
+		self.r_arm = pr2arm.create_pr2_arm('r', pr2arm.PR2ArmJointTrajectory)
+		self.cs.carefree_switch('l', '%s_arm_controller')
+		self.l_arm = pr2arm.create_pr2_arm('l', pr2arm.PR2ArmJointTrajectory)
+
+		self.epc = eptc.EPC('linear_move')
+		self.time_step = 1/20.
+
+
+	def get_angles(self):
+		self.r_arm.reset_ep()
+		self.l_arm.reset_ep()
+		self.r_angle = self.r_arm.get_joint_angles()
+		self.l_angle = self.l_arm.get_joint_angles()
+#		print 'r_arm: ', self.r_angle, '\nl_arm: ', self.l_angle
+
+
+	def move_arm_away(self,duration=5.):
+		self.get_angles()
+		self.t_vals = eptc.min_jerk_traj(duration/self.time_step)
+		self.r_ep =np.array([-1.96938757, 1.04533946, -1.87194269,
+			-1.92571321, -2.72870494, -1.52796493, -3.32591939])
+		self.l_ep = np.array([2.13760509, 0.98891465, 2.03678745,
+			-1.87417484, 2.5775505, -1.69534375, -3.07018065])
+		r_traj = self.r_arm.interpolate_ep(self.r_angle, self.r_ep, self.t_vals)
+		l_traj = self.l_arm.interpolate_ep(self.l_angle, self.l_ep, self.t_vals)
+		l_tc = eptc.EPTrajectoryControl(self.l_arm, l_traj)
+		r_tc = eptc.EPTrajectoryControl(self.r_arm, r_traj)
+		raw_input("press a key to continue")
+		self.epc.epc_motion(l_tc, self.time_step)
+		self.epc.epc_motion(r_tc, self.time_step)
+		
+	def get_artag(self,tool_frame='shaver_tool_ar'):
+		(pos, rot) = self.tf_listener.lookupTransform("l_gripper_tool_frame",
+                "/"+tool_frame,rospy.Time(0))
+	
+
+if __name__ == "__main__":
+	tg = ToolGrab()
+	print 'test'
+	tg.move_arm_away()
