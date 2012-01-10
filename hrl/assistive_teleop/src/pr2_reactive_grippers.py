7f7df27158422ef81f0a859cd832ebaa96098181
Phillip.Grice@gmail.com <Phillip.Grice@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-01-10 21:15:33 +0000

    new electric version of assistive_teleop package.  Needs internal name change from web_teleop_trunk.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2443 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/assistive_teleop/src/pr2_reactive_grippers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import roslib; roslib.load_manifest('web_teleop_trunk')
+import rospy
+
+import actionlib
+
+class Pr2Gripper():
+
+    def __init__(self, arm):
+       
+
+
+
+
+
+    def grab(self, gain, blocking=False):
+        pass
+
+    def release(self,
