364c6b64fc1b0415010a7789daa1e92c8df19e3b
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-01-28 01:02:27 +0000

    Added combobox for easier use.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2525 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/kelsey_sandbox/src/kelsey_sandbox/controller_gui.py
+++ hrl/kelsey_sandbox/src/kelsey_sandbox/controller_gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'controller_gui.ui'
 #
-# Created: Fri Jan 27 19:20:12 2012
+# Created: Fri Jan 27 19:59:00 2012
 #      by: PyQt4 UI code generator 4.7.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -16,15 +16,19 @@ class Ui_Frame(object):
         Frame.setFrameShape(QtGui.QFrame.StyledPanel)
         Frame.setFrameShadow(QtGui.QFrame.Raised)
         self.start_button = QtGui.QPushButton(Frame)
-        self.start_button.setGeometry(QtCore.QRect(10, 50, 161, 181))
+        self.start_button.setGeometry(QtCore.QRect(20, 20, 161, 181))
         self.start_button.setStyleSheet("font: 16pt \"AlArabiya\";\n"
 "background-color: rgb(120, 255, 96);")
         self.start_button.setObjectName("start_button")
         self.kill_button = QtGui.QPushButton(Frame)
-        self.kill_button.setGeometry(QtCore.QRect(200, 50, 161, 181))
+        self.kill_button.setGeometry(QtCore.QRect(210, 20, 161, 181))
         self.kill_button.setStyleSheet("background-color: rgb(255, 67, 67);\n"
 "font: 16pt \"AlArabiya\";")
         self.kill_button.setObjectName("kill_button")
+        self.controller_combo = QtGui.QComboBox(Frame)
+        self.controller_combo.setGeometry(QtCore.QRect(20, 230, 351, 31))
+        self.controller_combo.setEditable(True)
+        self.controller_combo.setObjectName("controller_combo")
 
         self.retranslateUi(Frame)
         QtCore.QMetaObject.connectSlotsByName(Frame)
--- hrl/kelsey_sandbox/src/kelsey_sandbox/controller_interface.py
+++ hrl/kelsey_sandbox/src/kelsey_sandbox/controller_interface.py
@@ -8,10 +8,9 @@ from controller_gui import Ui_Frame as QTControllerGUIFrame
 from subprocess import Popen
 
 class ControllerGUIFrame(QtGui.QFrame):
-    def __init__(self, controller):
+    def __init__(self):
         super(ControllerGUIFrame, self).__init__()
         self.init_ui()
-        self.controller = controller
 
     def init_ui(self):
         self.ui = QTControllerGUIFrame()
@@ -19,18 +18,29 @@ class ControllerGUIFrame(QtGui.QFrame):
 
         self.ui.start_button.clicked.connect(self.start_button_clk)
         self.ui.kill_button.clicked.connect(self.kill_button_clk)
+        self.controller = "l_arm_controller"
+        self.ui.controller_combo.addItem("l_arm_controller")
+        self.ui.controller_combo.addItem("l_cart")
+        self.ui.controller_combo.addItem("r_arm_controller")
+        self.ui.controller_combo.addItem("r_cart")
+        self.ui.controller_combo.activated[str].connect(self.combo_activated)
+
+    def combo_activated(self, text):
+        self.controller = text
 
     def start_button_clk(self):
-        Popen("rosrun pr2_controller_manager pr2_controller_manager start %s" % self.controller, shell=True)
+        if self.controller is not None:
+            Popen("rosrun pr2_controller_manager pr2_controller_manager start %s" % self.controller, 
+                  shell=True)
 
     def kill_button_clk(self):
-        Popen("rosrun pr2_controller_manager pr2_controller_manager stop %s" % self.controller, shell=True)
+        if self.controller is not None:
+            Popen("rosrun pr2_controller_manager pr2_controller_manager stop %s" % self.controller, 
+                  shell=True)
 
 def main():
     app = QtGui.QApplication(sys.argv)
-    assert(len(sys.argv) > 1)
-    controller = sys.argv[1]
-    frame = ControllerGUIFrame(controller)
+    frame = ControllerGUIFrame()
     frame.show()
     sys.exit(app.exec_())
 
