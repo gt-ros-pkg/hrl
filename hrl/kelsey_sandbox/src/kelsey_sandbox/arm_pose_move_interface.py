6e95bf8dadec652dcc4cab36c4c18a784fc9e933
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-02-14 10:29:25 +0000

    arm_pose_move interface gui is working
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2627 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/kelsey_sandbox/src/kelsey_sandbox/arm_pose_move_interface.py
+++ hrl/kelsey_sandbox/src/kelsey_sandbox/arm_pose_move_interface.py
@@ -7,59 +7,72 @@ import roslib
 roslib.load_manifest("rospy")
 roslib.load_manifest("std_msgs")
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Bool
 
-from arm_cart_control_gui import Ui_Frame as QTArmPoseMoveGUIFrame
+from arm_pose_move_gui import Ui_Frame as QTArmPoseMoveGUIFrame
+from msg import ArmPoseMoveCmd
+
+HEARTBEAT_TOPIC = '/arm_pose_move_gui/heartbeat'
+COMMAND_TOPIC = '/arm_pose_move_gui/command'
+MONITOR_RATE = 20
 
 class ArmPoseMoveGUIFrame(QtGui.QFrame):
     def __init__(self):
         super(ArmPoseMoveGUIFrame, self).__init__()
 
-        self.move_state_pub = rospy.Publisher(MOVE_STATE_TOPIC, String)
-        self.arm_state_pub = rospy.Publisher(LOAD_ARM_TOPIC, String)
+        self.heartbeat_pub = rospy.Publisher(HEARTBEAT_TOPIC, Bool)
+        self.cmd_pub = rospy.Publisher(COMMAND_TOPIC, ArmPoseMoveCmd)
 
-        self.cur_arm = "l"
         self.init_ui()
-        self.arm_left_clk()
 
     def init_ui(self):
-        self.ui = QTArmControlGUIFrame()
+        self.ui = QTArmPoseMoveGUIFrame()
         self.ui.setupUi(self)
 
-        self.ui.arm_left.clicked.connect(self.arm_left_clk)
-        self.ui.arm_right.clicked.connect(self.arm_right_clk)
+        self.ui.start_button.clicked.connect(self.start_clk)
+        self.ui.stop_button.clicked.connect(self.stop_clk)
+        self.ui.reset_button.clicked.connect(self.reset_clk)
+        trajectories = ['test1', 'test2']
+        for traj in trajectories:
+            self.ui.traj_combo.addItem(traj)
+        poses = ['ptest1', 'ptest2']
+        for pose in poses:
+            self.ui.joint_combo.addItem(pose)
+
         self.monitor_timer = QtCore.QTimer(self)
         QtCore.QObject.connect(self.monitor_timer, QtCore.SIGNAL("timeout()"), self.monitor_cb)
         self.monitor_timer.start(MONITOR_RATE)
 
+    def start_clk(self):
+        msg = ArmPoseMoveCmd()
+        msg.type = msg.START
+        if self.ui.traj_button.isChecked():
+            msg.traj_name = str(self.ui.traj_combo.currentText())
+            msg.is_trajectory = True
+            msg.is_forward = self.ui.forward_button.isChecked()
+        else:
+            msg.traj_name = str(self.ui.joint_combo.currentText())
+            msg.is_trajectory = False
+            msg.is_forward = self.ui.forward_button.isChecked()
+        self.cmd_pub.publish(msg)
+
+    def stop_clk(self):
+        msg = ArmPoseMoveCmd()
+        msg.type = msg.STOP
+        self.cmd_pub.publish(msg)
+
+    def reset_clk(self):
+        msg = ArmPoseMoveCmd()
+        msg.type = msg.RESET
+        self.cmd_pub.publish(msg)
+
     def monitor_cb(self):
-        move_button = ""
-        for button in MOVE_BUTTONS:
-            exec("is_down = self.ui.%s.isDown()" % button)
-            if is_down:
-                move_button = button
-        self.move_state_pub.publish(String(move_button))
-        self.arm_state_pub.publish(String(self.cur_arm))
-
-    def arm_left_clk(self):
-        self.ui.arm_left.setDisabled(True)
-        self.ui.arm_right.setEnabled(True)
-        self.ui.arm_left.setStyleSheet(ARM_STYLESHEET % ("left", "on"))
-        self.ui.arm_right.setStyleSheet(ARM_STYLESHEET % ("right", "off"))
-        self.cur_arm = "l"
-
-    def arm_right_clk(self):
-        self.arm_state_pub.publish(String("r"))
-        self.ui.arm_right.setDisabled(True)
-        self.ui.arm_left.setEnabled(True)
-        self.ui.arm_left.setStyleSheet(ARM_STYLESHEET % ("left", "off"))
-        self.ui.arm_right.setStyleSheet(ARM_STYLESHEET % ("right", "on"))
-        self.cur_arm = "r"
+        self.heartbeat_pub.publish(Bool())
 
 def main():
-    rospy.init_node("arm_cart_control_interface")
+    rospy.init_node("arm_pose_move_interface")
     app = QtGui.QApplication(sys.argv)
-    frame = ArmCartControlGUIFrame()
+    frame = ArmPoseMoveGUIFrame()
     frame.show()
     sys.exit(app.exec_())
 
