cdb2a6484b5d00db157f4eba794339d337386660
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-02-15 23:09:21 +0000

    Made servo_prepare into a message interface
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2654 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/kelsey_sandbox/src/kelsey_sandbox/servo_prepare.py
+++ hrl/kelsey_sandbox/src/kelsey_sandbox/servo_prepare.py
@@ -8,20 +8,20 @@ roslib.load_manifest('hrl_pr2_arms')
 import rospy
 import tf
 import tf.transformations as tf_trans
+from std_msgs.msg import Bool
 
 from hrl_generic_arms.ep_trajectory_controller import EPArmController
 from hrl_pr2_arms.pr2_arm import create_pr2_arm, PR2ArmJointTrajectory
 from hrl_pr2_arms.pr2_controller_switcher import ControllerSwitcher
 
-def main():
-    rospy.init_node("servo_prepare")
+joint_ctrl = '%s_arm_controller'
+
+def setup_servoing_arms(msg):
     ctrl_switcher = ControllerSwitcher()
-    ctrl_switcher.carefree_switch('r', '%s_arm_controller')
-    ctrl_switcher.carefree_switch('l', '%s_arm_controller')
-    r_arm = create_pr2_arm('r', PR2ArmJointTrajectory)
-    l_arm = create_pr2_arm('l', PR2ArmJointTrajectory)
-    r_arm.wait_for_joint_angles()
-    l_arm.wait_for_joint_angles()
+    ctrl_switcher.carefree_switch('r', joint_ctrl, reset=False)
+    ctrl_switcher.carefree_switch('l', joint_ctrl, reset=False)
+    r_arm = create_pr2_arm('r', PR2ArmJointTrajectory, controller_name=joint_ctrl)
+    l_arm = create_pr2_arm('l', PR2ArmJointTrajectory, controller_name=joint_ctrl)
 
     r_ep_arm_ctrl = EPArmController(r_arm)
     l_ep_arm_ctrl = EPArmController(l_arm)
@@ -30,8 +30,10 @@ def main():
     l_ep_arm_ctrl.execute_interpolated_ep([1.91,  1.25,  1.93, -1.53, -0.33, -0.03, -3.09],
                                           15, blocking=True)
 
-    
-    
+def main():
+    rospy.init_node("servo_prepare")
+    rospy.Subscriber("/pr2_ar_servo/arms_setup", Bool, setup_servoing_arms)
+    rospy.spin()
 
 if __name__ == "__main__":
     main()
