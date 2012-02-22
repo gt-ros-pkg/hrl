5e282cd2e8aa7339e117b3a9751468539fef0f78
kphawkins@gmail.com <kphawkins@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-02-22 00:30:17 +0000

    Added relay test and behavior_manager shell.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2705 5fc10e88-4e10-11de-b850-9977aa68d080

---
+++ hrl/kelsey_sandbox/src/behavior_manager.cpp
@@ -0,0 +1,19 @@
+
+
+#include <cstdio>
+#include "topic_tools/shape_shifter.h"
+#include "topic_tools/parse.h"
+
+class BehaviorManager
+{
+    public:
+        BehaviorManager();
+};
+
+BehaviorManager::BehaviorManager()
+{
+}
+
+int main(int argc, char **argv)
+{
+}
