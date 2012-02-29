052495dbc6aa28664ee7a52187a2baf1c378dbc6
Phillip.Grice@gmail.com <Phillip.Grice@gmail.com@5fc10e88-4e10-11de-b850-9977aa68d080>
2012-02-29 22:44:54 +0000

    updated, minor tweaks and cleanup for use with sttr roll demo.
    git-svn-id: https://gt-ros-pkg.googlecode.com/svn/trunk@2772 5fc10e88-4e10-11de-b850-9977aa68d080

--- hrl/assistive_teleop/src/assistive_teleop/searcher.py
+++ hrl/assistive_teleop/src/assistive_teleop/searcher.py
@@ -7,24 +7,23 @@ import decimal
 import numpy as np
 import tf
 from tf import transformations as tft
-from geometry_msgs.msg import PoseWithCovarianceStamped, PointStamped, Twist, Point32, PoseStamped
+from geometry_msgs.msg import (PoseWithCovarianceStamped, PointStamped, 
+                                Twist, Point32, PoseStamped)
 from sensor_msgs.msg import LaserScan, PointCloud
 from std_msgs.msg import String
-#import costmap_services.python_client as costmap
 
 class Follower:
-
     def __init__(self):
         rospy.init_node('relative_servoing')
-        rospy.Subscriber('robot_pose_ekf/odom_combined', PoseWithCovarianceStamped, self.update_position) 
+        rospy.Subscriber('robot_pose_ekf/odom_combined', 
+                         PoseWithCovarianceStamped, 
+                         self.update_position)
         rospy.Subscriber('/base_scan', LaserScan, self.update_base_laser)
         rospy.Subscriber('/servo_goal', PoseStamped, self.update_goal)
         self.vel_out = rospy.Publisher('base_controller/command', Twist)
         self.rate_test = rospy.Publisher('rate_test', String)
-        #self.cs = costmap.CostmapServices( accum = 3 )
         self.tfl = tf.TransformListener()
-        #self.driver = rospy.Publisher('/base_controller/command', Twist)
-        self.goal_out = rospy.Publisher('/servo_dest', PoseStamped)
+        self.goal_out = rospy.Publisher('/servo_dest', PoseStamped, latch=True)
         self.left_out = rospy.Publisher('/left_points', PointCloud)
         self.right_out = rospy.Publisher('/right_points', PointCloud)
         self.front_out = rospy.Publisher('/front_points', PointCloud)
@@ -47,44 +46,51 @@ class Follower:
         self.ang_thresh = self.ang_thresh_big
         self.retreat_thresh = 0.3
         self.curr_pos = PoseWithCovarianceStamped()
-        self.dist_thresh = 0.4
+        self.dist_thresh = 0.15
         self.left = [[],[]]
         self.right = [[],[]]
         self.front = [[],[]]
 
     def update_goal(self, msg):
-        self.goal_received = True
         msg.header.stamp = rospy.Time.now()
                 
-        #if not msg.header.frame_id == '/base_footprint' or msg.header.frame_id == 'base_footprint'
-        #    self.tfl.waitForTransform(msg.header.frame_id, '/base_footprint', msg.header.stamp, rospy.Duration(0.5))
-        #    msg = self.tfl.transformPose('/base_footprint', msg)
-        #self.ang_to_goal = 
-
-        self.tfl.waitForTransform(msg.header.frame_id, '/base_footprint', msg.header.stamp, rospy.Duration(0.5))
+        self.tfl.waitForTransform(msg.header.frame_id, '/base_footprint',
+                                  msg.header.stamp, rospy.Duration(0.5))
         self.bfp_goal = self.tfl.transformPose('/base_footprint', msg)
-        self.tfl.waitForTransform(msg.header.frame_id, 'odom_combined', msg.header.stamp, rospy.Duration(0.5))
+        self.tfl.waitForTransform(msg.header.frame_id, 'odom_combined',
+                                  msg.header.stamp, rospy.Duration(0.5))
         self.odom_goal = self.tfl.transformPose('/odom_combined', msg)
         self.goal_out.publish(self.odom_goal)
         
-        ang_to_goal = math.atan2(self.bfp_goal.pose.position.y, self.bfp_goal.pose.position.x)
-        self.ang_goal = self.curr_ang[2] + ang_to_goal #The desired angular position (the current angle in odom, plus the robot-relative change to face goal)
+        ang_to_goal = math.atan2(self.bfp_goal.pose.position.y,
+                                 self.bfp_goal.pose.position.x)
+        #The desired angular position 
+        #(current angle in odom, plus the robot-relative change to face goal)
+        self.ang_goal = self.curr_ang[2] + ang_to_goal
+        self.goal_received = True
         print "New Goal: \r\n %s" %self.bfp_goal
 
     def update_position(self, msg):
-        #self.curr_pose = msg.pose.pose
-        self.curr_ang = tft.euler_from_quaternion([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
-        self.ang_diff = np.unwrap([0, self.ang_goal - self.curr_ang[2]])[1] # Normalized via unwrap relative to 0; (keeps between -pi/pi)
+        self.curr_ang=tft.euler_from_quaternion([msg.pose.pose.orientation.x,
+                                                 msg.pose.pose.orientation.y,
+                                                 msg.pose.pose.orientation.z,
+                                                 msg.pose.pose.orientation.w])
+        # Normalized via unwrap relative to 0; (keeps between -pi/pi)
+        self.ang_diff = np.unwrap([0, self.ang_goal - self.curr_ang[2]])[1]
         #print "Ang Diff: %s" %self.ang_diff
 
-        self.dist_to_goal = math.sqrt((self.odom_goal.pose.position.x-msg.pose.pose.position.x)**2 + (self.odom_goal.pose.position.y-msg.pose.pose.position.y)**2)
+        self.dist_to_goal = ((self.odom_goal.pose.position.x-
+                              msg.pose.pose.position.x)**2 + 
+                              (self.odom_goal.pose.position.y-
+                              msg.pose.pose.position.y)**2)**(1./2.)
         #print "Dist to goal: %s" %self.dist_to_goal
 
         if self.goal_received:
-            if self.dist_to_goal < self.dist_thresh and abs(self.ang_diff) < self.ang_thresh:
+            if ((self.dist_to_goal < self.dist_thresh) and 
+                (abs(self.ang_diff) < self.ang_thresh)):
                 self.goal_present = False
                 self.goal_received = False
-                #print "Achieved Goal!"
+                print "Achieved Goal!"
             else:
                 self.goal_present = True
 
@@ -92,8 +98,11 @@ class Follower:
         max_angle = msg.angle_max
         ranges = np.array(msg.ranges)
         angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
-        near_angles = np.extract(np.logical_and(ranges<1, ranges>0.003), angles)#Filter out noise (<0.003), distant points(>1m), leaves nearby, relevant obstacles
-        near_ranges = np.extract(np.logical_and(ranges<1, ranges>0.003), ranges)
+        #Filter out noise(<0.003), points >1m, leaves obstacles
+        near_angles = np.extract(np.logical_and(ranges<1, ranges>0.003),
+                                 angles)
+        near_ranges = np.extract(np.logical_and(ranges<1, ranges>0.003),
+                                 ranges)
         self.bad_side = np.sign(near_angles[np.argmax(abs(near_angles))])
         #print "bad side: %s" %bad_side # (1 (pos) = left, -1 = right)
         #print "Min in Ranges: %s" %min(ranges)
@@ -106,7 +115,8 @@ class Follower:
         #print "Points: %s" %points
         self.bfp_points = np.vstack((np.add(0.275, xs),ys))
         #print "bfp Points: %s" %bfp_points
-        self.bfp_dists = np.sqrt(np.add(np.square(self.bfp_points[0][:]),np.square(self.bfp_points[1][:])))
+        self.bfp_dists = np.sqrt(np.add(np.square(self.bfp_points[0][:]),
+                                        np.square(self.bfp_points[1][:])))
         #print min(self.bfp_dists)
         if len(self.bfp_dists) >0:
             if min(self.bfp_dists) > 0.5:
@@ -116,9 +126,13 @@ class Follower:
         else:
             self.rot_safe = True
         
-        self.left = np.vstack((xs[np.nonzero(ys>0.35)[0]], ys[np.nonzero(ys>0.35)[0]]))
-        self.right = np.vstack((xs[np.nonzero(ys<-0.35)[0]], ys[np.nonzero(ys<-0.35)[0]]))
-        self.front = np.vstack((np.extract(np.logical_and(ys<0.35,ys>-0.35),xs), np.extract(np.logical_and(ys<0.35, ys>-0.35),ys)))
+        self.left = np.vstack((xs[np.nonzero(ys>0.35)[0]],
+                               ys[np.nonzero(ys>0.35)[0]]))
+        self.right= np.vstack((xs[np.nonzero(ys<-0.35)[0]],
+                                ys[np.nonzero(ys<-0.35)[0]]))
+        self.front = np.vstack(
+                          (np.extract(np.logical_and(ys<0.35,ys>-0.35),xs),
+                           np.extract(np.logical_and(ys<0.35, ys>-0.35),ys)))
 
         front_dist = (self.front[:][0]**2+self.front[:][1]**2)**(1/2)
 
@@ -166,20 +180,25 @@ class Follower:
             self.front_out.publish(frontScan)
 
     def set_rot(self):
-        if abs(self.ang_diff) < self.ang_thresh: #Fully oriented, relax constraint to avoid osscilation
+        if abs(self.ang_diff) < self.ang_thresh: 
+            #Fully oriented, relax constraint to avoid osscilation
             self.ang_thresh = self.ang_thresh_big
             self.command.angular.z = 0.0
-
-        else: # Not fully oriented, continue until pointed within small constraint
-            if self.rot_safe: # Check for obstacles in the narrow rotation radius
-                self.command.angular.z = np.sign(self.ang_diff)*np.clip(abs(0.35*self.ang_diff), 0.1, 0.5)
+        else: 
+            #Not oriented, continue until pointed within small constraint
+            if self.rot_safe: # Check for obstacles in the rotation radius
+                self.command.angular.z = (np.sign(self.ang_diff)*
+                                          np.clip(abs(0.35*self.ang_diff),
+                                                  0.1, 0.5))
                 self.ang_thresh = self.ang_thresh_small
             else:
                 print "Cannot Rotate, obstacles nearby"
 
     def set_trans(self):
-        if abs(self.ang_diff) < math.pi/6 and self.dist_to_goal > self.dist_thresh: #Facing the right direction and not there yet, so start moving.
-            self.command.linear.x = np.clip(self.dist_to_goal*0.125, 0.15, 0.3)
+        if (abs(self.ang_diff) < math.pi/6 and
+            self.dist_to_goal > self.dist_thresh):
+            #Facing the right direction and not there yet, so start moving.
+            self.command.linear.x=np.clip(self.dist_to_goal*0.125, 0.05, 0.3)
         else:
             self.command.linear.x = 0.0
 
@@ -191,57 +210,69 @@ class Follower:
             lefts = np.extract(self.left[:][1]<0.45, self.left[:][1])
            # print "lefts:\r\n %s" %lefts
             if len(lefts) > 0:
-                #left_dists = np.sqrt(np.add(np.square(lefts[:][0]), np.square(lefts[:][1])))
+                #left_dists = np.sqrt(np.add(np.square(lefts[:][0]),
+                #                            np.square(lefts[:][1])))
                 #push_from_left = np.sum(np.reciprocal(np.exp(lefts)))
                 push_from_left = -0.45 + min(lefts)
-                print "lpush: \r\n %s" %push_from_left
+                #print "lpush: \r\n %s" %push_from_left
         if len(self.right[:][0]) > 0:
             rights = np.extract(self.right[:][1]>-0.45, self.right[:][1])
            # print "rights:\r\n %s" %rights
             if len(rights) > 0:
-                #right_dists = np.sqrt(np.add(np.square(rights[:][0]), np.square(rights[:][1])))
+                #right_dists = np.sqrt(np.add(np.square(rights[:][0]),
+                #                             np.square(rights[:][1])))
                 #push_from_right = np.sum(np.reciprocal(np.exp(rights)))
                 push_from_right = 0.45 + max(rights)
-                print "rpush: \r\n %s" %push_from_right
+                #print "rpush: \r\n %s" %push_from_right
         #print "Push: %s" %(push_from_right + push_from_left)
         slide = push_from_right + push_from_left
         self.command.linear.y = np.sign(slide)*np.clip(abs(slide), 0.04, 0.07)
         print "Slide speed (m/s): %s" %slide
 
-        ##Determine obstacle avoidance rotation to avoid obstacles in front of robot#
+        ##Determine rotation to avoid obstacles in front of robot#
     def avoid_obstacles(self):
         if len(self.front[0][:]) > 0:
-            if min(self.front[0][:]) < self.retreat_thresh: #0.225:  #0.5 (round-up on corner-to-corner radius of robot) - 0.275 (x diff from base laser link to base footprint)
-                print "front[0][:] %s" %self.front[0][:]
-                front_dists = np.sqrt(np.add(np.square(self.front[0][:]),np.square(self.front[1][:])))
+            if min(self.front[0][:]) < self.retreat_thresh: 
+                #(round-up on corner-to-corner radius of robot) -
+                # 0.275 (x diff from base laser link to base footprint)
+                #print "front[0][:] %s" %self.front[0][:]
+                front_dists = np.sqrt(np.add(np.square(self.front[0][:]),
+                                             np.square(self.front[1][:])))
                 min_x = self.front[0][np.argmin(front_dists)]
                 min_y = self.front[1][np.argmin(front_dists)]
-                print "min x/y: %s,%s" %(min_x, min_y)
-                self.command.linear.x = -np.sign(min_x)*np.clip(abs(min_x),0.05,0.1) 
-                self.command.linear.y = -np.sign(min_y)*np.clip(abs(min_y),0.05,0.1) 
+                #print "min x/y: %s,%s" %(min_x, min_y)
+                self.command.linear.x = (-np.sign(min_x)*
+                                          np.clip(abs(min_x),0.05,0.1))
+                self.command.linear.y = (-np.sign(min_y)*
+                                          np.clip(abs(min_y),0.05,0.1)) 
                 self.command.angular.z = 0.0
-                print "TOO CLOSE: Back up slowly..." # This should probably be avoided...
+                # This should probably be avoided...
+                print "TOO CLOSE: Back up slowly..." 
                 self.retreat_thresh = 0.4
             elif min(self.front[0][:]) < 0.45: 
                 self.retreat_thresh = 0.3
                 print "Turning Away from obstacles in front"
                 #self.command.linear.x = 0.0
-               # front_dists = np.sqrt(np.add(np.square(self.front[0][:]),np.square(self.front[1][:])))
+               # front_dists = np.sqrt(np.add(np.square(self.front[0][:]),
+               #                              np.square(self.front[1][:])))
                # min_x = self.front[0][np.argmin(front_dists)]
                # min_y = self.front[1][np.argmin(front_dists)]
                # min_ang = math.atan2(min_y, min_x)
                # print "min_ang: %s" %min_ang
                # self.command.angular.z 
 
-                lfobs = self.front[0][np.logical_and(self.front[1]>0, self.front[0]<0.45)]
-                rfobs = self.front[0][np.logical_and(self.front[1]<0, self.front[0]<0.45)]
+                lfobs = self.front[0][np.logical_and(self.front[1]>0,
+                                                     self.front[0]<0.45)]
+                rfobs = self.front[0][np.logical_and(self.front[1]<0,
+                                                     self.front[0]<0.45)]
                # print "lfobs: %s" %lfobs
                 if len(lfobs) == 0:
                     self.command.linear.y = 0.07
                 #print "rfobs: %s" %rfobs
                 elif len(rfobs) == 0:
                     self.command.linear.y += -0.07
-                weight = np.reciprocal(np.sum(np.reciprocal(rfobs)) - np.sum(np.reciprocal(lfobs)))
+                weight = np.reciprocal(np.sum(np.reciprocal(rfobs)) -
+                                       np.sum(np.reciprocal(lfobs)))
                 if weight > 0:
                     self.command.angular.z += 0.05 
                 else:
@@ -255,9 +286,10 @@ class Follower:
             # x<0.1 (not in front), x>-0.8 (not behind)
             left_obs = self.left[:, self.left[1,:]<0.4] #points too close.
             if len(left_obs[0][:])>0:
-                left_obs = left_obs[:, np.logical_and(left_obs[0,:]<0.1, left_obs[0,:]>-0.8)]
+                left_obs = left_obs[:, np.logical_and(left_obs[0,:]<0.1,
+                                                      left_obs[0,:]>-0.8)]
                 if len(left_obs[:][0])>0:
-                    print "Obstacle immediately to the left, cannot move further"
+                    print "Obstacle immediately to the left, cannot move."
                     self.command.linear.y = 0
 
     def right_clear(self):
@@ -266,16 +298,11 @@ class Follower:
             # x<0.1 (not in front), x>-0.8 (not behind)
            right_obs = self.right[:, self.right[1,:]>-0.4] #points too close.
            if len(right_obs[0][:])>0:
-                right_obs = right_obs[:, np.logical_and(right_obs[0,:]<0.1, right_obs[0,:]>-0.8)]
+                right_obs = right_obs[:, np.logical_and(right_obs[0,:]<0.1,
+                                                        right_obs[0,:]>-0.8)]
                 if len(right_obs[:][0])>0:
-                    print "Obstacle immediately to the right, cannot move further"
+                    print "Obstacle immediately to the right, cannot move."
                     self.command.linear.y = 0
-
-    def check_costmap(self):
-        if self.cs.scoreTraj_PosHyst( self.command.linear.x, self.command.linear.y, self.command.linear.z ) != -1.0:
-            return True
-        else:
-            return False
 
     def run(self):
         if self.goal_present:
@@ -287,11 +314,8 @@ class Follower:
                 self.left_clear()
             elif self.command.linear.y < 0:
                 self.right_clear()
-           # if check_costmap():
             print "Sending vel_command: \r\n %s" %self.command
             self.vel_out.publish(self.command)
-          #  else:
-            #    print "COSTMAP SENSES COLLISION, NOT MOVING"
         else:
             pass
 
