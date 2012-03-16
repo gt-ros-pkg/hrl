1d55b11f7574dad09cc1755bd1be4359ebf11096
Kelsey Hawkins <kelsey@monty1.(none)>
2012-03-15 18:42:25 -0700

    Updated carefree_switch_controller to make more sense and have a help printout.

--- hrl_arm_control/hrl_pr2_arms/scripts/carefree_switch_controller.py
+++ hrl_arm_control/hrl_pr2_arms/scripts/carefree_switch_controller.py
@@ -9,18 +9,14 @@ from hrl_pr2_arms.pr2_controller_switcher import ControllerSwitcher
 
 def main():
     rospy.init_node("carefree_switch_controller", sys.argv)
-    if len(sys.argv) <= 1:
+    if len(sys.argv) <= 1 or sys.argv[1] in ["-h", "--help"]:
         print "Usage: arm new_controller [param_file]"
         return
     cs = ControllerSwitcher()
     if len(sys.argv) >= 4:
-        if len(sys.argv) >= 5:
-            pkg = sys.argv[3]
-            param_file = sys.argv[4]
-        else:
-            pkg = 'hrl_pr2_arms'
-            param_file = sys.argv[3]
-        param_path = "$(find %s)/params/%s" % (pkg, param_file)
+        param_path = sys.argv[3]
+        if 'find' not in param_path:
+            param_path = "$(find hrl_pr2_arms)/params/" + param_path
         print cs.carefree_switch(sys.argv[1], sys.argv[2], param_path)
     else:
         print cs.carefree_switch(sys.argv[1], sys.argv[2])
