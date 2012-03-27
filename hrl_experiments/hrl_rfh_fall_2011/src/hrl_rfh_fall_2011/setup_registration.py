44b3a5b2058832493abe30f4364687d41cd236a8
Kelsey Hawkins <kelsey@monty1.(none)>
2012-03-26 20:14:13 -0700

    Added point head to setup registration procedure.

--- hrl_experiments/hrl_rfh_fall_2011/src/hrl_rfh_fall_2011/setup_registration.py
+++ hrl_experiments/hrl_rfh_fall_2011/src/hrl_rfh_fall_2011/setup_registration.py
@@ -5,17 +5,27 @@ from threading import Lock
 
 import roslib
 roslib.load_manifest('hrl_rfh_fall_2011')
+roslib.load_manifest('actionlib')
+roslib.load_manifest('hrl_generic_arms')
+roslib.load_manifest('pr2_controllers_msgs')
 import roslib.substitution_args
 import rospy
 import rosbag
 from std_srvs.srv import Empty, EmptyResponse
+import actionlib
 
+from geometry_msgs.msg import TransformStamped
 from hrl_phri_2011.srv import LoadEllipsoidParams
+from hrl_generic_arms.pose_converter import PoseConverter
+from pr2_controllers_msgs.msg import PointHeadAction, PointHeadGoal
 
 class SetupRegistration(object):
     def __init__(self):
         rospy.loginfo("[setup_registration] SetupRegistration ready.")
         self.load_ell_params = rospy.ServiceProxy("/load_ellipsoid_params", LoadEllipsoidParams)
+        self.head_point_sac = actionlib.SimpleActionClient('/head_traj_controller/point_head_action',
+                                                           PointHeadAction)
+        self.head_point_sac.wait_for_server()
         self.lock = Lock()
 
     def run(self, req):
@@ -29,8 +39,16 @@ class SetupRegistration(object):
         ell_params_bag = rosbag.Bag(roslib.substitution_args.resolve_args(filepath), 'r')
         for topic, ell_param, time in ell_params_bag:
             cur_ell_param = ell_param
-        print cur_ell_param, type(cur_ell_param)
         self.load_ell_params(cur_ell_param)
+        cur_ell_param.e_frame.__class__ = TransformStamped
+        
+        head_goal = PointHeadGoal()
+        head_goal.target = PoseConverter.to_point_stamped_msg(cur_ell_param.e_frame)
+        head_goal.target.point.z -= 0.15
+        head_goal.target.header.stamp = rospy.Time()
+        head_goal.min_duration = rospy.Duration(1.)
+        head_goal.max_velocity = 1.
+        self.head_point_sac.send_goal_and_wait(head_goal)
 
         self.lock.release()
         ############################################################
