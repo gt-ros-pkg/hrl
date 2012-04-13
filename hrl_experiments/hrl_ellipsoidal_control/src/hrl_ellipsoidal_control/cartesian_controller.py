514791c4cb02d9528f70acd18a9959c2fbc908a7
Kelsey Hawkins <kelsey@monty1.(none)>
2012-04-12 20:31:40 -0700

    Added cartesian interface backend which allows us two controllers running through the same interface.

--- hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/cartesian_controller.py
+++ hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/cartesian_controller.py
@@ -27,9 +27,6 @@ from hrl_pr2_arms.pr2_arm import create_pr2_arm, PR2ArmCartesianBase, PR2ArmJTra
 from hrl_generic_arms.pose_converter import PoseConverter
 from hrl_pr2_arms.pr2_controller_switcher import ControllerSwitcher
 
-SETUP_ANGLES_R = [0, 0, np.pi/2, -np.pi/2, -np.pi, -np.pi/2, -np.pi/2]
-SETUP_ANGLES_L = [ 0.24575899,  0.35064596, -0.05039097, -1.87098988, -3.11778445,
-                  -1.33096993, -0.06899168]
 MIN_HEIGHT = 0.2
 
 # TODO Figure this out sometime...
@@ -42,16 +39,16 @@ MIN_HEIGHT = 0.2
 #        max_ang_diff = np.where(cur_diff > max_ang_diff, cur_diff, max_ang_diff)
 #        q_cur = q_next
 
-class EllipsoidController(object):
+class CartesianController(object):
     def __init__(self, arm):
         self.time_step = 1. / 20.
         self.arm = arm
-        self.ell_traj_behavior = EPC("cartesian_traj")
+        self.ell_traj_behavior = EPC("ellipsoid_traj")
         self.ell_space = EllipsoidSpace(1)
         self.tf_list = tf.TransformListener()
         self.found_params = False
         self.ctrl_switcher = ControllerSwitcher()
-        self.ell_cmd_lock = Lock()
+        self.cart_cmd_lock = Lock()
         self.params_lock = Lock()
         self.action_preempted = False
         self.ell_sub = rospy.Subscriber("/ellipsoid_params", EllipsoidParams, self.read_params)
@@ -60,16 +57,24 @@ class EllipsoidController(object):
         self.ell_move_act = actionlib.SimpleActionServer("/ellipsoid_move", EllipsoidMoveAction, 
                                                          self.command_move_exec, False)
         self.ell_move_act.start()
+
+    def read_params(self, e_params):
+        with self.params_lock:
+            self.ell_space.load_ell_params(e_params)
+            self.ell_space.center = np.mat(np.zeros((3, 1)))
+            self.ell_space.rot = np.mat(np.eye(3))
+            if not self.found_params:
+                rospy.loginfo("[cartesian_controller] Found params from /ellipsoid_params")
+            self.found_params = True
     
-    ##
-    # returns frame of end effector in terms of the location of ellipse_frame
-    def get_ep_frame(self):
+    def get_ell_ep(self):
         ee_pose = PoseConverter.to_pose_stamped_msg("/torso_lift_link", self.arm.get_ep())
         cur_time = rospy.Time.now()
         ee_pose.header.stamp = cur_time
         self.tf_list.waitForTransform("/torso_lift_link", "/ellipse_frame", cur_time, rospy.Duration(3))
         ell_pose = self.tf_list.transformPose("/ellipse_frame", ee_pose)
-        return PoseConverter.to_pos_rot(ell_pose)
+        pos, quat = PoseConverter.to_pos_quat(ell_pose)
+        return list(self.ell_space.pos_to_ellipsoidal(*pos))
 
     ##
     # Get pose in robot's frame of ellipsoidal coordinates
@@ -83,7 +88,7 @@ class EllipsoidController(object):
                                           
     def reset_arm_orientation(self, duration=10., gripper_rot=np.pi):
         with self.params_lock:
-            with self.ell_cmd_lock:
+            with self.cart_cmd_lock:
                 num_samps = duration / self.time_step
                 cur_pose = self.arm.get_end_effector_pose()
                 quat_gripper_rot = tf_trans.quaternion_from_euler(gripper_rot, 0, 0)
@@ -114,12 +119,26 @@ class EllipsoidController(object):
         self.execute_ell_move(change_ep, abs_ep_sel, req.gripper_rot, req.velocity)
         self.ell_move_act.set_succeeded(EllipsoidMoveResult(*self.get_ell_ep()))
 
-    def execute_ell_move(self, change_ep, abs_ep_sel, orient_quat=[0., 0., 0., 1.], velocity=0.001):
+    def execute_cart_move(self, change_ep, abs_sel, orient_quat=[0., 0., 0., 1.], velocity=0.001):
         with self.params_lock:
-            with self.ell_cmd_lock:
-                ell_f = np.where(abs_ep_sel, change_ep, 
-                                             np.array(self.get_ell_ep()) + change_ep)
-                self.execute_trajectory(ell_f, orient_quat, velocity)
+            with self.cart_cmd_lock:
+                cur_pos, cur_rot = self.arm.get_ep()
+                change_pos_ep, change_rot_ep = change_ep
+                abs_cart_ep_sel, is_abs_rot = abs_sel
+                pos_f = np.where(abs_cart_ep_sel, change_pos_ep, 
+                                 np.array(cur_pos + cur_rot * np.mat(change_pos_ep).T).T[0])
+                if is_abs_rot:
+                    # TODO FIX THIS
+                    rot_change_mat = change_rot_ep
+                    _, ell_final_rot = self.robot_ellipsoidal_pose(ell_f[0], ell_f[1], ell_f[2],
+                                                                   orient_quat)
+                    rot_mat_f = ell_final_rot * rot_change_mat
+                else:
+                    rpy = change_rot_ep
+                    _, cur_rot = self.arm.get_ep()
+                    rot_mat = np.mat(tf_trans.euler_matrix(*rpy))[:3,:3]
+                    rot_mat_f = cur_rot * rot_mat
+                self._execute_trajectory(pos_f, rot_mat_f, orient_quat, velocity)
 
     def _check_preempt(self, timer_event):
         if self.ell_move_act.is_preempt_requested():
@@ -136,23 +155,25 @@ class EllipsoidController(object):
                                                           "/ellipse_frame", cur_time))
         return ell_frame_mat
 
-    def execute_trajectory(self, ep_f, orient_quat=[0., 0., 0., 1.], velocity=0.001):
-        #ep_init = self.get_ep_frame() # get the current location of the end effector
-        
-        # TODO figure this out TODO
-        #num_samps = int(np.linalg.norm(ell_final - ell_init) / velocity)
-        #num_samps = max(2, num_samps)
-        t_vals = min_jerk_traj(num_samps) # makes movement smooth
-            
-        # smoothly interpolate from init to final
-        cart_interp_traj = self.arm.interpolate_ep(self.arm.get_ep(), ep_f, 
-                                                   t_vals)
+    def _execute_trajectory(self, pos_f, rot_mat_f, orient_quat=[0., 0., 0., 1.], velocity=0.001):
+        cur_pos, cur_rot = self.arm.get_ep()
+
+        rpy = tf_trans.euler_from_matrix(cur_rot.T * rot_mat_f) # get roll, pitch, yaw of angle diff
+
+        num_samps = np.max([2, int(np.linalg.norm(pos_f - cur_pos) / velocity), 
+                               int(np.linalg.norm(rpy) / velocity)])
+
+        ell_frame_mat = self.get_ell_frame()
+
+        traj = self.arm.interpolate_ep([cur_pos, cur_rot], 
+                                       [np.mat(pos_f).T, rot_mat_f], 
+                                       min_jerk_traj(num_samps))
 
         self.start_pub.publish(
-                PoseConverter.to_pose_stamped_msg("/torso_lift_link", cart_interp_traj[0]))
+                PoseConverter.to_pose_stamped_msg("/torso_lift_link", traj[0]))
         self.end_pub.publish(
-                PoseConverter.to_pose_stamped_msg("/torso_lift_link", cart_interp_traj[-1]))
-        ep_traj_control = EPTrajectoryControl(self.arm, cart_interp_traj)
+                PoseConverter.to_pose_stamped_msg("/torso_lift_link", traj[-1]))
+        ep_traj_control = EPTrajectoryControl(self.arm, traj)
         self.action_preempted = False
         self.ell_traj_behavior.stop_epc = False
         monitor_timer = rospy.Timer(rospy.Duration(0.1), self._check_preempt)
@@ -161,28 +182,6 @@ class EllipsoidController(object):
         if self.action_preempted:
             pass
 
-    def execute_rotation(self, rpy, velocity, gripper_rot=np.pi):
-        num_samps = int(np.linalg.norm(rpy) / velocity)
-        num_samps = max(2, num_samps)
-
-        cur_pose = self.arm.get_ep()
-        #quat_gripper_rot = tf_trans.quaternion_from_euler(gripper_rot, 0, 0)
-        #args = self.get_ell_ep() + [quat_gripper_rot]
-        #cur_ell_pos, cur_ell_rot = self.robot_ellipsoidal_pose(*args)
-        rot_mat = np.mat(tf_trans.euler_matrix(rpy[0], rpy[1], rpy[2]))[:3,:3]
-        new_pose = (cur_pose[0], cur_pose[1] * rot_mat)
-        #new_pose = (cur_ell_pos, cur_ell_rot * rot_mat)
-        adjust_traj = self.arm.interpolate_ep(cur_pose, new_pose, 
-                                              min_jerk_traj(num_samps))
-        ep_traj_control = EPTrajectoryControl(self.arm, adjust_traj)
-        self.start_pub.publish(
-                PoseConverter.to_pose_stamped_msg("/torso_lift_link", cur_pose))
-        self.end_pub.publish(
-                PoseConverter.to_pose_stamped_msg("/torso_lift_link", new_pose))
-        self.ell_traj_behavior.epc_motion(ep_traj_control, self.time_step)
-
-
-
 def main():
     rospy.init_node("cartesian_controller", sys.argv)
     cart_arm = create_pr2_arm('l', PR2ArmJTransposeTask, 
@@ -190,7 +189,7 @@ def main():
                               end_link="%s_gripper_shaver45_frame", timeout=0)
 
     rospy.sleep(1)
-    ell_controller = EllipsoidController(cart_arm)
+    cart_controller = CartesianController(cart_arm)
     rospy.spin()
     
 
--- hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/cartesian_interface_backend.py
+++ hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/cartesian_interface_backend.py
@@ -6,24 +6,23 @@ import roslib
 roslib.load_manifest("hrl_ellipsoidal_control")
 import rospy
 import tf.transformations as tf_trans
-from std_msgs.msg import String
 from std_srvs.srv import Empty, EmptyResponse
 
 from hrl_pr2_arms.pr2_arm import PR2ArmCartesianPostureBase, PR2ArmJointTrajectory
 from hrl_pr2_arms.pr2_arm import create_pr2_arm, PR2ArmJTransposeTask
 from hrl_pr2_arms.pr2_controller_switcher import ControllerSwitcher
-from hrl_ellipsoidal_control.ellipsoid_controller import EllipsoidController
+from hrl_ellipsoidal_control.cartesian_controller import CartesianController
 from hrl_ellipsoidal_control.interface_backend import ControllerInterfaceBackend
 
-LOCAL_VELOCITY = 0.0025
-LONGITUDE_STEP = 0.12
-LATITUDE_STEP = 0.096
-HEIGHT_STEP = 0.0986
-trans_params = {'translate_up' : (-LATITUDE_STEP, 0, 0),   'translate_down' : (LATITUDE_STEP, 0, 0),
-                'translate_right' : (0, -LONGITUDE_STEP, 0), 'translate_left' : (0, LONGITUDE_STEP, 0),
-                'translate_in' : (0, 0, -HEIGHT_STEP),      'translate_out' : (0, 0, HEIGHT_STEP)}
+TRANS_VELOCITY = 0.02
+HORIZONTAL_STEP = 0.02
+VERTICAL_STEP = 0.02
+DEPTH_STEP = 0.02
+trans_params = {'translate_up' : (0, 0, -VERTICAL_STEP),   'translate_down' : (0, 0, VERTICAL_STEP),
+                'translate_right' : (0, -HORIZONTAL_STEP, 0), 'translate_left' : (0, HORIZONTAL_STEP, 0),
+                'translate_in' : (DEPTH_STEP, 0, 0),      'translate_out' : (-DEPTH_STEP, 0, 0)}
 
-ROT_VELOCITY = 0.002
+ROT_VELOCITY = 0.016
 ROLL_STEP = np.pi/12
 PITCH_STEP = np.pi/12
 YAW_STEP = np.pi/12
@@ -33,25 +32,25 @@ rot_params = {'rotate_x_pos' : (-ROLL_STEP, 0, 0), 'rotate_x_neg' : (ROLL_STEP, 
 
 button_history = {}
 
-class EllipsoidInterfaceBackend(ControllerInterfaceBackend):
+class CartesianInterfaceBackend(ControllerInterfaceBackend):
     def __init__(self, cart_arm):
-        super(EllipsoidInterfaceBackend, self).__init__("Ellipsoid Controller")
+        super(CartesianInterfaceBackend, self).__init__("Cartesian Controller")
         self.cart_arm = cart_arm
-        self.ell_controller = EllipsoidController(self.cart_arm)
+        self.cart_controller = CartesianController(self.cart_arm)
 
     def run_controller(self, button_press):
         start_pos, _ = self.cart_arm.get_ep()
         quat_gripper_rot = tf_trans.quaternion_from_euler(np.pi, 0, 0)
         if button_press in trans_params:
             change_trans_ep = trans_params[button_press]
-            self.ell_controller.execute_ell_move((change_trans_ep, (0, 0, 0)), ((0, 0, 0), 0), 
-                                                 quat_gripper_rot, LOCAL_VELOCITY)
+            self.cart_controller.execute_cart_move((change_trans_ep, (0, 0, 0)), ((0, 0, 0), 0), 
+                                                 quat_gripper_rot, TRANS_VELOCITY)
         elif button_press in rot_params:
             change_rot_ep = rot_params[button_press]
-            self.ell_controller.execute_ell_move(((0, 0, 0), change_rot_ep), ((0, 0, 0), 0), 
+            self.cart_controller.execute_cart_move(((0, 0, 0), change_rot_ep), ((0, 0, 0), 0), 
                                                  quat_gripper_rot, ROT_VELOCITY)
         elif button_press == "reset_rotation":
-            self.ell_controller.execute_ell_move(((0, 0, 0), np.mat(np.eye(3))), ((0, 0, 0), 1), 
+            self.cart_controller.execute_cart_move(((0, 0, 0), np.mat(np.eye(3))), ((0, 0, 0), 1), 
                                                  quat_gripper_rot, ROT_VELOCITY)
         end_pos, _ = self.cart_arm.get_ep()
         dist = np.linalg.norm(end_pos - start_pos)
@@ -59,20 +58,19 @@ class EllipsoidInterfaceBackend(ControllerInterfaceBackend):
         button_history[button_press].append(dist)
 
 def main():
-    rospy.init_node("ellipsoidal_controller_backend")
+    rospy.init_node("cartesian_controller_backend")
 
     for button in trans_params.keys() + rot_params.keys():
         button_history[button] = []
-
 
     cart_arm = create_pr2_arm('l', PR2ArmJTransposeTask, 
                               controller_name='%s_cart_jt_task', 
                               end_link="%s_gripper_shaver45_frame")
 
-    ell_backend = EllipsoidInterfaceBackend(cart_arm)
-    ell_backend.disable_interface("Setting up arm.")
+    cart_backend = CartesianInterfaceBackend(cart_arm)
+    cart_backend.disable_interface("Setting up arm.")
 
-    if True:
+    if False:
         ctrl_switcher = ControllerSwitcher()
         ctrl_switcher.carefree_switch('l', '%s_arm_controller', None)
         rospy.sleep(1)
@@ -86,14 +84,15 @@ def main():
                                       "$(find hrl_rfh_fall_2011)/params/l_jt_task_shaver45.yaml") 
         rospy.sleep(1)
 
-    if True:
+    if False:
         rospy.sleep(1)
+        #cart_arm.set_posture(setup_angles)
         setup_angles = [0, 0, np.pi/2, -np.pi/2, -np.pi, -np.pi/2, -np.pi/2]
         cart_arm.set_posture(setup_angles)
         cart_arm.set_gains([200, 800, 800, 80, 80, 80], [15, 15, 15, 1.2, 1.2, 1.2])
-        ell_backend.ell_controller.reset_arm_orientation(8)
+        cart_backend.cart_controller.reset_arm_orientation(8)
 
-    ell_backend.enable_interface("Controller ready.")
+    cart_backend.enable_interface("Controller ready.")
     rospy.spin()
     print button_history
     print "MEANS:"
