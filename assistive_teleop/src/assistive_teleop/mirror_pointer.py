06f1135b36ca83b3bedae1c7398867adeae4148e
= <phillip.grice@gatech.edu>
2012-06-12 14:55:21 -0700

    updates to mirror_pointer.  Not yet functional

--- assistive_teleop/src/assistive_teleop/mirror_pointer.py
+++ assistive_teleop/src/assistive_teleop/mirror_pointer.py
@@ -15,48 +15,47 @@ class MirrorPointer(object):
         self.arm_pose = PoseStamped()
         self.head_pose = PoseStamped()
         self.head_pose.header.frame_id = 'torso_lift_link'
-        self.head_pose.pose.position = Point(1,0.5,0.5)
-        self.head_pose.pose.orientaiton = Quaternion(1,0,0,0)
+        self.head_pose.pose.position = Point(0.3,0.7,0.9)
+        self.head_pose.pose.orientation = Quaternion(1,0,0,0)
         self.test_pub = rospy.Publisher('mirror_test_pose', PoseStamped, latch=True)
         self.test_pub.publish(self.head_pose)
         self.goal_pub = rospy.Publisher('mirror_goal', PoseStamped, latch=True)
         
-        rospy.subscribe('/head_position', PoseStamped, head_pose_cb)
+        rospy.Subscriber('/head_position', PoseStamped, self.head_pose_cb)
 
     def curr_pose(self):
-        (trans,rot) = self.tf.lookupTransform("/torso_lift_link",
-                                            "/r_wrist_roll_link",
-                                            rospy.Time(0))
-        return (trans, rot)
-        #cp = PoseStamped()
-        #cp.header.stamp = rospy.Time.now()
-        #cp.header.frame_id = '/torso_lift_link'
-        #cp.pose.position = Point(*trans)
-        #cp.pose.orientation = Quaternion(*rot)
-        #return cp
-        
+        return self.tf.lookupTransform("torso_lift_link",
+                                        "r_wrist_roll_link",
+                                        rospy.Time(0))
+
     def head_pose_cb(self, msg):
         if not self.active:
             return
-        (trans, rot) = self.tf.transformPose(msg.header.frame_id,
-                                              '/r_gripper_tool_frame',
-                                              rospy.Time.now())
-        rotz = math.atan2(trans[1],trans[0])
+        #t = self.tf.getLatestCommonTime(msg.header.frame_id, '/r_wrist_roll_link')
+        #print "Latest common time: ", t.to_sec()
+        tp = self.tf.transformPose('/r_wrist_roll_link', msg)
+        trans = (tp.pose.position.x, tp.pose.position.y, tp.pose.position.z)
+        print "Trans: %s, %s, %s" %trans
+        rotx = math.atan2(trans[2],trans[1])
+
+        print "X Rot: ", rotx*(180./math.pi)
         roty = math.atan2(trans[0],trans[2])
-        quat = tft.quaternion_from_euler((0., roty, rotz))
-        (ctrans, crot) = curr_pose()
+        print "Y Rot: ", roty*(180./math.pi)
+        rotz = math.atan2(trans[1],trans[0])
+        print "Z Rot: ", rotz*(180./math.pi)
+        quat = tft.quaternion_from_euler(rotx, roty, rotz)
+        (ctrans, crot) = self.curr_pose()
         point_quat = tft.quaternion_multiply(crot,quat)
         goal = PoseStamped()
         goal.header.stamp = rospy.Time.now()
         goal.header.frame_id = 'torso_lift_link'
         goal.pose.position = Point(*ctrans)
         goal.pose.orientation = Quaternion(*point_quat)
-
-
-
+        self.goal_pub.publish(goal)
 
 if __name__=='__main__':
     rospy.init_node('mirror_pointer')
     mp = MirrorPointer()
+    rospy.sleep(1)
     mp.head_pose_cb(mp.head_pose)
     rospy.spin()
