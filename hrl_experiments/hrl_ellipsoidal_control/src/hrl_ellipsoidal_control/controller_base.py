Minor fixes. Added shaving side rotation thing.

hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/controller_base.py
@@ -130,16 +130,16 @@ class EllipsoidControllerBase(CartTrajController):
         ell_pose_mat = PoseConverter.to_homo_mat(pos, quat_rotated)
         return PoseConverter.to_pos_rot(kinect_frame_mat * ell_pose_mat)
                                           
-    def reset_arm_orientation(self, duration=10., gripper_rot=np.pi, blocking=True):
-        with self.cmd_lock:
-            num_samps = duration / self.time_step
-            cur_pose = self.arm.get_end_effector_pose()
-            quat_gripper_rot = tf_trans.quaternion_from_euler(gripper_rot, 0, 0)
-            args = self.get_ell_ep() + [quat_gripper_rot]
-            ell_pose = self.robot_ellipsoidal_pose(*args)
-            adjust_traj = self.arm.interpolate_ep(cur_pose, ell_pose, 
-                                                  min_jerk_traj(num_samps))
-            return self._run_traj(adjust_traj, blocking=blocking)
+    #def reset_arm_orientation(self, duration=10., gripper_rot=np.pi, blocking=True):
+    #    with self.cmd_lock:
+    #        num_samps = duration / self.time_step
+    #        cur_pose = self.arm.get_end_effector_pose()
+    #        quat_gripper_rot = tf_trans.quaternion_from_euler(gripper_rot, 0, 0)
+    #        args = self.get_ell_ep() + [quat_gripper_rot]
+    #        ell_pose = self.robot_ellipsoidal_pose(*args)
+    #        adjust_traj = self.arm.interpolate_ep(cur_pose, ell_pose, 
+    #                                              min_jerk_traj(num_samps))
+    #        return self._run_traj(adjust_traj, blocking=blocking)
 
     def get_ell_frame(self, frame="/torso_lift_link"):
         # find the current ellipsoid frame location in this frame

hrl_experiments/hrl_ellipsoidal_control/src/hrl_ellipsoidal_control/ellipsoid_controller.py
@@ -55,11 +55,14 @@ class EllipsoidController(EllipsoidControllerBase):
     def arm_in_bounds(self):
         ell_ep = self.get_ell_ep()
         equals = ell_ep == self._clip_ell_ep(ell_ep)
+        print ell_ep, equals
         if self._lon_bounds[0] >= 0 and ell_ep[1] >= 0:
             return np.all(equals)
         else:
+            ell_ep_1 = np.mod(ell_ep[1], 2 * np.pi)
             min_lon = np.mod(self._lon_bounds[0], 2 * np.pi)
-            return equals[0] and equals[2] and np.mod(ell_ep[1], 2 * np.pi) > min_lon
+            return (equals[0] and equals[2] and 
+                    (ell_ep_1 >= min_lon or ell_ep_1 <= self._lon_bounds[1]))
 
 
     def _parse_ell_move(self, change_ep, abs_sel, orient_quat):

hrl_experiments/hrl_face_adls/src/hrl_face_adls/face_adls_manager.py
@@ -22,8 +22,6 @@ from hrl_face_adls.face_adls_parameters import *
 from hrl_face_adls.msg import StringArray
 from hrl_face_adls.srv import EnableFaceController, EnableFaceControllerResponse
 
-quat_gripper_rot = tf_trans.quaternion_from_euler(np.pi, 0, 0)
-
 class ForceCollisionMonitor(object):
     def __init__(self):
         self.last_activity_time = rospy.get_time()
@@ -173,6 +171,11 @@ class FaceADLsManager(object):
         self.global_poses = rospy.get_param('/face_adls/%s_global_poses' % shaving_side)
         self.global_move_poses_pub.publish(self.global_poses.keys())
 
+        if shaving_side == 'r':
+            self.gripper_rot = tf_trans.quaternion_from_euler(np.pi, 0, 0)
+        else:
+            self.gripper_rot = tf_trans.quaternion_from_euler(0, 0, 0)
+
         self.controller_enabled_pub.publish(Bool(True))
         return True
 
@@ -194,7 +197,7 @@ class FaceADLsManager(object):
         if forced:
             self.is_forced_retreat = True
         self.ell_ctrl.execute_ell_move(((0, 0, height), (0, 0, 0)), ((0, 0, 1), 0), 
-                                       quat_gripper_rot, velocity, blocking=False)
+                                       self.gripper_rot, velocity, blocking=False)
         rospy.loginfo("[face_adls_manager] Retreating from current location.")
         self.ell_ctrl.wait_until_stopped()
         self.is_forced_retreat = False
@@ -221,15 +224,15 @@ class FaceADLsManager(object):
         self.publish_feedback(Messages.GLOBAL_START % goal_pose_name)
         try:
             if not self.ell_ctrl.execute_ell_move(((0, 0, RETREAT_HEIGHT), (0, 0, 0)), ((0, 0, 1), 0), 
-                                                  quat_gripper_rot, APPROACH_VELOCITY, blocking=True):
+                                                  self.gripper_rot, APPROACH_VELOCITY, blocking=True):
                 raise Exception
             if not self.ell_ctrl.execute_ell_move(((goal_pose[0][0], goal_pose[0][1], RETREAT_HEIGHT), 
                                                   (0, 0, 0)), 
                                                   ((1, 1, 1), 0), 
-                                                  quat_gripper_rot, GLOBAL_VELOCITY, blocking=True):
+                                                  self.gripper_rot, GLOBAL_VELOCITY, blocking=True):
                 raise Exception
             if not self.ell_ctrl.execute_ell_move((goal_pose[0], (0, 0, 0)), ((1, 1, 1), 0), 
-                                                  quat_gripper_rot, GLOBAL_VELOCITY, blocking=True):
+                                                  self.gripper_rot, GLOBAL_VELOCITY, blocking=True):
                 raise Exception
         except:
             self.publish_feedback(Messages.GLOBAL_PREEMPT % goal_pose_name)
@@ -256,16 +259,16 @@ class FaceADLsManager(object):
             self.publish_feedback(Messages.LOCAL_START % button_names_dict[button_press])
             change_trans_ep = ell_trans_params[button_press]
             success = self.ell_ctrl.execute_ell_move((change_trans_ep, (0, 0, 0)), ((0, 0, 0), 0), 
-                                                    quat_gripper_rot, ELL_LOCAL_VEL, blocking=True)
+                                                    self.gripper_rot, ELL_LOCAL_VEL, blocking=True)
         elif button_press in ell_rot_params:
             self.publish_feedback(Messages.LOCAL_START % button_names_dict[button_press])
             change_rot_ep = ell_rot_params[button_press]
             success = self.ell_ctrl.execute_ell_move(((0, 0, 0), change_rot_ep), ((0, 0, 0), 0), 
-                                                    quat_gripper_rot, ELL_ROT_VEL, blocking=True)
+                                                    self.gripper_rot, ELL_ROT_VEL, blocking=True)
         elif button_press == "reset_rotation":
             self.publish_feedback(Messages.ROT_RESET_START)
             success = self.ell_ctrl.execute_ell_move(((0, 0, 0), np.mat(np.eye(3))), ((0, 0, 0), 1), 
-                                                    quat_gripper_rot, ELL_ROT_VEL, blocking=True)
+                                                    self.gripper_rot, ELL_ROT_VEL, blocking=True)
         else:
             rospy.logerr("[face_adls_manager] Unknown ellipsoidal local command")
 

hrl_experiments/hrl_face_adls/src/hrl_face_adls/face_adls_parameters.py
@@ -21,7 +21,7 @@ TIMEOUT_TIME = 30.0
 LAT_BOUNDS = {'r' : (np.pi/8, 7*np.pi/8), 'l' : (np.pi/8, 7*np.pi/8)}
 #LON_BOUNDS = {'r' : (-np.inf, np.inf), 'l' : (-np.inf, np.inf)}
 LON_BOUNDS = {'r' : (-5*np.pi/8, np.pi/8), 'l' : (-np.pi/8, 5*np.pi/8)}
-HEIGHT_BOUNDS = {'r' : (0.2, 2.5), 'l' : (0.2, 2.5)}
+HEIGHT_BOUNDS = {'r' : (0.2, 3.5), 'l' : (0.2, 3.5)}
 
 outcomes_spa = ['succeeded','preempted','aborted']
 