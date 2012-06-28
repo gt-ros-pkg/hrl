d0cdecefa6f9a4488af0b2f26f133d52f948cb91
Healthcare Robotics Lab <gthrl@prj1.(none)>
2012-06-28 09:14:04 -0700

    Added better linear thresholding for AR servoing image processing.

--- hrl_experiments/hrl_face_adls/src/ar_servo_image_proc.cpp
+++ hrl_experiments/hrl_face_adls/src/ar_servo_image_proc.cpp
@@ -5,6 +5,8 @@
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
 
+using namespace std;
+
 image_transport::CameraSubscriber camera_sub;
 image_transport::Publisher output_pub;
 cv_bridge::CvImagePtr cv_img;
@@ -17,15 +19,21 @@ void doOverlay(const sensor_msgs::ImageConstPtr& img_msg,
     cv::Mat input(cv_img->image.rows, cv_img->image.cols, CV_8UC1);
     cv_bridge::CvImage new_cv_img;
     new_cv_img.image = cv::Mat(cv_img->image.rows, cv_img->image.cols, CV_8UC1);
+    double black_cap, white_cap;
+    ros::param::param<double>("~black_cap", black_cap, 50);
+    ros::param::param<double>("~white_cap", white_cap, 150);
     for(int j=0;j<cv_img->image.cols;j++)
         for(int i=0;i<cv_img->image.rows;i++) {
-            float inten = (cv_img->image.at<cv::Vec3b>(i, j)[0] +
-                           cv_img->image.at<cv::Vec3b>(i, j)[1] +
-                           cv_img->image.at<cv::Vec3b>(i, j)[2])/3.;
+            double inten = (cv_img->image.at<cv::Vec3b>(i, j)[0] +
+                            cv_img->image.at<cv::Vec3b>(i, j)[1] +
+                            cv_img->image.at<cv::Vec3b>(i, j)[2])/3.;
+            inten = 255.0 / (white_cap - black_cap) * (inten - black_cap);
+            inten = max(0.0, min(inten, 255.0));
             input.at<uint8_t>(i, j) = (uint8_t) inten;
         }
 
-    cv::equalizeHist(input, new_cv_img.image);
+    new_cv_img.image = input;
+    //cv::equalizeHist(input, new_cv_img.image);
     new_cv_img.header = cv_img->header;
     new_cv_img.encoding = sensor_msgs::image_encodings::MONO8;
     output_pub.publish(new_cv_img.toImageMsg());
--- hrl_experiments/hrl_face_adls/src/hrl_face_adls/srv/__init__.py
+++ hrl_experiments/hrl_face_adls/src/hrl_face_adls/srv/__init__.py
@@ -1,4 +1,5 @@
 from ._InitializeRegistration import *
 from ._EnableCartController import *
+from ._ClickImage import *
 from ._EnableFaceController import *
 from ._RequestRegistration import *
