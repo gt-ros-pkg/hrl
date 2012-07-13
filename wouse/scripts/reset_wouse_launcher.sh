a7d99ace0b7c7b77df8bbc962e0dbda55a6f5c6e
pgrice <phillip.grice@gatech.edu>
2012-07-13 17:09:00 -0400

    working on launcher for wouse training session

--- wouse/scripts/reset_wouse_launcher.sh
+++ wouse/scripts/reset_wouse_launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source /opt/ros/electric/setup.bash
+
+source /opt/ros/fuerte/setup.bash
 export ROS_PACKAGE_PATH="/home/pgrice/git/gt-ros-pkg/gt-ros-pkg.hrl/wouse:$ROS_PACKAGE_PATH"
 export ROS_MASTER_URI=http://monty1.hsi.gatech.edu:11311
-env
 python ~/git/gt-ros-pkg.hrl/wouse/scripts/reset_wouse.py
