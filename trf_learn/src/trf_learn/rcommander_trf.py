032128b71aac3f6def7fa3b2dd2ca8b1920c14e4
Hai Nguyen <haidai@gmail.com>
2012-08-07 15:52:25 -0400

    now uses pr2 robot class

--- trf_learn/src/trf_learn/rcommander_trf.py
+++ trf_learn/src/trf_learn/rcommander_trf.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
-import roslib; roslib.load_manifest('rcommander_plain')
+import roslib; roslib.load_manifest('trf_learn')
 import rcommander.rcommander as rc
 import rospy
 import tf
+import pypr2.pr2_utils as pu
 
-class MyRobotClass:
-    def __init__(self):
-        self.some_resource = "hello"
 
 rospy.init_node('rcommander_trf', anonymous=True)
-robot = MyRobotClass()
+
 tf    = tf.TransformListener()
-rc.run_rcommander(['default', 'default_frame', 'trf'], robot, tf, width=800, height=600)
+pr2 = pu.PR2(tf)
+rc.run_rcommander(['default', 'default_frame', 'trf', 'pr2'], pr2, tf, width=800, height=600)
