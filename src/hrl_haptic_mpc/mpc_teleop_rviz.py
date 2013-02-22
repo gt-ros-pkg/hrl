7518a1bbe11455ff587e882b9b83e29678b512d9
Kevin Chow <kevinlichow90@gmail.com>
2013-02-22 16:36:58 -0500

    changed mpc_teleop_rviz.py slightly so that it works with crona...otherwise unchanged

--- src/hrl_haptic_mpc/mpc_teleop_rviz.py
+++ src/hrl_haptic_mpc/mpc_teleop_rviz.py
@@ -212,8 +212,8 @@ class MPCTeleopInteractiveMarkers():
     self.zero_cody_meka_skin_pub = rospy.Publisher('/skin_patch_forearm_right/zero_sensor', Empty)
     self.zero_cody_fabric_forearm_pub = rospy.Publisher('/fabric_forearm_sensor/zero_sensor', Empty)
     self.zero_cody_fabric_wrist_pub = rospy.Publisher('/fabric_wrist_sensor/zero_sensor', Empty)
-    
-    self.gripper_action_client = actionlib.SimpleActionClient(arm+'_gripper_controller/gripper_action', Pr2GripperCommandAction)
+    if self.opt.robot != 'crona':
+        self.gripper_action_client = actionlib.SimpleActionClient(arm+'_gripper_controller/gripper_action', Pr2GripperCommandAction)
  
     self.server = ims.InteractiveMarkerServer('teleop_rviz_server')
 
